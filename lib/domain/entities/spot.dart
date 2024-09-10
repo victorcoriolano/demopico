9691ba96d0047f77020cd60005f20a6063f7ba92
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-09-10 15:29:31 -0300

    Criação da entidade spot
    spot = local, ponto de interesse

---
+++ lib/domain/entities/spot.dart
@@ -0,0 +1,13 @@
+class Spot {
+  final int id;
+  final String spotName;
+  final String? description;
+  final int lat;
+  final int long;
+  
+  Spot({required this.id, 
+  required this.spotName, 
+  required this.description, 
+  required this.lat, 
+  required this.long});
+}
