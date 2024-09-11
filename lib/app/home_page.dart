9b2cefdfb5fe691b0295940ed874901c8b007143
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-09-10 23:41:47 -0300

    reorganizando arquvios

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:demopico/features/home/presentation/ui/pages/central_page.dart';
+import 'package:demopico/features/home/presentation/pages/central_page.dart';
 import 'package:demopico/features/mapa/presentation/pages/map_page.dart';
 import 'package:demopico/features/profile/presentation/pages/user_page.dart';
 import 'package:flutter/material.dart';
