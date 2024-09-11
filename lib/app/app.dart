0e5ea55b165b7da6ea894cce4bd297637cb531d3
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-09-10 23:35:37 -0300

    movendo homepage para /app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,4 @@
-import 'package:demopico/features/home/presentation/ui/pages/home_page.dart';
+import 'package:demopico/app/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
