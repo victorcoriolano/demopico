7862446e5816632bb89a35f809da8b6e7059ba02
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-13 09:58:51 -0300

    adicionando key android e ios google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+     GMSServices.provideAPIKey("AIzaSyD9Vb3pQkxxG7Hp28-q-1KshsJ9NqIq6Cw")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
