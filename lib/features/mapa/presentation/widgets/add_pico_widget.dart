71d43b9ff6c4daef06b67e452a17146b432db996
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-17 01:37:04 -0300

    subinod button

--- lib/features/mapa/presentation/widgets/add_pico_widget.dart
+++ lib/features/mapa/presentation/widgets/add_pico_widget.dart
@@ -5,12 +5,55 @@ class AddPicoWidget extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton.large(onPressed: onPressed,
-    backgroundColor: Color.fromARGB(255, 139, 0, 0),
-    child: const Center(
-      child: Icon(Icons.add) ,
-    ),
-    
-    );
+    return  Stack(
+        children: [
+          Positioned(
+            bottom: 20.0, // Distância do fundo da tela
+            right: 20.0, // Distância da borda direita da tela
+            child: Container(
+              width: 70, // Tamanho total do Container
+              height: 70, // Tamanho total do Container
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent, // Cor de fundo do Container
+                border: Border.all(
+                  color: Color(0xff343434), // Cor da borda
+                  width: 2, // Largura da borda
+                ),
+              ),
+              child: Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white, // Cor de fundo
+                    elevation: 0, // Remove a sombra
+                    shape: CircleBorder(), // Forma circular
+                    padding: EdgeInsets.zero, // Remove o padding padrão
+                  ),
+                  onPressed: () {
+                    // Função de exemplo
+                  },
+                  child: SizedBox.expand(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 40, // Largura da linha horizontal
+                          height: 6, // Espessura da linha horizontal
+                          color: Color.fromARGB(255, 139, 0, 0), // Cor da linha
+                        ),
+                        Container(
+                          width: 6, // Espessura da linha vertical
+                          height: 40, // Altura da linha vertical
+                          color: Color.fromARGB(255, 139, 0, 0), // Cor da linha
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
   }
 }
