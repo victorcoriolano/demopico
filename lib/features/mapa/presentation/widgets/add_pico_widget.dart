d2bc3f66df151483a88895300b50b27428dc40a6
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-18 11:44:42 -0300

    alteraçoes finais no visual do buttom

--- lib/features/mapa/presentation/widgets/add_pico_widget.dart
+++ lib/features/mapa/presentation/widgets/add_pico_widget.dart
@@ -2,60 +2,61 @@ import 'package:flutter/material.dart';
 
 class AddPicoWidget extends StatelessWidget {
   const AddPicoWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
-            bottom:40.0, // Distância do fundo da tela
-            right: 20.0, // Distância da borda direita da tela
-            child: Container(
-              width: 75, // Tamanho total do Container
-              height: 75, // Tamanho total do Container
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.transparent, // Cor de fundo do Container
-                border: Border.all(
-                  color: const Color.fromARGB(255, 162, 162, 162), // Cor da borda
-                  width: 2, // Largura da borda
-                ),
-              ),
-              child: Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white, // Cor de fundo
-                    elevation: 6, // Remove a sombra
-                    shape: const CircleBorder(), // Forma circular
-                    padding: EdgeInsets.zero, // Remove o padding padrão
-                  ),
-                  onPressed: () {
-                    // Função de exemplo
-                  },
-                  child: SizedBox.expand(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                         decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                            color: const Color.fromARGB(255, 139, 0, 0), // Cor da linha
-                          ),
-                          width: 42, // Largura da linha horizontal
-                          height: 8, // Espessura da linha horizontal
-                        ),
-                        Container(
-                           decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                            color: const Color.fromARGB(255, 139, 0, 0), // Cor da linha
-                          ),
-                          width: 8, // Espessura da linha vertical
-                          height: 42, // Altura da linha vertical
-                        ),
-                      ],
+      bottom: 40.0, // Distância do fundo da tela
+      right: 20.0, // Distância da borda direita da tela
+      child: Container(
+        width: 72, // Tamanho total do Container
+        height: 72, // Tamanho total do Container
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.transparent, // Cor de fundo do Container
+          border: Border.all(
+            color: const Color.fromARGB(255, 162, 162, 162), // Cor da borda
+            width: 2, // Largura da borda
+          ),
+        ),
+        child: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white, // Cor de fundo
+              elevation: 6, // Remove a sombra
+              shape: const CircleBorder(), // Forma circular
+              padding: EdgeInsets.zero, // Remove o padding padrão
+            ),
+            onPressed: () {
+              // Função de exemplo
+            },
+            child: SizedBox.expand(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      color:  Color(0xFFBB271A),
+                      // Cor da linha
                     ),
+                    width: 44, // Largura da linha horizontal
+                    height: 8, // Espessura da linha horizontal
                   ),
-                ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      color: Color(0xFFBB271A),
+                    ),
+                    width: 8, // Espessura da linha vertical
+                    height: 44, // Altura da linha vertical
+                  ),
+                ],
               ),
             ),
-          ); 
+          ),
+        ),
+      ),
+    );
   }
 }
