665d8056bf7f729ff2156e07a4fb35ee34b246fc
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-18 15:08:27 -0300

    começando markers maps widget

---
+++ lib/features/mapa/presentation/widgets/markers_maps_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MarkersMapsWidget extends StatelessWidget {
+  const MarkersMapsWidget({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Marker(
+          markerId: markerPicoId,
+          position: positionPicoId,
+          
+        )
+    ) ;
+      
+  
+  }
+}
