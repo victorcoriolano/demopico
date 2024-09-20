cc9b25ebf5e432cd6236717c751f0f4b32685a4e
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-19 22:35:59 -0300

    entity markers

---
+++ lib/core/domain/entities/marker_maps_entity.dart
@@ -0,0 +1,17 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MarkerData {
+  final String id;
+  final LatLng position;
+
+  MarkerData(this.id, this.position,);
+}
+
+Set<Marker> createMarkers(List<MarkerData> markersData) {
+  return markersData.map((markerData) {
+    return Marker(
+      markerId: MarkerId(markerData.id),
+      position: markerData.position,
+    );
+  }).toSet();
+}
