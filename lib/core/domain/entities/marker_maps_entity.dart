34379fb63aad2b5d73c217486ebb8120ee224b3a
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-28 14:42:51 -0300

    uodate

--- lib/core/domain/entities/marker_maps_entity.dart
+++ lib/core/domain/entities/marker_maps_entity.dart
@@ -1,3 +1,4 @@
+import 'package:demopico/core/domain/entities/pico_entity_dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MarkerData {
@@ -7,11 +8,13 @@ class MarkerData {
   MarkerData({required this.id, required this.position});
 }
 
-Set<Marker> createMarkers(List<MarkerData> markersData) {
+
+
+Set<Marker> createMarkers(List<Pico> markersData) {
   return markersData.map((markerData) {
     return Marker(
-      markerId: MarkerId(markerData.id),
-      position: markerData.position,
+      markerId: MarkerId(markerData.urlIdPico),
+      position:   Pico().id,
     );
   }).toSet();
 }
