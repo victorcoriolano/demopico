32de5cf2df29b6e6af5b41b5f67b2f49e3e63698
Arthur Selingin <arthur_santos@outlook.com.br>
2024-10-01 22:02:03 -0300

    centralizando top mapa

--- lib/features/mapa/presentation/widgets/top_side_map_widget.dart
+++ lib/features/mapa/presentation/widgets/top_side_map_widget.dart
@@ -2,44 +2,46 @@ import 'package:flutter/material.dart';
 
 class TopSideMapWidget extends StatelessWidget implements PreferredSizeWidget {
   const TopSideMapWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-        backgroundColor: Color(0xFF8B0000),
-        title: Row(
-          children: [
-            Expanded(
-              child: Container(
-                height: 40, // Ajuste da altura da barra de pesquisa
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 10), // Centraliza o conteúdo verticalmente
-                    hintText: 'Buscar Picos',
-                    prefixIcon: Icon(Icons.search, color: Colors.grey), // Ícone ajustado para centralizar melhor
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
+@override
+Widget build(BuildContext context) {
+  return AppBar(
+    toolbarHeight: 100, // Ajuste a altura da AppBar se necessário
+    backgroundColor: Color(0xFF8B0000),
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center, // Centraliza o conteúdo do Row
+      children: [
+        Expanded(
+          child: Container(
+            height: 50, // Altura do campo de busca
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 10), // Centraliza o conteúdo verticalmente
+                hintText: 'Buscar Picos',
+                prefixIcon: Icon(Icons.search, color: Colors.grey), // Ícone ajustado para centralizar melhor
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
                 ),
+                filled: true,
+                fillColor: Colors.white,
               ),
             ),
-            SizedBox(width: 10), // Espaçamento entre a barra de busca e os ícones
-            IconButton(
-              icon: Icon(Icons.filter_list, color: Colors.white),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.settings, color: Colors.white),
-              onPressed: () {},
-            ),
-          ],
+          ),
         ),
-      );
-  }
+        SizedBox(width: 10), // Espaçamento entre a barra de busca e os ícones
+        IconButton(
+          icon: Icon(Icons.filter_list, color: Colors.white),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.settings, color: Colors.white),
+          onPressed: () {},
+        ),
+      ],
+    ),
+  );
+}
+
 
   @override
   Size get preferredSize => const Size.fromHeight(80);
