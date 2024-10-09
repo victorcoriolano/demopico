9a3624f312bb614cc6de8d759b25926b83915b7a
Arthur Selingin <arthur_santos@outlook.com.br>
2024-10-08 21:09:31 -0300

    separando scroll da foto

--- lib/features/mapa/presentation/widgets/add_pico_modal/segunda_tela.dart
+++ lib/features/mapa/presentation/widgets/add_pico_modal/segunda_tela.dart
@@ -31,65 +31,67 @@ class _SegundaTelaState extends State<SegundaTela> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-      
-            child: SingleChildScrollView(
-              
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Imagem no topo
-                   Center(
-                      child: Padding(
-                       
-                        padding: const EdgeInsets.all(12.0),
-                        child: Image.asset('assets/images/progresso2.png', ),
-                      ),
-                    ),
-
-                  // Widget de cada atributo com descrição abaixo dos ícones
-                  buildAtributo(
-                    'Chão',
-                    'Liso / Plaza',
-                    atributos['Chão']!,
-                    (valor) => setAtributo('Chão', valor),
-                  ),
-                  buildAtributo(
-                    'Iluminação',
-                    'Clarinho',
-                    atributos['Iluminação']!,
-                    (valor) => setAtributo('Iluminação', valor),
-                  ),
-                  buildAtributo(
-                    'Policiamento',
-                    'Paloso',
-                    atributos['Policiamento']!,
-                    (valor) => setAtributo('Policiamento', valor),
-                  ),
-                  buildAtributo(
-                    'Movimento',
-                    'Cheio',
-                    atributos['Movimento']!,
-                    (valor) => setAtributo('Movimento', valor),
-                  ),
-                  buildAtributo(
-                    'Kick-Out',
-                    'Bem Capaz',
-                    atributos['Kick-Out']!,
-                    (valor) => setAtributo('Kick-Out', valor),
-                  ),
-                  // Botão de prosseguir
-                
-                ],
+  backgroundColor: Colors.grey[200],
+  body: Column(
+    children: [
+      // A imagem ficará fora da rolagem
+      Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Image.asset(
+          'assets/images/progresso2.png',
+        ),
+      ),
+      // O restante dos widgets ficará dentro da rolagem
+      Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Widget de cada atributo com descrição abaixo dos ícones
+              buildAtributo(
+                'Chão',
+                'Liso / Plaza',
+                atributos['Chão']!,
+                (valor) => setAtributo('Chão', valor),
               ),
-            ),
-          )
-    );
+              buildAtributo(
+                'Iluminação',
+                'Clarinho',
+                atributos['Iluminação']!,
+                (valor) => setAtributo('Iluminação', valor),
+              ),
+              buildAtributo(
+                'Policiamento',
+                'Paloso',
+                atributos['Policiamento']!,
+                (valor) => setAtributo('Policiamento', valor),
+              ),
+              buildAtributo(
+                'Movimento',
+                'Cheio',
+                atributos['Movimento']!,
+                (valor) => setAtributo('Movimento', valor),
+              ),
+              buildAtributo(
+                'Kick-Out',
+                'Bem Capaz',
+                atributos['Kick-Out']!,
+                (valor) => setAtributo('Kick-Out', valor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
+  ),
+);
+
+        
   }
 
   // Widget para construir cada linha de atributo
-  Widget buildAtributo(String titulo, String descricao, int valor, Function(int) onChanged) {
+  Widget buildAtributo(
+      String titulo, String descricao, int valor, Function(int) onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os itens
       children: [
@@ -107,30 +109,28 @@ class _SegundaTelaState extends State<SegundaTela> {
         // Avaliação por imagens customizadas
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-           // Centraliza os ícones
+          // Centraliza os ícones
           children: List.generate(5, (index) {
-           // Ajustado para um espaçamento menor
-           return SizedBox(
-            width:70
-            ,
-               child: Container(
+            // Ajustado para um espaçamento menor
+            return SizedBox(
+              width: 70,
+              child: Container(
                 margin: EdgeInsets.only(bottom: 10, top: 5),
-                 child: IconButton(
-                      icon: Image.asset( 
-                      'assets/images/iconPico.png', // Substitua pelo caminho da sua imagem
-                        color: index < valor ? Color(0xFF8B0000) : Colors.grey[300], // Altera a cor do ícone baseado na avaliação
-                      ),
-                      iconSize: 10, // Tamanho aumentado para 50
-                      onPressed: () {
-                        onChanged(index + 1); // Atualiza o valor do atributo
-                      } ,
-                     ),
-               ),
-       
-      );
-      
-          
-          
+                child: IconButton(
+                  icon: Image.asset(
+                    'assets/images/iconPico.png', // Substitua pelo caminho da sua imagem
+                    color: index < valor
+                        ? Color(0xFF8B0000)
+                        : Colors.grey[
+                            300], // Altera a cor do ícone baseado na avaliação
+                  ),
+                  iconSize: 10, // Tamanho aumentado para 50
+                  onPressed: () {
+                    onChanged(index + 1); // Atualiza o valor do atributo
+                  },
+                ),
+              ),
+            );
           }),
         ),
 
