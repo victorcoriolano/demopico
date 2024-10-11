5b21d680983a5acceffb994070a5d8ba705f26e0
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-10-11 09:59:36 -0300

    observação

--- lib/features/user/data/models/user_model.dart
+++ lib/features/user/data/models/user_model.dart
@@ -1,10 +1,13 @@
 import 'package:demopico/features/user/domain/entities/user.dart';
 
+//Usar senha como atributo? Firebase deve tratar isso
+//Senha é passado como parâmetro nos métodos de SignIn/SignUp e não deve mais estar no código-fonte depois
+//Analisar
 class UserModel extends User {
   UserModel(
-      {required super.senha,
-      required super.email,
-      required super.vulgo,
+      {super.senha,
+      super.email,
+      super.vulgo,
       super.id});
 
   factory UserModel.fromFirebase(User firebaseUser) {
