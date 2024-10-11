90d97d8fa0f9d9e11adc84c121710482dcb0201a
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-10-11 10:04:54 -0300

    retirando senha como atributo

--- lib/features/user/data/repositories/user_repository.dart
+++ lib/features/user/data/repositories/user_repository.dart
@@ -2,6 +2,6 @@ import 'package:demopico/features/user/data/models/user_credentials.dart';
 
 class CredentialsRepository {
   static List<UserCredentials> usersCredentials = [
-    UserCredentials(email: '1', senha: '1', vulgo: 'vito'),
+    UserCredentials(email: '1', vulgo: 'vito'),
   ];
 }
