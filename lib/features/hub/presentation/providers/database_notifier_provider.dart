739b8c5a0141013246733cf62637821998d34cc3
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-13 14:52:17 -0300

    passando serviço do usuario

--- lib/features/hub/presentation/providers/database_notifier_provider.dart
+++ lib/features/hub/presentation/providers/database_notifier_provider.dart
@@ -1,17 +1,20 @@
 import 'package:demopico/features/hub/infra/services/database_service.dart';
 import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/user/data/models/user.dart';
+import 'package:demopico/features/user/data/services/userService.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HubProvider extends ChangeNotifier {
   final _db = HubService();
+  final  UserService _userService = UserService();
 
   void notify() {
     notifyListeners();
   }
 
   Future<UserM?> retrieveUserProfileData(String uid) =>
-      _db.getUserDetailsFromFirestore(uid);
+      _userService.getUserDetailsFromFirestore(uid);
 
   List<Communique> _allCommuniques = [];
 
