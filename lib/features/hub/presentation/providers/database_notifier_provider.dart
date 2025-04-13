dad7c50ac7a03f628bbc793e5577ac3a95fe03f3
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-13 17:51:06 -0300

    desaclopando

--- lib/features/hub/presentation/providers/database_notifier_provider.dart
+++ lib/features/hub/presentation/providers/database_notifier_provider.dart
@@ -2,31 +2,34 @@ import 'package:demopico/features/hub/infra/services/database_service.dart';
 import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/user/data/models/user.dart';
 import 'package:demopico/features/user/data/services/userService.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HubProvider extends ChangeNotifier {
-  final _db = HubService();
-  final  UserService _userService = UserService();
+  final HubService hubService;
+
+  HubProvider({required this.hubService});
 
   void notify() {
     notifyListeners();
   }
 
-  Future<UserM?> retrieveUserProfileData(String uid) =>
-      _userService.getUserDetailsFromFirestore(uid);
+
+//codiigo de doido que n acho q serve pra nada
+//    Future<UserM?> retrieveUserProfileData(String uid) =>
+  //    _userService.getUserDetailsFromFirestore(uid);
+
 
   List<Communique> _allCommuniques = [];
 
   List<Communique> get allCommuniques => _allCommuniques;
 
   Future<void> postHubCommunique(String text, String type) async {
-    await _db.postHubCommuniqueToFirebase(text, type);
+    await hubService.postHubCommuniqueToFirebase(text, type);
     getAllCommuniques();
   }
 
   Future<void> getAllCommuniques() async {
-    final allCommuniquesFromDb = await _db.getAllCommuniques();
+    final allCommuniquesFromDb = await hubService.getAllCommuniques();
     _allCommuniques = allCommuniquesFromDb;
     notifyListeners();
   }
