5ab37f6d3a14b527754da7b5c9de521aeeec0af8
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-13 13:36:00 -0300

    movendo provider

---
+++ lib/features/hub/presentation/providers/database_notifier_provider.dart
@@ -0,0 +1,30 @@
+import 'package:demopico/features/hub/infra/services/database_service.dart';
+import 'package:demopico/features/hub/domain/entities/communique.dart';
+import 'package:demopico/features/user/data/models/user.dart';
+import 'package:flutter/material.dart';
+
+class HubProvider extends ChangeNotifier {
+  final _db = HubService();
+
+  void notify() {
+    notifyListeners();
+  }
+
+  Future<UserM?> retrieveUserProfileData(String uid) =>
+      _db.getUserDetailsFromFirestore(uid);
+
+  List<Communique> _allCommuniques = [];
+
+  List<Communique> get allCommuniques => _allCommuniques;
+
+  Future<void> postHubCommunique(String text, String type) async {
+    await _db.postHubCommuniqueToFirebase(text, type);
+    getAllCommuniques();
+  }
+
+  Future<void> getAllCommuniques() async {
+    final allCommuniquesFromDb = await _db.getAllCommuniques();
+    _allCommuniques = allCommuniquesFromDb;
+    notifyListeners();
+  }
+}
