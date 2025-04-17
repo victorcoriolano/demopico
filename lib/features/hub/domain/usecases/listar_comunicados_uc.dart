a2fa8b27bddaf623372b4180a02d6740d9962619
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-17 17:11:16 -0300

    listar comunicados caso de uso

--- lib/features/hub/domain/usecases/listar_comunicados_uc.dart
+++ lib/features/hub/domain/usecases/listar_comunicados_uc.dart
@@ -1,3 +1,11 @@
+import 'package:demopico/features/hub/domain/entities/communique.dart';
+import 'package:demopico/features/hub/infra/services/hub_service.dart';
+
 class ListarComunicado{
-  
+  HubService hubService;
+  ListarComunicado({required this.hubService});
+
+  Future<List<Communique?>> listar() async{
+   return await hubService.getInstance.getAllCommuniques();
+  }
 }
