add50035c1e40b44561012eb5e5156c569f50854
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-17 14:57:33 -0300

    renomeando chamada de instancia

--- lib/features/external/datasources/firestore.dart
+++ lib/features/external/datasources/firestore.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Firestore {
   FirebaseFirestore? _firestore;
-  FirebaseFirestore  get firestore {
+  FirebaseFirestore  get getInstance {
     _firestore ??= FirebaseFirestore.instance;
     return _firestore!;
   }
