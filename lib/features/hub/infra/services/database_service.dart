c3e00e302e513a5adf6562c9c6110548acae48db
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-04-16 21:51:49 -0300

    movendo firestore para instancia unica em external

--- lib/features/hub/infra/services/database_service.dart
+++ lib/features/hub/infra/services/database_service.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
-
+import 'package:demopico/features/external/datasources/firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/user/data/models/user.dart';
 import 'package:demopico/features/user/data/services/auth_service.dart';
+import 'package:demopico/features/user/data/services/database_service.dart';
 import 'package:flutter/foundation.dart';
 
 class HubService {
@@ -13,85 +14,10 @@ class HubService {
     return _instance!;
   }
 
-  FirebaseFirestore? _firestore;
-  FirebaseFirestore get firestore {
-    _firestore ??= FirebaseFirestore.instance;
-    return _firestore!;
-  }
-
-  AuthService auth = AuthService();
-////////////////////////////
-////////////////////////////
-////////////////////////////
-  Future<void> addUserDetailsToFirestore({required UserM newUser}) async {
-    String uid = newUser.id!;
-
-    final mappedUser = newUser.toJsonMap();
-
-    await firestore.collection('users').doc(uid).set(mappedUser);
-  }
-
-/////////////////////////////////////////////////////
-/////////////////////////////////////////////////////
-/////////////////////////////////////////////////////
-  Future<UserM?> getUserDetailsFromFirestore(String? uid) async {
-    try {
-      DocumentSnapshot userSnapshot =
-          await firestore.collection('users').doc(uid).get();
-
-      if (userSnapshot.exists) {
-        return UserM.fromDocument(userSnapshot);
-      } else {
-        return null;
-      }
-    } on FirebaseException catch (e) {
-      if (kDebugMode) {
-        print(e.code);
-        print(e.message);
-        print(e.stackTrace);
-      }
-    }
-    return null;
-  }
-
-//Serviço GET ID by USERNAME
-  Future<String?> getIDByVulgo(
-    String vulgo,
-  ) async {
-    QuerySnapshot idSnapshot = await firestore
-        .collection("users_email_vulgo")
-        .where('vulgo', isEqualTo: vulgo)
-        .get();
-    if (idSnapshot.docs.isNotEmpty) {
-      return idSnapshot.docs.first.id;
-    } else {
-      return null;
-    }
-  }
-
-  //Serviço GET EMAIL by ID
-  Future<String?> getEmailByID(String id) async {
-    DocumentSnapshot emailSnapshot =
-        await firestore.collection("user_email_vulgo").doc(id).get();
-    if (emailSnapshot.exists) {
-      Map<String, dynamic>? data =
-          emailSnapshot.data() as Map<String, dynamic>?;
-      if (data != null && data.containsKey('email')) {
-        return data['email'] as String;
-      } else {
-        return null;
-      }
-    } else {
-      return null;
-    }
-  }
-
-
-////////////////
-////////////////
 // Postar no hub
   Future<void> postHubCommuniqueToFirebase(String text, type) async {
     try {
+      AuthService auth = AuthService();
       String? uid = auth.currentUser?.uid;
       String? name = auth.currentUser?.displayName;
       print('$uid, $name');
@@ -100,9 +26,12 @@ class HubService {
           print('erro em pegar name e uid do auth current user');
         }
       } else {
-        UserM? user = await getUserDetailsFromFirestore(uid);
+        DatabaseService dbService = DatabaseService();
+        UserM? user = await dbService.getUserDetailsFromFirestore(uid);
         print(user.toString());
         if (user != null) {
+          Firestore firelibrary = Firestore();
+          FirebaseFirestore firestore = firelibrary.firestore;
           Communique newCommunique = Communique(
             id: Random(27345).toString(),
             uid: uid!,
