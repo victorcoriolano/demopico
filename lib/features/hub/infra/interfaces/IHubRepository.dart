34721789d73d15d435197f94ee04dc6f3444c2d1
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-17 15:31:01 -0300

    mudando retorno na interface

--- lib/features/hub/infra/interfaces/IHubRepository.dart
+++ lib/features/hub/infra/interfaces/IHubRepository.dart
@@ -1,6 +1,8 @@
+import 'package:demopico/features/hub/domain/entities/communique.dart';
+
 abstract class IHubRepository {
-  Future<void> createCommunique(Object obj);
-  Future<void> updateCommunique(Object obj);
+  Future<void> createCommunique(Communique communique);
+  Future<void> updateCommunique(Communique communique);
   Future<void> deleteCommunique(String id);
-  Future<List<Object>?> listCommuniques();
+  Future<List<Communique?>> listCommuniques();
 }
