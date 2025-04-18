7a72254df1d09c4744e99ceec5272d8d4fa681ad
Gabriel Pires <dasaybarreto724@gmail.com>
2025-04-18 16:35:21 -0300

    criando pastas para teste e adicionando mockito para mockar dependencias

---
+++ test/test_map/use_cases/test_create_spot_uc.dart
@@ -0,0 +1,10 @@
+import 'package:demopico/features/mapa/domain/interfaces/i_spot_repository.dart';
+import 'package:demopico/features/mapa/domain/usecases/create_spot_uc.dart';
+import 'package:test/test.dart';
+
+void main() {
+    test("testando aplicação de caso de uso ao criar pico", () {
+      //testar se o pico foi criado e retornar true
+    });
+
+}
