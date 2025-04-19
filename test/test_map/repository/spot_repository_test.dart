8c2d021dd1d401bf652e1bcc4d87539c4883ffc4
Gabriel Pires <dasaybarreto724@gmail.com>
2025-04-19 14:07:00 -0300

    adicionando testes de lançamento de exceção

--- test/test_map/repository/spot_repository_test.dart
+++ test/test_map/repository/spot_repository_test.dart
@@ -1,7 +1,3 @@
-
-
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demopico/features/mapa/data/models/pico_model.dart';
 import 'package:demopico/features/mapa/data/repository/firebase_repository_map.dart';
@@ -10,7 +6,6 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'spot_repository_test.mocks.dart';
-
 
 //cirando anotação para criar o mock
 @GenerateNiceMocks([
@@ -21,17 +16,13 @@ import 'spot_repository_test.mocks.dart';
   MockSpec<QuerySnapshot<Map<String, dynamic>>>(),
   MockSpec<QueryDocumentSnapshot<Map<String, dynamic>>>(),
 ])
-
-
-
 void main() {
-  group("Testando o firebase repository dos métodos hard do pico", () {
-
-    //variáveis mockadas para utilizar nos testes 
+  group("Deve testar a interação com o firebase ", () {
+    //variáveis mockadas para utilizar nos testes
     late MockFirebaseFirestore mockFirestore;
-    late MockCollectionReference mockCollection; 
-    late MockDocumentReference mockDocRef; 
-    late MockDocumentSnapshot mockDocSnapshot; 
+    late MockCollectionReference mockCollection;
+    late MockDocumentReference mockDocRef;
+    late MockDocumentSnapshot mockDocSnapshot;
     late FirebaseRepositoryMap repositoryMap;
     late MockQuerySnapshot mockQuerySnapshot;
     late MockQueryDocumentSnapshot mockQueryDocSnapshot;
@@ -53,25 +44,27 @@ void main() {
       picoName: "Pico Legal",
     );
     final testPicoNotaNova = testPico.copyWith(nota: 5, numeroAvaliacoes: 11);
-    
-    //setando os mocks para utilizar nos testes
-    setUp((){
-       mockFirestore = MockFirebaseFirestore();
-       mockCollection = MockCollectionReference();
-       mockDocSnapshot = MockDocumentSnapshot();
-       mockDocRef = MockDocumentReference();  
-       mockQuerySnapshot = MockQuerySnapshot();
-       mockQueryDocSnapshot = MockQueryDocumentSnapshot();
 
-       repositoryMap = FirebaseRepositoryMap(mockFirestore);
+    //setando os mocks para utilizar nos testes
+    setUp(() {
+      mockFirestore = MockFirebaseFirestore();
+      mockCollection = MockCollectionReference();
+      mockDocSnapshot = MockDocumentSnapshot();
+      mockDocRef = MockDocumentReference();
+      mockQuerySnapshot = MockQuerySnapshot();
+      mockQueryDocSnapshot = MockQueryDocumentSnapshot();
+
+      repositoryMap = FirebaseRepositoryMap(mockFirestore);
     });
-    
+
     //testando criar pico
-    test("deve criar um pico com sucesso e retornar a instancia como pico", () async  {
-      // criando fluxo que ocorrerá se der tudo certo 
+    test("deve criar um pico com sucesso e retornar a instancia como pico",
+        () async {
+      // criando fluxo que ocorrerá se der tudo certo
       when(mockFirestore.collection("spots")).thenReturn(mockCollection);
-      when(mockCollection.add(testPico.toJson())).thenAnswer((_) async => mockDocRef);
-      when(mockDocSnapshot.id).thenReturn("1");  
+      when(mockCollection.add(testPico.toJson()))
+          .thenAnswer((_) async => mockDocRef);
+      when(mockDocSnapshot.id).thenReturn("1");
       when(mockDocSnapshot.exists).thenReturn(true);
       when(mockDocRef.get()).thenAnswer((_) async => mockDocSnapshot);
       when(mockDocSnapshot.data()).thenReturn(testPico.toJson());
@@ -79,17 +72,16 @@ void main() {
       //chamando o método
       final result = await repositoryMap.createSpot(testPico);
 
-      //verificando os resultados 
+      //verificando os resultados
       expect(result, isA<PicoModel>());
       expect(result!.id, "1");
       expect(result.picoName, "Pico Legal");
-      
     });
 
-    test("deve listar os picos salvos", () async{
+    test("deve pegar todos os picos do firebase", () async {
       when(mockFirestore.collection("spots")).thenReturn(mockCollection);
       when(mockCollection.get(any)).thenAnswer((_) async => mockQuerySnapshot);
-      when(mockQuerySnapshot.docs).thenReturn([mockQueryDocSnapshot]); 
+      when(mockQuerySnapshot.docs).thenReturn([mockQueryDocSnapshot]);
       when(mockQueryDocSnapshot.data()).thenReturn(testPico.toJson());
       when(mockQueryDocSnapshot.id).thenReturn("1");
 
@@ -104,7 +96,8 @@ void main() {
       when(mockDocSnapshot.data()).thenReturn(testPicoNotaNova.toJson());
       when(mockDocSnapshot.exists).thenReturn(true);
       when(mockDocSnapshot.reference).thenReturn(mockDocRef);
-      when(mockDocRef.update({"nota": 5.0, 'avaliacoes': 11})).thenAnswer((_) async {});
+      when(mockDocRef.update({"nota": 5.0, 'avaliacoes': 11}))
+          .thenAnswer((_) async {});
 
       final result = await repositoryMap.salvarNota(testPicoNotaNova);
       expect(result, isA<PicoModel>());
@@ -112,34 +105,62 @@ void main() {
       expect(result.numeroAvaliacoes, 11);
     });
 
-    
-  test("deve deletar um pico", () async{
-    when(mockFirestore.collection("spots")).thenReturn(mockCollection);
-    when(mockCollection.doc('1')).thenReturn(mockDocRef);
-    when(mockDocRef.delete()).thenAnswer((_) async {});
-    when(mockDocRef.get(any)).thenAnswer((_) async {
-      final snapshotDeleted = MockDocumentSnapshot();
-      when(snapshotDeleted.exists).thenReturn(false);
-      return snapshotDeleted;
+    test("deve deletar um pico", () async {
+      when(mockFirestore.collection("spots")).thenReturn(mockCollection);
+      when(mockCollection.doc('1')).thenReturn(mockDocRef);
+      when(mockDocRef.delete()).thenAnswer((_) async {});
+      when(mockDocRef.get(any)).thenAnswer((_) async {
+        final snapshotDeleted = MockDocumentSnapshot();
+        when(snapshotDeleted.exists).thenReturn(false);
+        return snapshotDeleted;
+      });
+
+      await repositoryMap.deleteSpot("1");
+
+      verify(mockDocRef.delete()).called(1);
+      expect(mockDocRef.delete(), completes);
+
+      //verificando se o pico foi deletado
+      final snapshotDel = await mockDocRef.get();
+      expect(snapshotDel.exists, isFalse);
     });
 
-    await repositoryMap.deleteSpot("1");
-    
-    verify(mockDocRef.delete()).called(1);
-    expect(mockDocRef.delete(), completes);
+    test('deve lançar uma exceção quando o documento não existir depois da criação', () async {
+      when(mockFirestore.collection('spots')).thenReturn(mockCollection);
+      when(mockCollection.add(testPico.toJson())).thenAnswer((_) async => mockDocRef);
+      when(mockDocSnapshot.exists).thenReturn(false);
+      when(mockDocSnapshot.data()).thenReturn(null);
+      when(mockDocRef.get()).thenAnswer((_) async => mockDocSnapshot);
 
-    //verificando se o pico foi deletado
-    final snapshotDel = await mockDocRef.get();
-    expect(snapshotDel.exists, isFalse);
+      expect(() => repositoryMap.createSpot(testPico), throwsException);
+    });
+
+     test('deve lançar uma exceção quando o firebase lançar um erro', () async {
+      when(mockFirestore.collection('spots')).thenReturn(mockCollection);
+      when(mockCollection.add(testPico.toJson())).thenThrow(FirebaseException(plugin: 'test'));
+
+      expect(() => repositoryMap.createSpot(testPico), throwsException);
+    });
+
+    test('deve lança uma exceção quando o documento existir mais os dados forem nulos', () async {
+      when(mockFirestore.collection('spots')).thenReturn(mockCollection);
+      when(mockCollection.add(testPico.toJson())).thenAnswer((_) async => mockDocRef);
+      when(mockDocSnapshot.exists).thenReturn(true);//domento existe
+      when(mockDocSnapshot.data()).thenReturn(null);//dados nulos
+      when(mockDocRef.get()).thenAnswer((_) async => mockDocSnapshot);
+
+      expect(() => repositoryMap.createSpot(testPico), throwsException);
+    });
+
+    test('deve saber lidar com erros inesperados', () async {
+      when(mockFirestore.collection('spots')).thenReturn(mockCollection);
+      when(mockCollection.add(testPico.toJson())).thenThrow(Exception('Unexpected error'));
+
+      expect(() => repositoryMap.createSpot(testPico), throwsException);
+    });
   });
-  });
 
 
-  
 }
 
 
-
-
-
- 
