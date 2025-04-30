6f93e5d2ea867d9b859376c64ba173edd3da5c46
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-04-30 13:12:34 -0300

    criando hub repo

--- lib/features/hub/infra/interfaces/i_hub_repository.dart
+++ lib/features/hub/infra/interfaces/i_hub_repository.dart
@@ -1,8 +1,7 @@
 import 'package:demopico/features/hub/domain/entities/communique.dart';
-
 abstract class IHubRepository {
-  Future<void> createCommunique(Communique communique);
+  Future<void> postHubCommuniqueToFirebase(String text, dynamic type);
+  Future<List<Communique>> getAllCommuniques();
   Future<void> updateCommunique(Communique communique);
-  Future<void> deleteCommunique(String id);
-  Future<List<Communique>> listCommuniques();
+  Future<void> deleteCommunique(String communiqueId);
 }
