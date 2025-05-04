dbe24aca324ef5865c6348bc4f3d505ed40da868
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-04 10:15:43 -0300

    interface e implementação

---
+++ lib/features/user/domain/interfaces/i_user_database_repository.dart
@@ -0,0 +1,8 @@
+import 'package:demopico/features/user/domain/models/user.dart';
+
+abstract class IUserDatabaseRepository{
+  Future<void> addUserDetails(UserM newUser);
+  Future<UserM?> getUserDetails(String uid);
+  Future<String?> getUserIDByVulgo(String vulgo);
+  Future<String?> getEmailByUserID(String uid);
+}
