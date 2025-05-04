a6dbceed363cad9bc4f292852a4c315c7a73ace2
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-04 10:17:17 -0300

    interface auth service

---
+++ lib/features/user/domain/interfaces/i_user_auth_service.dart
@@ -0,0 +1,5 @@
+abstract class IUserAuthService {
+  Future<bool> signUp(String inputName, String inputEmail, String password, bool isColetivo);
+  Future<bool> loginByEmail(String email, String password);
+  Future<void> logout();
+}
