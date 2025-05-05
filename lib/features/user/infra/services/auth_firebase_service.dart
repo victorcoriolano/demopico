64543c58455cc8a3188dc67b35e193aea573097a
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-05 09:52:46 -0300

    quase finalizando auth service

--- lib/features/user/infra/services/auth_firebase_service.dart
+++ lib/features/user/infra/services/auth_firebase_service.dart
@@ -1,22 +1,22 @@
 import 'package:demopico/features/user/domain/interfaces/i_user_auth_service.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
-import 'package:demopico/features/user/infra/repositories/auth_enum.dart';
-import 'package:demopico/features/user/infra/repositories/sign_methods.dart';
 import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
-class AuthFirebaseService implements IUserAuthService{
-  
+class AuthFirebaseService implements IUserAuthService {
   static AuthFirebaseService? _authFirebaseService;
 
- static AuthFirebaseService get getInstace{
-    _authFirebaseService ??= AuthFirebaseService(auth: FirebaseAuth.instance, userDatabaseRepositoryIMP: UserFirebaseRepository.getInstance);
+  static AuthFirebaseService get getInstace {
+    _authFirebaseService ??= AuthFirebaseService(
+        auth: FirebaseAuth.instance,
+        userDatabaseRepositoryIMP: UserFirebaseRepository.getInstance);
     return _authFirebaseService!;
   }
 
-  AuthFirebaseService({required this.auth, required this.userDatabaseRepositoryIMP});
+  AuthFirebaseService(
+      {required this.auth, required this.userDatabaseRepositoryIMP});
 
   final FirebaseAuth auth;
   final IUserDatabaseRepository userDatabaseRepositoryIMP;
@@ -28,29 +28,6 @@ class AuthFirebaseService implements IUserAuthService{
   UserM? _localUser;
   UserM? get getLocalUser => _localUser;
 
-  String todayDate =
-      '${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}';
-
-  //transforma dados do firebase em dados na model
-  //cria um user model de acordo com a nova conta criada
-  UserM? userFromFirebaseUser(User? user) {
-    return user != null
-        ? UserM(
-            name: user.displayName,
-            email: user.email,
-            description: 'Edite para atualizar sua bio',
-            id: user.uid,
-            picosAdicionados: '0',
-            picosSalvos: '0',
-            location: null,
-            conexoes: '0',
-            dob: todayDate,
-            authEnumState: AuthEnumState.notDetermined,
-            pictureUrl: null,
-          )
-        : null;
-  }
-
   Stream<User?> getAuthStateChanges() {
     return auth.authStateChanges();
   }
@@ -60,71 +37,38 @@ class AuthFirebaseService implements IUserAuthService{
     return auth.currentUser;
   }
 
-
+  @override
   Future<bool> signUp(String inputName, String inputEmail, String password, bool isColetivo) async {
     try {
-      UserCredential authResult = await auth.createUserWithEmailAndPassword(
-          email: inputEmail, password: password);
+      UserCredential authResult = await auth.createUserWithEmailAndPassword(email: inputEmail, password: password);
       User? signedInUser = authResult.user;
-      UserM? localUser = userFromFirebaseUser(signedInUser);
-      if(localUser == null) throw Exception("O usuario não pode ser nulo para salvar no banco");
+
+      if (signedInUser == null) throw Exception("O usuario não pode ser criado");
+
+      UserM localUser = UserM.userFromFirebaseAuthUser(signedInUser, inputEmail, isColetivo);
+
       await userDatabaseRepositoryIMP.addUserDetails(localUser);
-
-
-      //verifica se ele é válido, seta os estados de auth e cria o user no db
-      if (signedInUser != null && signedInUser.uid.isNotEmpty) {
-        signedInUser.updateDisplayName(inputName);
-        //cria um user model de acordo com a nova conta criada
-        UserM? localUser = userFromFirebaseUser(signedInUser);
-        //atualização da model temporaria
-        if (localUser != null) {
-          localUser.isColetivo = isColetivo;
-
-          localUser.signMethod = SignMethods.email;
-          localUser.email = inputEmail;
-          localUser.name = inputName;
-          //atualização da model local e adiciona o user no repository
-          _localUser = localUser;
-          await dbService
-              .addUserDetailsToFirestore(newUser: _localUser!);
-             
-          return true;
-        } else {
-          print('local user null');
-        }
-      } else {
-        print('signedinuser null');
-      }
-      return false;
+      return true;
     } catch (e) {
-      print('EXCEPTION NO TRY DO METODO SIGN UP');
-      print(e);
+      Exception("Erro ao criar usuario");
       return false;
     }
   }
 
+  @override
   Future<bool> loginByEmail(String email, String password) async {
     try {
-      final authResult = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
+      final authResult = await auth.signInWithEmailAndPassword(email: email, password: password);
       User? signedUser = authResult.user;
-
-      if (signedUser != null) {
-        UserM? firestoreUser =
-            await dbService.getUserDetailsFromFirestore(signedUser.uid);
-        auth.currentUser!.updateDisplayName(firestoreUser!.name);
-        auth.currentUser!.updatePhotoURL(firestoreUser.pictureUrl);
-        auth.userChanges();
-        return true;
-      }
-      return false;
+      if (signedUser == null)throw Exception("Não foi possível fazer o login, usuario não encontrado");
+      return true;
     } catch (e) {
-      print(e.toString());
-      print('erro no login authservice: $e');
+      Exception("Erro ao tentar realizar o login, tente novamente");
       return false;
     }
   }
 
+  @override
   Future<void> logout() async {
     try {
       await auth.signOut();
