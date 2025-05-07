2bad76a53e33754cd3f31f848967e6b5e2735a07
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-07 12:11:36 -0300

    instancia unica home provider

--- lib/features/home/provider/home_provider.dart
+++ lib/features/home/provider/home_provider.dart
@@ -6,6 +6,13 @@ class HomeProvider extends ChangeNotifier {
   ListarComunicado listarComunicado;
   HomeProvider({required this.listarComunicado});
   
+  static HomeProvider? _homeProvider;
+
+  static HomeProvider get getInstance{
+    _homeProvider ??= HomeProvider(listarComunicado: ListarComunicado.getInstance);
+    return _homeProvider!;
+  }
+
     List<Communique?> _allCommuniques = [];
 
     List<Communique?> get allCommuniques  => _allCommuniques;
