b6c637d1990bcc054d76bd5457387219643f05c9
selingindev <88356338+selingindev@users.noreply.github.com>
2025-05-08 15:27:35 -0300

    adicionando teste de listar spots favoritos

--- test/test_map/services/service_favorite_spot_test.dart
+++ test/test_map/services/service_favorite_spot_test.dart
@@ -6,32 +6,50 @@ import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../use_cases/create_spot_uc_test.dart';
+
 void main() {
-  group("Deve testar o serviço de salvar spot", (){
+  group("Deve testar o serviço de salvar spot", () {
     late FakeFirebaseFirestore fakeFirestore;
-    setUp(()async {
+    setUp(() async {
       fakeFirestore = FakeFirebaseFirestore();
       //criando um spot fake pra referenciar no pico favorito
-    
+      await fakeFirestore
+          .collection("spots")
+          .doc(testPico.id)
+          .set(testPico.toJson());
     });
 
     test("Deve salvar um spot dos favoritos", () async {
-      final service =  FirebaseFavoriteSpotService(fakeFirestore);
+      final service = FirebaseFavoriteSpotService(fakeFirestore);
       //criando um spot fake pra referenciar no pico favorito
-      await fakeFirestore.collection("spots").doc(testPico.id).set(testPico.toJson());
-      
-      final result = await service.saveSpot(PicoFavorito(idPico: "1", idUsuario: "1"));
-      
-      
+
+      final result =
+          await service.saveSpot(PicoFavorito(idPico: "1", idUsuario: "1"));
+
       expect(result.idPico, "1");
       expect(result, isA<PicoFavoritoModel>());
 
-      final savedData = await fakeFirestore.collection("picosFavoritados").get().then((docs) => docs.docs.first.data());
+      final savedData = await fakeFirestore
+          .collection("picosFavoritados")
+          .get()
+          .then((docs) => docs.docs.first.data());
       expect(savedData['idUsuario'], "1");
-      expect(savedData['idPico'] , isA<DocumentReference>()); 
+      expect(savedData['idPico'], isA<DocumentReference>());
     });
-    test("Deve retornar um lista de spot salvo", (){});
-    test("Deve deletar um spot salvo", (){});
-    test("Deve retornar limpar a lista de spots", (){});
+
+    test("Deve retornar um lista de spot salvo", () async {
+      final service = FirebaseFavoriteSpotService(fakeFirestore);
+      String idUser = "1";
+      await service.saveSpot(PicoFavorito(idPico: "1", idUsuario: "1"));
+      await service.saveSpot(PicoFavorito(idPico: "2", idUsuario: "1"));
+
+      final result = await service.listFavoriteSpot(idUser);
+
+      expect(result, isA<List<PicoFavoritoModel>>());
+      expect(result[1].idPico, equals("2"));
+      expect(result.length, equals(2));
+    });
+    test("Deve deletar um spot salvo", () {});
+    test("Deve retornar limpar a lista de spots", () {});
   });
 }
