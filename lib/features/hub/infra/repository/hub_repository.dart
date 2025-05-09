77b1f89ebf6cbe3955032d140470905cf967de4c
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-09 01:56:33 -0300

    arrumando hub passando user

--- lib/features/hub/infra/repository/hub_repository.dart
+++ lib/features/hub/infra/repository/hub_repository.dart
@@ -4,7 +4,11 @@ import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/hub/domain/interfaces/i_hub_repository.dart';
 import 'package:demopico/features/hub/domain/interfaces/i_hub_service.dart';
 import 'package:demopico/features/hub/infra/services/hub_service.dart';
-import 'package:demopico/features/user/infra/services/user_service.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_auth_service.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:demopico/features/user/infra/services/user_auth_firebase_service.dart';
 import 'package:flutter/foundation.dart';
 
 class HubRepository implements IHubRepository {
@@ -12,23 +16,26 @@ class HubRepository implements IHubRepository {
   static HubRepository? _hubRepository;
 
   static HubRepository get getInstance{
-    _hubRepository ??= HubRepository(userService: UserService.getInstance, iHubService: HubService(firestore: Firestore()   ).getInstance);
+    _hubRepository ??= HubRepository(userAuthServiceIMP: UserAuthFirebaseService.getInstance, hubServiceIMP: HubService.getInstance, userDatabaseRepositoryIMP: UserFirebaseRepository.getInstance);
     return _hubRepository!;
   }
 
-  final UserService userService;
-  final IHubService iHubService;
+  final IUserDatabaseRepository  userDatabaseRepositoryIMP;
+  final IUserAuthService userAuthServiceIMP;
+  final IHubService hubServiceIMP;
 
   HubRepository({
-    required this.userService,
-    required this.iHubService,
+    required this.userDatabaseRepositoryIMP,
+    required this.userAuthServiceIMP,
+    required this.hubServiceIMP,
   });
 
   // Criar comunicado
   @override
   Future<void> postHubCommuniqueToFirebase(String text, dynamic type) async {
     try {
-      final user = await userService.getCurrentUser();
+      String userID = userAuthServiceIMP.currentUser();
+      UserM? user = await userDatabaseRepositoryIMP.getUserDetails(userID);
       final id = FirebaseFirestore.instance.collection('comunicados').doc().id;
 
       if (user != null) {
@@ -43,7 +50,7 @@ class HubRepository implements IHubRepository {
           likedBy: [],
           type: type,
         );
-        await iHubService.createCommunique(newCommunique);
+        await hubServiceIMP.createCommunique(newCommunique);
       } else {
         if (kDebugMode) print('Usuário não encontrado.');
       }
@@ -60,7 +67,7 @@ class HubRepository implements IHubRepository {
   @override
   Future<List<Communique>> getAllCommuniques() async {
     try {
-      return await iHubService.listCommuniques();
+      return await hubServiceIMP.listCommuniques();
     } catch (e) {
       if (kDebugMode) print(e);
       return [];
@@ -71,7 +78,7 @@ class HubRepository implements IHubRepository {
   @override
   Future<void> updateCommunique(Communique communique) async {
     try {
-      await iHubService.updateCommunique(communique);
+      await hubServiceIMP.updateCommunique(communique);
     } catch (e) {
       if (kDebugMode) print('Erro ao atualizar comunicado: $e');
     }
@@ -81,7 +88,7 @@ class HubRepository implements IHubRepository {
   @override
   Future<void> deleteCommunique(String communiqueId) async {
     try {
-      await iHubService.deleteCommunique(communiqueId);
+      await hubServiceIMP.deleteCommunique(communiqueId);
     } catch (e) {
       if (kDebugMode) print('Erro ao deletar comunicado: $e');
     }
