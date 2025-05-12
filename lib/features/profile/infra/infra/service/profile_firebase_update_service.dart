2530940c6b22a64cb04516e034ef7ba88c85643b
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-12 17:32:42 -0300

    implementando update service

--- lib/features/profile/infra/infra/service/profile_firebase_update_service.dart
+++ lib/features/profile/infra/infra/service/profile_firebase_update_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demopico/features/external/datasources/firestore.dart';
 import 'package:demopico/features/profile/infra/domain/interfaces/profile_database_update_service.dart';
 
@@ -10,84 +11,34 @@ class ProfileFirebaseUpdateService implements ProfileDatabaseUpdateService {
   }
 
   ProfileFirebaseUpdateService();
-
   final Firestore firestore = Firestore();
 
   @override
   void atualizarBio(String newBio, String uid) async {
-    
     await firestore.getInstance
         .collection('users')
         .doc(uid)
         .update({'description': newBio});
-    
-  
   }
 
   @override
   void atualizarContribuicoes(String uid) async {
-    // TODO: implement atualizarContribuicoes
+    await firestore.getInstance.collection('users').doc(uid).update({
+      'picosAdicionados': FieldValue.increment(1),
+    });
   }
 
   @override
   void atualizarFoto(String newImg, String uid) async {
-    // TODO: implement atualizarFoto
+    await firestore.getInstance
+        .collection('users')
+        .doc(uid)
+        .update({'pictureUrl': newImg});
   }
 
   @override
   void atualizarSeguidores(String uid) async {
-    // TODO: implement atualizarSeguidores
+    await firestore.getInstance
+        .collection('users').doc(uid).update({'conexoes': FieldValue.increment(1)});
   }
 }
-
-/*
-  Future<void> atualizarContribuicoes() async {
-    final id = auth.currentUser?.uid ?? 'User not found';
-    final reference = firestore.collection("users");
-
-    if (id != 'User not found') { 
-      try {
-        await reference.doc(id).update({
-          'picosAdicionados': FieldValue.increment(1),
-        });
-        print('Contribuição atualizada com sucesso!');
-      } catch (e) {
-        print('Erro ao atualizar contribuições: $e');
-      }
-    } else {
-      print('Usuário não encontrado.');
-    }
-  }
-
-//////////////////////////
-///////////////////////////
-/////////////////////////////
-  Future<void> updateUserBioInFirebase(String newBio) async {
-    String uid = auth.currentUser!.uid;
-    try {
-      await firestore
-          .collection('users')
-          .doc(uid)
-          .update({'description': newBio});
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
-    }
-  }
-
-  Future<void> updateUserImgInFirebase(String newImg) async {
-    print("OIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII ${newImg}");
-    String uid = auth.currentUser!.uid;
-    try {
-      await firestore
-          .collection('users')
-          .doc(uid)
-          .update({'pictureUrl': newImg});
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
-    }
-  }
-  */
