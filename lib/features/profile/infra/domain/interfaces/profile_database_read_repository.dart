a0826ebeeccf80b56a7001e51d7200fd2d010fa0
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-12 14:17:13 -0300

    interfaces para conseguir ler o dado do user

---
+++ lib/features/profile/infra/domain/interfaces/profile_database_read_repository.dart
@@ -0,0 +1,8 @@
+import 'package:demopico/features/user/domain/models/user.dart';
+
+abstract class ProfileDatabaseReadRepository{  
+  Future<String?> pegarContribuicoes(UserM user);
+  Future<String?> pegarSeguidores(UserM user);
+  Future<String?> pegarBio(UserM user);
+  Future<String?> pegarFoto(UserM user);
+}
