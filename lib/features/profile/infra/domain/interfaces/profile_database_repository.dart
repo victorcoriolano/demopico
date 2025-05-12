26666234868fadc2f382f2640ee7c03d1f3a1aa2
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-12 14:16:40 -0300

    renomenado para implementar a logica de interfcaes segregadas read e update

--- lib/features/profile/infra/domain/interfaces/profile_database_repository.dart
+++
@@ -1,7 +0,0 @@
-abstract class ProfileDatabaseService{
-  Future<String?> atualizarContribuicoes();
-  Future<String?> atualizarSeguidores();
-  Future<String?> atualizarBio(String newBio);
-  Future<String?> atualizarFoto(String newImg);
-  // pegarcONTRIbicoes
-}
--- lib/features/profile/infra/domain/interfaces/profile_database_service.dart
+++
@@ -1,7 +0,0 @@
-abstract class ProfileDatabaseService{
-  Future<String?> atualizarContribuicoes(String uid);
-  Future<String?> atualizarSeguidores(String uid);
-  Future<String?> atualizarBio(String newBio, String uid);
-  Future<String?> atualizarFoto(String newImg, String uid);
-  // pegarcONTRIbicoes
-}
