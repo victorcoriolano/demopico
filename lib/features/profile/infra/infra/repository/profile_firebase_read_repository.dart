88acbd61a37b1bb1332cc250bebed4b8f63d3f17
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-13 13:16:41 -0300

    repository read implementando

--- lib/features/profile/infra/infra/repository/profile_firebase_read_repository.dart
+++ lib/features/profile/infra/infra/repository/profile_firebase_read_repository.dart
@@ -1,42 +1,56 @@
+import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/features/profile/infra/domain/interfaces/i_profile_database_read_repository.dart';
-import 'package:demopico/features/user/domain/interfaces/i_user_database_service.dart';
-import 'package:demopico/features/user/infra/services/user_firebase_service.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_auth_service.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:demopico/features/user/infra/services/user_auth_firebase_service.dart';
 
-class ProfileFirebaseReadRepository implements IProfileDatabaseReadRepository{
-
+class ProfileFirebaseReadRepository implements IProfileDatabaseReadRepository {
   static ProfileFirebaseReadRepository? _profileFirebaseReadRepository;
 
-  static ProfileFirebaseReadRepository get getInstance{
-    _profileFirebaseReadRepository ??= ProfileFirebaseReadRepository(userDatabaseServiceIMP: UserFirebaseService.getInstance);
+  static ProfileFirebaseReadRepository get getInstance {
+    _profileFirebaseReadRepository ??= ProfileFirebaseReadRepository(
+        userAuthServiceIMP: UserAuthFirebaseService.getInstance,
+        userDatabaseRepository: UserFirebaseRepository.getInstance);
     return _profileFirebaseReadRepository!;
   }
 
-  ProfileFirebaseReadRepository({required this.userDatabaseServiceIMP});
+  ProfileFirebaseReadRepository(
+      {required this.userAuthServiceIMP, required this.userDatabaseRepository});
 
-  final IUserDatabaseService userDatabaseServiceIMP;
-  
+  final IUserAuthService userAuthServiceIMP;
+  final IUserDatabaseRepository userDatabaseRepository;
 
   @override
-  Future<String?> pegarBio() async {
-     await userDatabaseServiceIMP.;
+  Future<String> pegarBio() async {
+    String uid = userAuthServiceIMP.currentUser();
+    UserM? user = await userDatabaseRepository.getUserDetails(uid);
+    if (user == null) throw UserNotFoundFailure();
+    return user.description!;
   }
 
   @override
-  Future<String?> pegarContribuicoes() async {
-    // TODO: implement pegarContribuicoes
-    throw UnimplementedError();
+  Future<String> pegarContribuicoes() async {
+    String uid = userAuthServiceIMP.currentUser();
+    UserM? user = await userDatabaseRepository.getUserDetails(uid);
+    if (user == null) throw UserNotFoundFailure();
+    return user.picosSalvos!;
   }
 
   @override
-  Future<String?> pegarFoto() async {
-    // TODO: implement pegarFoto
-    throw UnimplementedError();
+  Future<String> pegarFoto() async {
+    String uid = userAuthServiceIMP.currentUser();
+    UserM? user = await userDatabaseRepository.getUserDetails(uid);
+    if (user == null) throw UserNotFoundFailure();
+    return user.pictureUrl!;
   }
 
   @override
-  Future<String?> pegarSeguidores() async {
-    // TODO: implement pegarSeguidores
-    throw UnimplementedError();
+  Future<String> pegarSeguidores() async {
+    String uid = userAuthServiceIMP.currentUser();
+    UserM? user = await userDatabaseRepository.getUserDetails(uid);
+    if (user == null) throw UserNotFoundFailure();
+    return user.conexoes!;
   }
-
-} 
+}
