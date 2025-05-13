34fab9cce61d749e2d64a59711017bbd6ca20794
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-13 12:34:59 -0300

    identificador de interface

--- lib/features/profile/infra/domain/interfaces/profile_database_read_repository.dart
+++ lib/features/profile/infra/domain/interfaces/profile_database_read_repository.dart
@@ -1,6 +1,6 @@
 import 'package:demopico/features/user/domain/models/user.dart';
 
-abstract class ProfileDatabaseReadRepository{  
+abstract class IProfileDatabaseReadRepository{  
   Future<String?> pegarContribuicoes(UserM user);
   Future<String?> pegarSeguidores(UserM user);
   Future<String?> pegarBio(UserM user);
