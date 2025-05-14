597e459ac71147a958335f2895ce9ff856864788
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-14 11:25:31 -0300

    instancia unica nos casos de uso

--- lib/features/mapa/domain/usecases/create_spot_uc.dart
+++ lib/features/mapa/domain/usecases/create_spot_uc.dart
@@ -1,17 +1,23 @@
+import 'package:demopico/features/mapa/data/services/firebase_spots_service.dart';
 import 'package:demopico/features/mapa/domain/interfaces/i_spot_repository.dart';
 import 'package:demopico/features/mapa/domain/models/pico_model.dart';
 
 class CreateSpotUc {
-  final ISpotRepository spotRepository;
+  static CreateSpotUc? _createSpotUc;
+  static CreateSpotUc get getInstance {
+    _createSpotUc ??=
+        CreateSpotUc(spotRepositoryIMP: FirebaseSpotsService.getInstance);
+    return _createSpotUc!;
+  }
 
-  CreateSpotUc(this.spotRepository);
+  final ISpotRepository spotRepositoryIMP;
+
+  CreateSpotUc({required this.spotRepositoryIMP});
 
   Future<PicoModel?> createSpot(PicoModel pico) async {
-
-    
     try {
-      final picoCriado = await spotRepository.createSpot(pico);
-      if(picoCriado != null){
+      final picoCriado = await spotRepositoryIMP.createSpot(pico);
+      if (picoCriado != null) {
         return picoCriado;
       }
       throw Exception("Pico retornou null");
