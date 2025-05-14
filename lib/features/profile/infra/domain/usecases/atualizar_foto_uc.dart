1702c730b118ac384fcdde64ad1591419014526f
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-13 22:39:25 -0300

    atualizar foto uc

---
+++ lib/features/profile/infra/domain/usecases/atualizar_foto_uc.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demopico/core/common/errors/failure_server.dart';
+import 'package:demopico/features/profile/infra/domain/interfaces/i_profile_database_read_repository.dart';
+import 'package:demopico/features/profile/infra/domain/interfaces/i_profile_database_update_repository.dart';
+import 'package:demopico/features/profile/infra/infra/repository/profile_firebase_read_repository.dart';
+import 'package:demopico/features/profile/infra/infra/repository/profile_firebase_update_repository.dart';
+import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:flutter/foundation.dart';
+
+class AtualizarFotoUc {
+  static AtualizarFotoUc? _instance;
+  static AtualizarFotoUc get getInstance {
+    _instance ??= AtualizarFotoUc(
+      profileDatabaseReadRepositoryIMP: ProfileFirebaseReadRepository.getInstance,
+      profileDatabaseUpdateRepositoryIMP: ProfileFirebaseUpdateRepository.getInstance,
+    );
+    return _instance!;
+  }
+
+  AtualizarFotoUc({
+    required this.profileDatabaseReadRepositoryIMP,
+    required this.profileDatabaseUpdateRepositoryIMP,
+  });
+
+  final IProfileDatabaseReadRepository profileDatabaseReadRepositoryIMP;
+  final IProfileDatabaseUpdateRepository profileDatabaseUpdateRepositoryIMP;
+
+  void atualizar(String newFoto, UserM user) {
+    try {
+      if (user.id == null) throw UserNotFoundFailure();
+      profileDatabaseUpdateRepositoryIMP.atualizarFoto(newFoto, user);
+    } on FirebaseException catch (e) {
+      if (kDebugMode) print(e);
+      throw FirebaseFailure();
+    } catch (e) {
+      if (kDebugMode) print('Erro inesperado: $e');
+      throw GenericErrorFailure();
+    }
+  }
+
+  Future<String> pegar() async {
+    try {
+      return await profileDatabaseReadRepositoryIMP.pegarFoto();
+    } on FirebaseException catch (e) {
+      if (kDebugMode) print(e);
+      throw FirebaseFailure();
+    } catch (e) {
+      if (kDebugMode) print('Erro inesperado: $e');
+      throw GenericErrorFailure();
+    }
+  }
+}
