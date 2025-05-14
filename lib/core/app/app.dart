b9c31fbf38dd8e19534e907513c2aafc780d7ce4
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-14 11:00:50 -0300

    implementando getinstance nos provider do mapa

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,74 +1,51 @@
 import 'package:demopico/core/app/home_page.dart';
-import 'package:demopico/features/external/datasources/firestore.dart';
 import 'package:demopico/features/home/provider/home_provider.dart';
 import 'package:demopico/features/hub/domain/usecases/listar_comunicados_uc.dart';
 import 'package:demopico/features/hub/domain/usecases/postar_comunicado_uc.dart';
 import 'package:demopico/features/hub/presentation/pages/hub_page.dart';
 import 'package:demopico/features/hub/presentation/providers/hub_provider.dart';
-import 'package:demopico/features/mapa/domain/usecases/comment_spot_uc.dart';
-import 'package:demopico/features/mapa/domain/usecases/pick_image_uc.dart';
-import 'package:demopico/features/mapa/domain/usecases/save_history_spot_uc.dart';
-import 'package:demopico/features/mapa/domain/usecases/save_image_uc.dart';
 import 'package:demopico/features/mapa/presentation/controllers/add_pico_controller.dart';
 import 'package:demopico/features/mapa/presentation/controllers/comment_controller.dart';
 import 'package:demopico/features/mapa/presentation/controllers/historico_controller.dart';
 import 'package:demopico/features/mapa/presentation/controllers/map_controller.dart';
 import 'package:demopico/features/mapa/presentation/controllers/spot_controller.dart';
 import 'package:demopico/features/mapa/presentation/pages/map_page.dart';
-import 'package:demopico/features/mapa/data/services/image_picker_service.dart';
-import 'package:demopico/features/mapa/data/services/firebase_files_service.dart';
-import 'package:demopico/features/mapa/data/services/firebase_comment_service.dart';
-import 'package:demopico/features/mapa/data/services/local_storage_service.dart';
 import 'package:demopico/features/user/infra/services/user_auth_firebase_service.dart';
 import 'package:demopico/features/user/presentation/controllers/auth_user_provider.dart';
-import 'package:demopico/core/common/inject_dependencies.dart';
 import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 
 class MyAppWidget extends StatelessWidget {
-  MyAppWidget({super.key});
+  const MyAppWidget({super.key});
 
-  final Firestore firestoreInstance = Firestore();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => AuthUserProvider.getInstance
-        ),
+        ChangeNotifierProvider(create: (_) => AuthUserProvider.getInstance),
         StreamProvider(
-          create: (_) => UserAuthFirebaseService.getInstance.getAuthStateChanges(),
+          create: (_) =>
+              UserAuthFirebaseService.getInstance.getAuthStateChanges(),
           initialData: null,
         ),
+        ChangeNotifierProvider(create: (_) => AddPicoProvider.getInstance),
         ChangeNotifierProvider(
-            create: (_) => AddPicoProvider(
-                PickImageUC(ImagePickerService()),
-                SaveImageUC(FirebaseFilesService(
-                    FirebaseStorage.instance)))),
-        ChangeNotifierProvider(create: (_) => MapControllerProvider()),
+            create: (_) => MapControllerProvider.getInstance),
         ChangeNotifierProvider(
-            create: (_) => serviceLocator<SpotControllerProvider>()),
+            create: (_) => SpotControllerProvider.getInstance),
         ChangeNotifierProvider(create: (_) => UserDatabaseProvider.getInstance),
+        ChangeNotifierProvider(create: (_) => HistoricoController.getInstance),
         ChangeNotifierProvider(
-            create: (_) =>
-                HistoricoController(SaveHistoryUc(LocalStorageService()))),
-         ChangeNotifierProvider(
           create: (_) => HubProvider(
             postarComunicado: PostarComunicado.getInstance,
             listarComunicado: ListarComunicado.getInstance,
           ),
         ),
-         ChangeNotifierProvider(
-            create: (_) => HomeProvider.getInstance),
-        ChangeNotifierProvider(
-            create: (_) =>
-                CommentController(CommentSpotUC(FirebaseCommentService(serviceLocator<FirebaseFirestore>())))),
+        ChangeNotifierProvider(create: (_) => HomeProvider.getInstance),
+        ChangeNotifierProvider(create: (_) => CommentController.getInstance),
       ],
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
@@ -89,5 +66,3 @@ class MyAppWidget extends StatelessWidget {
     );
   }
 }
-
-
