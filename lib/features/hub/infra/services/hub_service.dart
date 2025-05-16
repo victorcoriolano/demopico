a1ba2108bb5c1ed1149330bfcbb1de1095cd3f60
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-16 11:47:23 -0300

    arrumando alguns getInstance

--- lib/features/hub/infra/services/hub_service.dart
+++ lib/features/hub/infra/services/hub_service.dart
@@ -3,49 +3,49 @@ import 'package:demopico/features/external/datasources/firestore.dart';
 import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/hub/domain/interfaces/i_hub_service.dart';
 
-class HubService implements IHubService{
-  
+class HubService implements IHubService {
   static HubService? _hubService;
-  final Firestore firestore;
+  final FirebaseFirestore firestore;
 
   HubService({required this.firestore});
-  
-   static HubService  get getInstance {
-    _hubService ??= HubService(firestore: Firestore());
+
+  static HubService get getInstance {
+    _hubService ??= HubService(firestore: Firestore.getInstance);
     return _hubService!;
   }
-  
+
   @override
   Future<void> createCommunique(Communique communique) async {
-      try{
-        firestore.getInstance.collection('communique').add(communique.toJsonMap());
-      }
-     catch(e) {
+    try {
+      firestore.collection('communique').add(communique.toJsonMap());
+    } catch (e) {
       throw Exception('Error ao criar comunicado');
     }
   }
-      @override
-      Future<void> deleteCommunique(String id) {
-        throw UnimplementedError();
-      }
-    
-      @override
-      Future<List<Communique>> listCommuniques()async {
-        try{
-         QuerySnapshot querySnapshot = await firestore.getInstance
+
+  @override
+  Future<void> deleteCommunique(String id) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Communique>> listCommuniques() async {
+    try {
+      QuerySnapshot querySnapshot = await firestore
           .collection('communique')
           .orderBy('timestamp', descending: true)
           .get();
 
-      return querySnapshot.docs.map((doc) => Communique.fromDocument(doc)).toList();
-        }catch(e){
-          throw Exception('Não foi possível listar os comunicados');
-        }
-      }
-    
-      @override
-      Future<void> updateCommunique(Communique communique) {
-        throw UnimplementedError();
-      }
+      return querySnapshot.docs
+          .map((doc) => Communique.fromDocument(doc))
+          .toList();
+    } catch (e) {
+      throw Exception('Não foi possível listar os comunicados');
+    }
   }
-    
+
+  @override
+  Future<void> updateCommunique(Communique communique) {
+    throw UnimplementedError();
+  }
+}
