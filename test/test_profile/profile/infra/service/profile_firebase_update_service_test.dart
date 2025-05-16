88bcfa94f02f60c7ebfa7f5782b8ea898f45f59f
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-16 12:43:55 -0300

    teste ppara trocar de bio no service funcionando com exito

--- test/test_profile/profile/infra/service/profile_firebase_update_service_test.dart
+++ test/test_profile/profile/infra/service/profile_firebase_update_service_test.dart
@@ -1,14 +1,19 @@
+import 'package:demopico/features/profile/infra/repository/profile_firebase_read_repository.dart';
 import 'package:demopico/features/profile/infra/service/profile_firebase_update_service.dart';
+import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:demopico/features/user/infra/services/user_firebase_service.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../mocks/mocks_profile.dart';
 
 void main() {
- group('Teste update profile data', (){
-   late FakeFirebaseFirestore fakeFirestore;
+  group('Teste update profile data', () {
+    late FakeFirebaseFirestore fakeFirestore;
     setUp(() async {
       fakeFirestore = FakeFirebaseFirestore();
+
       //criando um spot fake pra referenciar no pico favorito
       await fakeFirestore
           .collection("users")
@@ -16,9 +21,20 @@ void main() {
           .set(testeProfileCerto.toJsonMap());
     });
 
-  test('Este teste deve subir uma nova bio e retorna-la' , () async {
-    final Service = ProfileFirebaseUpdateService(firestore: fakeFirestore);
-  });
+    test('Este teste deve subir uma nova bio e retorna-la', () async {
+      final serviceUpdate =
+          ProfileFirebaseUpdateService(firestore: fakeFirestore);
 
- });
+      final serviceRead = ProfileFirebaseReadRepository(
+          userDatabaseRepository: UserFirebaseRepository(
+              userFirebaseService:
+                  UserFirebaseService(firebaseFirestore: fakeFirestore)));
+      String novaBio = 'newText';
+
+      serviceUpdate.atualizarBio(novaBio, testeProfileCerto.id!);
+
+      final result = await serviceRead.pegarBio(testeProfileCerto.id!);
+      expect(result, novaBio);
+    });
+  });
 }
