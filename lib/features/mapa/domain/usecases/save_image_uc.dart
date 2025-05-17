6918145b6c5a28285c216cbec3610545aa6f0380
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-17 14:25:28 -0300

    refatorando caso de uso

--- lib/features/mapa/domain/usecases/save_image_uc.dart
+++ lib/features/mapa/domain/usecases/save_image_uc.dart
@@ -1,5 +1,5 @@
 
-import 'package:demopico/features/mapa/data/data_sources/remote/firebase_files_service.dart';
+import 'package:demopico/features/mapa/data/repositories/files_storage_repository.dart';
 import 'package:demopico/features/mapa/domain/interfaces/i_save_image_repository.dart';
 import 'package:demopico/features/mapa/domain/models/upload_file_model.dart';
 import 'package:demopico/features/mapa/domain/models/upload_result_file_model.dart';
@@ -10,7 +10,7 @@ class SaveImageUC{
   static SaveImageUC? _saveImageUC;
 
  static SaveImageUC  get getInstance{
-    _saveImageUC ??= SaveImageUC(saveImageRepositoryIMP: FirebaseFilesService.getInstance);
+    _saveImageUC ??= SaveImageUC(saveImageRepositoryIMP: FilesStorageRepository.getInstance);
     return _saveImageUC!;
   } 
 
@@ -18,16 +18,12 @@ class SaveImageUC{
 
   SaveImageUC({required this.saveImageRepositoryIMP});
 
-  Future<List<UploadResultFileModel>> saveImage(List<UploadFileModel> files) async{
-    try{
-      List<UploadResultFileModel> urls = await saveImageRepositoryIMP.saveFiles(files);
-      if(urls.isEmpty){
-        throw Exception("Não foi possível salvar a imagem");
-      }
-      return urls;
-    } on Exception catch(e) {
-      throw Exception("Erro ao salvar imagem: $e");
-      
-    }
+  List<UploadResultFileModel> saveImage(List<UploadFileModel> files) {
+    final uploadTask = saveImageRepositoryIMP.saveFiles(files);
+    final uploadResult = uploadTask.map((task) {
+      final result = UploadResultFileModel(progress: task.onProgress, url: task.url);
+      return result;
+    }).toList();
+    return uploadResult;
   }
 }
