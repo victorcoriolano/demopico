9147bff560a1a93f2171b9009ecd5a244a1a6726
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-17 14:22:25 -0300

    criando implementações para data source

---
+++ lib/features/mapa/data/data_sources/remote/firebase_file_remote_datasource.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:demopico/features/mapa/data/data_sources/interfaces/upload_task_interface.dart';
+import 'package:demopico/features/mapa/domain/models/upload_file_model.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseFileRemoteDatasource implements FileRemoteDataSource {
+  static FirebaseFileRemoteDatasource? _firebaseFileRemoteDatasource;
+
+  static FirebaseFileRemoteDatasource get getInstance {
+    return _firebaseFileRemoteDatasource ??=
+        FirebaseFileRemoteDatasource(firebaseStorage: FirebaseStorage.instance);
+  }
+
+  final FirebaseStorage firebaseStorage;
+
+  FirebaseFileRemoteDatasource({required this.firebaseStorage});
+
+  @override
+  List<UploadTaskInterface> uploadFile(List<UploadFileModel> files) {
+    final tasks = files.map((file) {
+      final uploadTask =
+          firebaseStorage.ref().child(file.fileName).putData(file.bytes);
+      return FirebaseUploadTask(uploadTask: uploadTask);
+    }).toList();
+
+    return tasks;
+  }
+}
+
+class FirebaseUploadTask implements UploadTaskInterface {
+  final _controller = StreamController<double>();
+  final UploadTask uploadTask;
+
+  FirebaseUploadTask({required this.uploadTask}) {
+    uploadTask.snapshotEvents.listen((event) {
+      if (event.state == TaskState.running) {
+        final progress = event.bytesTransferred / event.totalBytes;
+        _controller.add(progress);
+      }
+    });
+  }
+  @override
+  Stream<double> get onProgress => _controller.stream;
+
+  @override
+  Future<String> get url => uploadTask.snapshot.ref.getDownloadURL();
+}
