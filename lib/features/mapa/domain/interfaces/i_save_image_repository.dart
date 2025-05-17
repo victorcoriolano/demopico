c148b6935b48a2a8e4815921f1d752dcfc7a6824
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-17 14:24:32 -0300

    refatorando interface de repository

--- lib/features/mapa/domain/interfaces/i_save_image_repository.dart
+++ lib/features/mapa/domain/interfaces/i_save_image_repository.dart
@@ -1,10 +1,9 @@
 
+import 'package:demopico/features/mapa/data/data_sources/interfaces/upload_task_interface.dart';
 import 'package:demopico/features/mapa/domain/models/upload_file_model.dart';
-import 'package:demopico/features/mapa/domain/models/upload_result_file_model.dart';
 
 
 abstract class ISaveImageRepository {
-  Future<List<UploadResultFileModel>> saveFiles(List<UploadFileModel> files);
-  
+  List<UploadTaskInterface> saveFiles(List<UploadFileModel> files);
 }
 
