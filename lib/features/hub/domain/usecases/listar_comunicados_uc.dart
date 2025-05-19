d2af9754ea1a8f24ae3e93fcbd11687ea60b919c
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-05-19 12:50:31 -0300

    add: verificando se a lista retornou vazia para lançar exception

--- lib/features/hub/domain/usecases/listar_comunicados_uc.dart
+++ lib/features/hub/domain/usecases/listar_comunicados_uc.dart
@@ -1,11 +1,15 @@
 import 'package:demopico/features/hub/domain/entities/communique.dart';
 import 'package:demopico/features/hub/infra/services/hub_service.dart';
 
-class ListarComunicado{
+class ListarComunicado {
   HubService hubService;
   ListarComunicado({required this.hubService});
 
-  Future<List<Communique>> listar() async{
-   return await hubService.getInstance.getAllCommuniques();
+  Future<List<Communique>> listar() async {
+    var dados = await hubService.getInstance.getAllCommuniques();
+    if (dados.isEmpty) {
+      throw Exception('Não foi possível listar os comunicados');
+    }
+    return dados;
   }
 }
