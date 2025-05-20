c8875199d3c386fe07f5a8701435423beac7daa1
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-20 18:28:22 -0300

    teste unitario repository profile update

--- test/test_profile/profile/infra/repository/profile_firebase_update_repository_test.dart
+++ test/test_profile/profile/infra/repository/profile_firebase_update_repository_test.dart
@@ -1,53 +1,41 @@
 import 'package:demopico/features/profile/infra/repository/profile_firebase_update_repository.dart';
-import 'package:demopico/features/profile/infra/service/profile_firebase_update_service.dart';
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../../../mocks/mocks_profile.dart';
 
+class MockProfileFirebaseUpdateRepository extends Mock
+    implements ProfileFirebaseUpdateRepository {}
+
 void main() {
   group('Teste update profile data repository', () {
-    late FakeFirebaseFirestore fakeFirestore;
-    setUp(() async {
-      fakeFirestore = FakeFirebaseFirestore();
+    late MockProfileFirebaseUpdateRepository
+        fakeProfileFirebaseUpdateRepository;
 
-      //criando um spot fake pra referenciar no pico favorito
-      await fakeFirestore
-          .collection("users")
-          .doc(testeProfileCerto.id)
-          .set(testeProfileCerto.toJsonMap());
+    setUp(() async {
+      fakeProfileFirebaseUpdateRepository =
+          MockProfileFirebaseUpdateRepository();
     });
 
     test('Este teste deve subir uma nova bio', () async {
-      final repositoryUpdate = ProfileFirebaseUpdateRepository(
-          profileDatabaseUpdateServiceIMP:
-              ProfileFirebaseUpdateService(firestore: fakeFirestore));
-
+      final repositoryUpdate = fakeProfileFirebaseUpdateRepository;
       String novaBio = 'teste';
       repositoryUpdate.atualizarBio(novaBio, testeProfileCerto);
     });
 
     test('Este teste deve adicionar um novo seguidor ', () async {
-      final repositoryUpdate = ProfileFirebaseUpdateRepository(
-          profileDatabaseUpdateServiceIMP:
-              ProfileFirebaseUpdateService(firestore: fakeFirestore));
+      final repositoryUpdate = fakeProfileFirebaseUpdateRepository;
       repositoryUpdate.atualizarSeguidores(testeProfileCerto);
     });
 
     test('Este teste deve adicionar uma nova contribuição ', () async {
-      final repositoryUpdate = ProfileFirebaseUpdateRepository(
-          profileDatabaseUpdateServiceIMP:
-              ProfileFirebaseUpdateService(firestore: fakeFirestore));
+      final repositoryUpdate = fakeProfileFirebaseUpdateRepository;
       repositoryUpdate.atualizarContribuicoes(testeProfileCerto);
     });
 
     test('Este teste deve subir uma nova image ', () async {
-      final repositoryUpdate = ProfileFirebaseUpdateRepository(
-          profileDatabaseUpdateServiceIMP:
-              ProfileFirebaseUpdateService(firestore: fakeFirestore));
+      final repositoryUpdate = fakeProfileFirebaseUpdateRepository;
       String newImage = 'certo';
-
       repositoryUpdate.atualizarFoto(newImage, testeProfileCerto);
     });
   });
