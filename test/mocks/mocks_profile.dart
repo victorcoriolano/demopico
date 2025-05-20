ab78800ef59474a0decb9c3dcea71028847d42d5
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-20 15:27:55 -0300

    fazendo fail com teste

--- test/mocks/mocks_profile.dart
+++ test/mocks/mocks_profile.dart
@@ -18,13 +18,13 @@ import 'package:demopico/features/user/domain/models/user.dart';
   final testeProfileErrado = UserM(
    name: 'artu' ,
    description: null,
-   id: '1',
-   pictureUrl:'',
+   id: null,
+   pictureUrl: null,
    isColetivo: false,
    signMethod: SignMethods.email,
    location:'',
    dob:'',
-   conexoes: 0,
-   picosAdicionados: 0,
+   conexoes: null,
+   picosAdicionados: null,
   );
 
