e7d11c647818763c7ed5a1a867af426a8d34c1df
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-20 10:59:46 -0300

    base dos testes

--- test/test_profile/profile/infra/repository/profile_firebase_read_repository_test.dart
+++ test/test_profile/profile/infra/repository/profile_firebase_read_repository_test.dart
@@ -1,8 +1,36 @@
 
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../mocks/mocks_profile.dart';
 
 void main() {
   group("Este group possui todas as operações de read do Profile", () async {
+late FakeFirebaseFirestore fakeFirestore;
+    setUp(() async {
+      fakeFirestore = FakeFirebaseFirestore();
+
+      //criando um spot fake pra referenciar no pico favorito
+      await fakeFirestore
+          .collection("users")
+          .doc(testeProfileCerto.id)
+          .set(testeProfileCerto.toJsonMap());
+  });
+
+  test("Este teste deve retornar uma string de nova foto ", () async{
 
   });
-}
+  
+  test("Este teste deve retornar", () async{
+
+  });
+  
+  test("Este teste deve retornar", () async{
+
+  });
+  
+  test("Este teste deve retornar", () async{
+
+  });
+
+});}
