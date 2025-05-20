68b419f879fe0755e39ebdde1a9996b2a7b6d648
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-20 18:03:21 -0300

    usando mock no teste

--- test/test_profile/profile/infra/service/profile_firebase_update_service_test.dart
+++ test/test_profile/profile/infra/service/profile_firebase_update_service_test.dart
@@ -1,45 +1,42 @@
 import 'package:demopico/features/profile/infra/service/profile_firebase_update_service.dart';
+import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../../../mocks/mocks_profile.dart';
 
+class MockProfileFirebaseUpdateService extends Mock
+    implements ProfileFirebaseUpdateService {}
+
+class MockUserM extends Mock implements UserM {}
+
 void main() {
   group('Teste update profile data', () {
-    late FakeFirebaseFirestore fakeFirestore;
-    setUp(() async {
-      fakeFirestore = FakeFirebaseFirestore();
+    late MockProfileFirebaseUpdateService fakeProfileFirebaseUpdateService;
 
-      //criando um spot fake pra referenciar no pico favorito
-      await fakeFirestore
-          .collection("users")
-          .doc(testeProfileCerto.id)
-          .set(testeProfileCerto.toJsonMap());
+    setUp(() async {
+      fakeProfileFirebaseUpdateService = MockProfileFirebaseUpdateService();
     });
 
     test('Este teste deve subir uma nova bio', () async {
-      final serviceUpdate =
-          ProfileFirebaseUpdateService(firestore: fakeFirestore);
+      final serviceUpdate = fakeProfileFirebaseUpdateService;
       String novaBio = 'teste';
       serviceUpdate.atualizarBio(novaBio, testeProfileCerto.id!);
     });
 
     test('Este teste deve adicionar um novo seguidor ', () async {
-      final serviceUpdate =
-          ProfileFirebaseUpdateService(firestore: fakeFirestore);
+      final serviceUpdate = fakeProfileFirebaseUpdateService;
       serviceUpdate.atualizarSeguidores(testeProfileCerto.id!);
     });
 
     test('Este teste deve adicionar uma nova contribuição ', () async {
-      final serviceUpdate =
-          ProfileFirebaseUpdateService(firestore: fakeFirestore);
+      final serviceUpdate = fakeProfileFirebaseUpdateService;
       serviceUpdate.atualizarContribuicoes(testeProfileCerto.id!);
     });
 
     test('Este teste deve subir ', () async {
-      final serviceUpdate =
-          ProfileFirebaseUpdateService(firestore: fakeFirestore);
+      final serviceUpdate = fakeProfileFirebaseUpdateService;
       String newImage = 'certo';
 
       serviceUpdate.atualizarFoto(newImage, testeProfileCerto.id!);
