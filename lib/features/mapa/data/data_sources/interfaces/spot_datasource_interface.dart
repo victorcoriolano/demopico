e7dd1f07f0a9f110d70a8a2c053a1ea9581b6a5a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-22 12:14:25 -0300

    refatorando datasource para receber um dto ao invés da classe de dominio

--- lib/features/mapa/data/data_sources/interfaces/spot_datasource_interface.dart
+++ lib/features/mapa/data/data_sources/interfaces/spot_datasource_interface.dart
@@ -1,11 +1,11 @@
 
+import 'package:demopico/features/mapa/data/dtos/pico_model_firebase_dto.dart';
 import 'package:demopico/features/mapa/domain/entities/filters.dart';
-import 'package:demopico/features/mapa/domain/models/pico_model.dart';
 
 abstract class SpotRemoteDataSource {
-  Future<PicoModel> create(Map<String, dynamic> data);
-  Future<Map<String, dynamic>> update(Map<String, dynamic> data);
-  Stream<List<Map<String, dynamic>>> load([Filters? filtro]);
-  Future<PicoModel> getbyID(String id);
+  Future<PicoModelFirebaseDto> create(PicoModelFirebaseDto data);
+  Future<void> update(PicoModelFirebaseDto data);
+  Stream<List<PicoModelFirebaseDto>> load([Filters? filtro]);
+  Future<PicoModelFirebaseDto> getbyID(String id);
   Future<void> delete(String id);
 }
