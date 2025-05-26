85dcdb8d133aed601971dfccaf4912b56d060606
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-26 20:43:42 -0300

    concertando testes

---
+++ test/test_map/datasources/datasource_firebase_comment_test.dart
@@ -0,0 +1,82 @@
+
+import 'package:demopico/features/mapa/data/data_sources/remote/firebase_comment_service.dart';
+import 'package:demopico/features/mapa/data/dtos/firebase_dto.dart';
+import 'package:demopico/features/mapa/data/mappers/mapper_dto_commentmodel.dart';
+import 'package:demopico/features/mapa/domain/models/comment_model.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("Serviço de dados dos comentários com firebase", () {
+
+    late FakeFirebaseFirestore fakeFirestore;
+
+    late FirebaseCommentRemoteDataSource service;
+    late CommentModel comment;
+
+    setUpAll(() {
+      fakeFirestore = FakeFirebaseFirestore();
+      service = FirebaseCommentRemoteDataSource(firebaseFirestore: fakeFirestore);
+      comment = CommentModel(id: "1",peakId: "1", userId: "userId", content: "teste", timestamp: DateTime.now());
+    });
+
+    test("Deve salvar um comentário", () async {
+      
+      final result = await service.create(MapperDtoCommentmodel.toDto(comment));
+
+      expect(result, isA<FirebaseDTO>());
+      expect(result.id, isNotEmpty);
+
+    });
+
+    test("Deve Listar comentários com base no id do pico", () async{
+      //criando fakes 
+      Future.wait([
+        fakeFirestore.collection('comments').add({
+          'peakId': '1',
+          'userId': 'userId',
+          'content': 'teste',
+          'timestamp': DateTime.now().toIso8601String(),
+        }),
+        fakeFirestore.collection('comments').add({
+          'peakId': '1',
+          'userId': 'userId',
+          'content': 'teste',
+          'timestamp': DateTime.now().toIso8601String(),
+        }),
+        fakeFirestore.collection('comments').add({
+          'peakId': '1',
+          'userId': 'userId',
+          'content': 'teste',
+          'timestamp': DateTime.now().toIso8601String(),
+        }),
+      ]);
+
+      final result = await service.getBySpotId("1");
+      expect(result, isA<List<FirebaseDTO>>());
+      expect(result.length, 4); //  3 comentários mais o comentário criado no teste anterior
+      
+    });
+
+    test("Deve deletar um comentário", () async {
+      //criando fakes
+      final commentOnDB = await service.create(MapperDtoCommentmodel.toDto(comment));
+      await service.delete(commentOnDB.id);
+      final result = await service.getBySpotId("2");
+      expect(result.length, equals(0)); // 0 comentários do pico 2
+    });
+
+    test("Deve atualizar um comentário", () async {
+      //criando fakes
+      final commentBD = await service.create(MapperDtoCommentmodel.toDto(comment));
+            var id = commentBD.id;
+      final updateComment = comment.copyWith(content: "teste atualizado", id: id);
+        
+      await service.update(MapperDtoCommentmodel.toDto(updateComment));
+      
+      final updated = await service.getBySpotId("1");
+      expect(updated[0].data['content'], equals("teste atualizado"));
+    });
+  });
+}  
--- test/test_map/datasources/datasource_spot_test.dart
+++ test/test_map/datasources/datasource_spot_test.dart
@@ -1,6 +1,6 @@
 
 import 'package:demopico/features/mapa/data/data_sources/remote/firebase_spot_remote_datasource.dart';
-import 'package:demopico/features/mapa/data/dtos/spot_firebase_dto.dart';
+import 'package:demopico/features/mapa/data/dtos/firebase_dto.dart';
 import 'package:demopico/features/mapa/data/mappers/mapper_dto_picomodel.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
@@ -27,7 +27,7 @@ import '../../mocks/mocks_spots.dart';
 
         final result = await dataSource.create(MapperDtoPicomodel.toDto(testPico));
 
-        expect(result, isA<SpotFirebaseDTO>());
+        expect(result, isA<FirebaseDTO>());
         expect(result.data, isA<Map<String, dynamic>>());
         expect(result.id, isA<String>());
         debugPrint(result.id);
@@ -78,9 +78,9 @@ import '../../mocks/mocks_spots.dart';
         expectLater(
           stream,
           emitsInOrder([
-            isA<List<SpotFirebaseDTO>>()
+            isA<List<FirebaseDTO>>()
                 .having((list) => list.length, "deve ter 2 instancia", equals(2)),
-            isA<List<SpotFirebaseDTO>>()
+            isA<List<FirebaseDTO>>()
                 .having((list) => list.length, "deve ter 3 instancia", equals(3)),
             // Após adicionar o pico
           ]),
