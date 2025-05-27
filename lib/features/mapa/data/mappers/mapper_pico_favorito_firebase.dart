c4b226c08cc7d7ee9c3cf88d1a7058e0f13b7d84
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-26 21:52:32 -0300

    criando mapeador para picos favoritos

--- lib/features/mapa/data/mappers/mapper_pico_favorito_firebase.dart
+++ lib/features/mapa/data/mappers/mapper_pico_favorito_firebase.dart
@@ -1,24 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demopico/features/mapa/data/dtos/firebase_dto.dart';
 import 'package:demopico/features/mapa/domain/entities/pico_favorito.dart';
 import 'package:demopico/features/mapa/domain/models/pico_favorito_model.dart';
 
 class MapperFavoriteSpotFirebase {
-  static PicoFavoritoModel fromFirebase(DocumentSnapshot doc){
+  static FirebaseDTO toDto(PicoFavorito picoFavorito) {
+    return FirebaseDTO(
+      id: "",
+      data: {
+        'idUsuario': picoFavorito.idUsuario,
+        'idPico': picoFavorito.idPico,
+      }
+    );
+  }
+  static PicoFavoritoModel fromDto(FirebaseDTO dto) {
+    return PicoFavoritoModel(
+      idPico: dto.data['idPico'],
+      idUsuario: dto.data['idUsuario'],
+      id: dto.id,
+    );
+  }
+  static FirebaseDTO fromFirebase(DocumentSnapshot doc){
     final data = doc.data() as Map<String, dynamic>;
 
-    return PicoFavoritoModel(
+    return FirebaseDTO(
       id: doc.id,
-      idPico: (data['idPico'] as DocumentReference).id,
-      idUsuario: data['idUsuario'] as String,
+      data: {
+        'idUsuario': data['idUsuario'] as String,
+        'idPico': data['idPico'] as DocumentReference,
+      }
     );
   }
 
-  static Map<String, dynamic> toFirebase(PicoFavorito picoFavorito, FirebaseFirestore firebaseInstance) {
+  static Map<String, dynamic> toFirebase(FirebaseDTO picoFavorito, FirebaseFirestore firebaseInstance) {
     return {
-      'idUsuario': picoFavorito.idUsuario,
+      'idUsuario': picoFavorito.data['idUsuario'] as String,
       'idPico': firebaseInstance
         .collection('spots')  
-        .doc(picoFavorito.idPico),
+        .doc(picoFavorito.data["idPico"]),
     };
   }
 }
