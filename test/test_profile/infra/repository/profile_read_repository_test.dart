e16c3ce2e8497fea79f558690f44867b827309df
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-30 23:22:44 -0300

    começando read test

--- test/test_profile/infra/repository/profile_read_repository_test.dart
+++ test/test_profile/infra/repository/profile_read_repository_test.dart
@@ -1,46 +1,55 @@
 import 'package:demopico/features/profile/domain/interfaces/i_profile_read_repository.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_profile_update_datasource.dart';
 import 'package:demopico/features/profile/infra/repository/profile_read_repository.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_auth_service.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_service.dart';
+import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:demopico/features/user/infra/services/user_firebase_service.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../../mocks/mocks_profile.dart';
 
-class MockDatasource extends Mock
-    implements IProfileReadDatasource {}
-
+class MockRepository extends Mock
+    implements IUserDatabaseRepository {}
 void main() {
   group("Este group possui todas as operações de read do Profile", () {
-    late MockDatasource fakeProfileReadRepository;
+    late MockRepository fakeUserRepository;
+    late ProfileReadRepository fakeProfileReadRepository;
+    
     setUp(() async {
-      fakeProfileReadRepository = ProfileReadRepository.getInstance;
+      fakeUserRepository = MockRepository();
+      fakeProfileReadRepository = ProfileReadRepository(userRepositoryIMP: MockRepository());
     });
 
     test("Este teste deve retornar uma string de nova foto ", () async {
       final repositoryRead = fakeProfileReadRepository;
       String text = await repositoryRead.getPhoto(testeProfileCerto);
-      expect(text, text);
+      
+      
     });
 
     test("Este teste deve retornar uma string de uma nova bio", () async {
       final repositoryRead = fakeProfileReadRepository;
 
       String text = await repositoryRead.getBio(testeProfileCerto);
-      expect(text, text);
+      
     });
 
     test("Este teste deve retornar um novo int de seguidor", () async {
       final repositoryRead = fakeProfileReadRepository;
 
-      int text = await repositoryRead.getFollowers(testeProfileCerto);
-      expect(text, text);
+       await repositoryRead.getFollowers(testeProfileCerto);
+      
     });
 
     test("Este teste deve retornar um novo int de contribuições", () async {
       final repositoryRead = fakeProfileReadRepository;
 
-      int text = await repositoryRead.getContributions(testeProfileCerto);
-      expect(text, text);
+      await repositoryRead.getContributions(testeProfileCerto);
+      
     });
   });
 }
