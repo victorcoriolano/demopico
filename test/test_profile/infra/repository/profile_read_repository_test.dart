552230b537a03ea294fcb879af982320449ed84f
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-03 11:01:01 -0300

    teste read repository feito

--- test/test_profile/infra/repository/profile_read_repository_test.dart
+++ test/test_profile/infra/repository/profile_read_repository_test.dart
@@ -1,60 +1,69 @@
 import 'package:demopico/features/profile/infra/repository/profile_read_repository.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
-import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../../mocks/mocks_profile.dart';
 
-class MockRepository extends Mock
-    implements UserFirebaseRepository {}
+class MockUserRepository extends Mock implements IUserDatabaseRepository {}
+
 void main() {
   group("Este group possui todas as operações de read do Profile", () {
-    late UserFirebaseRepository fakeUserRepository;
+    late MockUserRepository fakeUserRepository;
     late ProfileReadRepository fakeProfileReadRepository;
-    
+
     setUp(() async {
-      fakeUserRepository = MockRepository();
-      fakeProfileReadRepository = ProfileReadRepository(userRepositoryIMP: MockRepository());
+      fakeUserRepository = MockUserRepository();
+      fakeProfileReadRepository =
+          ProfileReadRepository(userRepositoryIMP: fakeUserRepository);
     });
 
     test("Este teste deve retornar uma string de nova foto ", () async {
-      final repositoryRead = fakeProfileReadRepository;
-      final repositoryUser = fakeUserRepository;
-
- // Mockando o retorno do getUserDetails
-       when(() => repositoryUser.getUserDetails(any()))
+      when(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
           .thenAnswer((_) async => testeProfileCerto);
 
-      await repositoryRead.getPhoto(testeProfileCerto);
-      verify(() => repositoryUser.getUserDetails(testeProfileCerto.id!)).called(1);
+      final result =
+          await fakeProfileReadRepository.getPhoto(testeProfileCerto);
 
-      
+      expect(result, equals(testeProfileCerto.pictureUrl));
+
+      verify(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .called(1);
     });
 
     test("Este teste deve retornar uma string de uma nova bio", () async {
-      final repositoryRead = fakeProfileReadRepository;
-      final repositoryUser = fakeUserRepository;
+      when(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .thenAnswer((_) async => testeProfileCerto);
 
-      await repositoryRead.getBio(testeProfileCerto);
-      
+      final result = await fakeProfileReadRepository.getBio(testeProfileCerto);
+
+      expect(result, equals(testeProfileCerto.description));
+      verify(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .called(1);
     });
 
     test("Este teste deve retornar um novo int de seguidor", () async {
-      final repositoryRead = fakeProfileReadRepository;
-      final repositoryUser = fakeUserRepository;
+      when(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .thenAnswer((_) async => testeProfileCerto);
 
-       await repositoryRead.getFollowers(testeProfileCerto);
-      
+      final result =
+          await fakeProfileReadRepository.getFollowers(testeProfileCerto);
+
+      expect(result, equals(testeProfileCerto.conexoes));
+      verify(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .called(1);
     });
 
     test("Este teste deve retornar um novo int de contribuições", () async {
-      final repositoryRead = fakeProfileReadRepository;
-      final repositoryUser = fakeUserRepository;
+      when(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .thenAnswer((_) async => testeProfileCerto);
 
-      await repositoryRead.getContributions(testeProfileCerto);
-      
+      final result =
+          await fakeProfileReadRepository.getContributions(testeProfileCerto);
+
+      expect(result, equals(testeProfileCerto.picosAdicionados));
+      verify(() => fakeUserRepository.getUserDetails(testeProfileCerto.id!))
+          .called(1);
     });
   });
 }
