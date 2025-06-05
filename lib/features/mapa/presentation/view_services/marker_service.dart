5d9c3c23e2804cc78ee260ef3cba9b5a8ea75dc6
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-04 22:37:23 -0300

    correção de marker service

--- lib/features/mapa/presentation/view_services/marker_service.dart
+++ lib/features/mapa/presentation/view_services/marker_service.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:demopico/features/mapa/domain/entities/pico_entity.dart';
+import 'package:demopico/features/mapa/presentation/view_services/modal_helper.dart';
 import 'package:demopico/features/mapa/presentation/widgets/icon_marker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:widget_to_marker/widget_to_marker.dart';
@@ -13,29 +14,29 @@ class MarkerService {
   }
 
   final Map<String, BitmapDescriptor> _markerIcons = {};
+  Set<Marker> markers = {};
 
   Map<String, BitmapDescriptor> get markerIcons => _markerIcons;
 
   Future<void> preloadIcons(List<Pico> picos) async {
     for (final pico in picos) {
+      // carrega os icons 
       final icon = await IconMarker(
-        text: pico.picoName, // Você pode colocar nome ou outro dado aqui
+        text: pico.picoName, 
       ).toBitmapDescriptor(
         logicalSize: const Size(150, 150),
         imageSize: const Size(120, 150),
       );
       _markerIcons[pico.picoName] = icon;
-    }
-  }
 
-  Set<Marker> createMarkers(List<Pico> picos, void Function(Pico) onTapPico) {
-    return picos.map((pico) {
-      return Marker(
+      // carrega os markers 
+      final marker = Marker(
         markerId: MarkerId(pico.picoName),
         position: LatLng(pico.lat, pico.long),
         icon: _markerIcons[pico.picoName] ?? BitmapDescriptor.defaultMarker,
-        onTap: () => onTapPico(pico),
+        onTap: () => ModalHelper.openModalInfoPico,
       );
-    }).toSet();
+      markers.add(marker);
+    }
   }
 }
