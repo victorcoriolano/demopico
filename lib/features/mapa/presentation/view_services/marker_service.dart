3075dbcd86c91849c64bb33ea1b922c3e7e581a4
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-05 11:04:49 -0300

    corrigindo marker service

--- lib/features/mapa/presentation/view_services/marker_service.dart
+++ lib/features/mapa/presentation/view_services/marker_service.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
 
 import 'package:demopico/features/mapa/domain/entities/pico_entity.dart';
-import 'package:demopico/features/mapa/presentation/view_services/modal_helper.dart';
 import 'package:demopico/features/mapa/presentation/widgets/icon_marker.dart';
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:widget_to_marker/widget_to_marker.dart';
 
@@ -18,7 +17,7 @@ class MarkerService {
 
   Map<String, BitmapDescriptor> get markerIcons => _markerIcons;
 
-  Future<void> preloadIcons(List<Pico> picos) async {
+  Future<void> preloadIcons(List<Pico> picos, void Function(Pico) onTap) async {
     for (final pico in picos) {
       // carrega os icons 
       final icon = await IconMarker(
@@ -28,15 +27,17 @@ class MarkerService {
         imageSize: const Size(120, 150),
       );
       _markerIcons[pico.picoName] = icon;
+      debugPrint("criou o icon");
 
       // carrega os markers 
       final marker = Marker(
         markerId: MarkerId(pico.picoName),
         position: LatLng(pico.lat, pico.long),
         icon: _markerIcons[pico.picoName] ?? BitmapDescriptor.defaultMarker,
-        onTap: () => ModalHelper.openModalInfoPico,
+        onTap: () => onTap(pico),
       );
       markers.add(marker);
+      debugPrint("criou o marker");
     }
   }
 }
