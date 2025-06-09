2af862ed556276a298ba54895aed99256b303bd5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-09 08:46:28 -0300

    adicionando get name  fromString no enum siginMethods

--- lib/features/user/domain/enums/sign_methods.dart
+++ lib/features/user/domain/enums/sign_methods.dart
@@ -2,4 +2,27 @@ enum SignMethods {
   email,
   google,
   phone;
+
+  String get name {
+    switch (this) {
+      case SignMethods.email:
+        return 'email';
+      case SignMethods.google:
+        return 'google';
+      case SignMethods.phone:
+        return 'phone';
+    }
+  }
+  factory SignMethods.fromString(String value) {
+    switch (value) {
+      case 'email':
+        return SignMethods.email;
+      case 'google':
+        return SignMethods.google;
+      case 'phone':
+        return SignMethods.phone;
+      default:
+        return SignMethods.email;
+    }
+  }
 }
