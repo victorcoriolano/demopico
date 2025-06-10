273cf661ed63fe217c56b4c023b044ba19649b68
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-10 11:37:40 -0300

    separando em dois widget para implementar animação

--- lib/features/profile/presentation/widgets/profile_top_side_data_widget.dart
+++ lib/features/profile/presentation/widgets/profile_top_side_data_widget.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ProfileDataWidget extends StatelessWidget {
+class ProfileTopSideDataWidget extends StatelessWidget {
   final String? name;
   final String? avatarUrl;
   final String? backgroundUrl;
-  final String? description;
-  final int? followers;
-  final int? contributions;
   final double avatarSize;
-
-  const ProfileDataWidget({
+  const ProfileTopSideDataWidget({
     super.key,
     required this.name,
     required this.avatarUrl,
     required this.backgroundUrl,
-    required this.description,
-    required this.followers,
-    required this.contributions,
     this.avatarSize = 80,
   });
 
@@ -67,49 +60,6 @@ class ProfileDataWidget extends StatelessWidget {
           name ?? 'Nome de usuário não encontrado...',
           style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
-
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: (screenWidth * 0.20) / 2, vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '$followers\nSeguidores',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              Text(
-                '$contributions\nContribuições',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Center(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: screenWidth > 600 ? 400 : null,
-              alignment: Alignment.center,
-              child: Text(
-                textAlign: TextAlign.center,
-                description ?? '',
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-        )
       ],
     );
   }
