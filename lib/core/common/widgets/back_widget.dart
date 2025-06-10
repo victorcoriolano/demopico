c6ea67d9d95c7d67c4fa53bf93e0c305bdf7bba1
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-10 09:06:15 -0300

    custom back widget

---
+++ lib/core/common/widgets/back_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomBackButton extends StatelessWidget {
+  final Widget destination;
+  final double iconSize;
+
+  const CustomBackButton({
+    super.key,
+    required this.destination,
+    this.iconSize = 35,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Get.to(
+          () => destination,
+          popGesture: true,
+          preventDuplicates: true,
+          curve: Curves.easeInBack,
+          transition: Transition.downToUp,
+        );
+      },
+      icon: const Icon(Icons.arrow_back),
+      color: const Color.fromARGB(255, 0, 0, 0),
+      splashRadius: 0.5,
+      tooltip: "Voltar",
+      splashColor: null,
+      focusColor: null,
+      hoverColor: null,
+      iconSize: iconSize,
+    );
+  }
+}
