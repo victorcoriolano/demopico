d043e823b4cd525dda5cc9a2b3aee82d79b5b99c
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-12 16:04:51 -0300

    criando use case de deletar arquivos

---
+++ lib/features/mapa/domain/usecases/delete_file_uc.dart
@@ -0,0 +1,21 @@
+
+import 'package:demopico/features/mapa/data/repositories/files_storage_repository.dart';
+
+class DeleteFileUc {
+  final FilesStorageRepository repository;
+
+  DeleteFileUc({required this.repository});
+
+  static DeleteFileUc? _instance;
+
+  static DeleteFileUc get instance{
+    _instance ??= DeleteFileUc(
+      repository: FilesStorageRepository.getInstance);
+    return _instance!;
+  }
+
+
+  Future<void> deletarFile(List<String> urls) async =>
+      await repository.deleteFiles(urls);
+  
+}
