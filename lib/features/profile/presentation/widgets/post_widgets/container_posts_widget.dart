2fc4f74456329324969e2104bb3eee4e3e2a948d
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-11 21:55:20 -0300

    testando widget card

--- lib/features/profile/presentation/widgets/post_widgets/container_posts_widget.dart
+++ lib/features/profile/presentation/widgets/post_widgets/container_posts_widget.dart
@@ -1,3 +1,4 @@
+import 'package:demopico/features/profile/domain/models/post.dart';
 import 'package:demopico/features/profile/presentation/widgets/post_widgets/card_post_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,46 @@ class ContainerPostsWidget extends StatefulWidget {
 }
 
 class _ContainerPostsWidgetState extends State<ContainerPostsWidget> {
+
+  Post post = Post(
+    nome: 'João Silva',
+    userId: 'user123',
+    postId: 'post123',
+    urlUserPhoto: 'https://example.com/user_photo.jpg',
+    description: 'Curtindo um dia de sol no parque!',
+    urlPhotos: [
+      'https://firebasestorage.googleapis.com/v0/b/pico-skatepico.appspot.com/o/spots_images%2Fimages%2FDegrau%20no%20Centro%20.jpg?alt=media&token=2dda3751-c51e-4981-875d-5ab7c92cbded',
+      'https://firebasestorage.googleapis.com/v0/b/pico-skatepico.appspot.com/o/spots_images%2Fimages%2FPico%20top.jpg?alt=media&token=1a489ff1-0c61-4e31-b793-117c46ced16f'
+    ],
+    dateTime: DateTime.now(),
+    curtidas: 42,
+  );
+
+    Post post1 = Post(
+    nome: 'João Silva',
+    userId: 'user123',
+    postId: 'post123',
+    urlUserPhoto: 'https://example.com/user_photo.jpg',
+    description: 'Curtindo um dia de sol no parque!',
+    urlPhotos: [
+      'https://firebasestorage.googleapis.com/v0/b/pico-skatepico.appspot.com/o/spots_images%2Fimages%2FPico%20top.jpg?alt=media&token=1a489ff1-0c61-4e31-b793-117c46ced16',
+
+    ],
+    dateTime: DateTime.now(),
+    curtidas: 42,
+  );
+
+    List<Post> listPost = [];
+
+    void inserir(){
+      listPost.clear();
+      listPost.add(post);
+      listPost.add(post1);
+      print(listPost);
+    }
+
+  
+
   final List<Map<String, String>> posts = [
     {'type': 'photo', 'title': 'Foto 1'},
     {'type': 'photo', 'title': 'Foto 2'},
@@ -27,6 +68,7 @@ class _ContainerPostsWidgetState extends State<ContainerPostsWidget> {
   ];
   @override
   Widget build(BuildContext context) {
+    inserir();
     List<Map<String, String>> filteredPosts =
         posts.where((post) => post['type'] == 'photo').toList();
     return Padding(
@@ -36,15 +78,15 @@ class _ContainerPostsWidgetState extends State<ContainerPostsWidget> {
           crossAxisCount: 3,
           childAspectRatio: 1,
         ),
-        itemCount: filteredPosts.length,
+        itemCount: listPost.length,
         itemBuilder: (context, index) {
-          return const Card(
-            margin: EdgeInsets.all(0.8),
-              shape: RoundedRectangleBorder(
+          return  Card(
+            margin: const EdgeInsets.all(0.8),
+              shape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.zero,
         ),
             child: Center(
-              child: CardPostWidget(imagePath: "assets/images/rayssaLeal.png")
+              child: CardPostWidget(post: listPost[index])
             ),
           );
         },
