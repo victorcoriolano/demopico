b3aa083aed2663eabf61aff82cead41356331368
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-11 21:55:40 -0300

    card photo

--- lib/features/profile/presentation/widgets/post_widgets/card_post_widget.dart
+++ lib/features/profile/presentation/widgets/post_widgets/card_post_widget.dart
@@ -1,19 +1,21 @@
-import 'package:demopico/core/app/home_page.dart';
+import 'package:demopico/features/profile/domain/models/post.dart';
+import 'package:demopico/features/profile/presentation/widgets/post_widgets/post_widget.dart';
 import 'package:flutter/material.dart';
 
 class CardPostWidget extends StatelessWidget {
-  final String imagePath;
+  final Post post;
 
-  const CardPostWidget({super.key, required this.imagePath});
+  const CardPostWidget({super.key, required this.post});
 
   @override
   Widget build(BuildContext context) {
+    String cardPhoto = post.urlPhotos[0];
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (_) => const HomePage(),
+            builder: (_) => PostWidget(post: post),
           ),
         );
       },
@@ -21,8 +23,8 @@ class CardPostWidget extends StatelessWidget {
         margin: const EdgeInsets.all(0),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
         clipBehavior: Clip.antiAlias,
-        child: Image.asset(
-          imagePath,
+        child: Image.network(
+          cardPhoto,
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
