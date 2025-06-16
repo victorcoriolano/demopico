9ae37e3ae445a89441e331b7f9f1ebefac8f27b9
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-16 16:42:03 -0300

    EXEMPLE

--- lib/features/mapa/data/mappers/mapper_dto_commentmodel.dart
+++ lib/features/mapa/data/mappers/mapper_dto_commentmodel.dart
@@ -1,15 +1,19 @@
+import 'package:demopico/core/common/data/interfaces/datasource/mappers/i_mapper_firebase_dto.dart';
 import 'package:demopico/features/mapa/data/dtos/firebase_dto.dart';
 import 'package:demopico/features/mapa/domain/models/comment_model.dart';
 
-class MapperDtoCommentmodel {
-  static CommentModel fromDto(FirebaseDTO commentDto) {
+class MapperDtoCommentmodel implements IMapperFirebaseDto<CommentModel> {
+  
+  @override
+   CommentModel fromDto(FirebaseDTO commentDto) {
     return CommentModel.fromJson(commentDto.data, commentDto.id);
   }
-
-  static FirebaseDTO toDto(CommentModel commentModel) {
-    return FirebaseDTO(
-      id: commentModel.id,
-      data: commentModel.toJson(),
+  
+  @override
+  FirebaseDTO toDTO(CommentModel model) {
+     return FirebaseDTO(
+      id: model.id,
+      data: model.toJson(),
     );
   }
 }
