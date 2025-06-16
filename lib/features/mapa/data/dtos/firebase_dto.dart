95e9c928d826a2b3f372906bbef6e42cbe3f45b6
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-15 23:27:22 -0300

    refatora a classe FirebaseDTO para melhorar a legibilidade e a estrutura do código

--- lib/features/mapa/data/dtos/firebase_dto.dart
+++ lib/features/mapa/data/dtos/firebase_dto.dart
@@ -1,14 +1,30 @@
 class FirebaseDTO {
-    final String id;
-    final Map<String, dynamic> data;
+  final String id;
+  final Map<String, dynamic> data;
 
-    FirebaseDTO({required this.id, required this.data});
+  FirebaseDTO({required this.id, required this.data});
 
-    FirebaseDTO copyWith({
-        String? id,
-        Map<String, dynamic>? data,
-    }) => FirebaseDTO(
-            id: id ?? this.id,
-            data: data ?? this.data,
-        );
+  FirebaseDTO copyWith({
+    String? id,
+    Map<String, dynamic>? data,
+  }) {
+    return FirebaseDTO(
+      id: id ?? this.id,
+      data: data ?? this.data,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'data': data,
+    };
+  }
+
+  factory FirebaseDTO.fromJson(Map<String, dynamic> json) {
+    return FirebaseDTO(
+      id: json['id'],
+      data: Map<String, dynamic>.from(json['data']),
+    );
+  }
 }
