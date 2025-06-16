dbac7a4c9907aae3f5932a0c084504b7cc9360e8
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-16 19:34:30 -0300

    alterações nos testes para funcionar no novo fluxo

--- test/test_map/datasources/datasource_spot_test.dart
+++ test/test_map/datasources/datasource_spot_test.dart
@@ -38,7 +38,7 @@ import '../../mocks/mocks_spots.dart';
       test("deve alterar um spot", () async {
         await fakeFirestore.collection("spots")
           .doc("1")
-          .set(testPico.toJson());
+          .set(testPico.toMap());
 
         var newPico = testPico
           .copyWith(
@@ -49,14 +49,14 @@ import '../../mocks/mocks_spots.dart';
 
         
         final dadosAlterados = await dataSource.getbyID("1");
-        expect(dadosAlterados.data, newPico.toJson());
+        expect(dadosAlterados.data, newPico.toMap());
         expect(dadosAlterados.id, equals('1') );
 
       });
 
       test("deve deletar um spot", () async {
         
-        await fakeFirestore.collection("spots").doc("1").set(testPico.toJson());
+        await fakeFirestore.collection("spots").doc("1").set(testPico.toMap());
         
 
         await dataSource.delete("1");
