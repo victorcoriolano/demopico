2a7cec8914b3169caaf181bb85318a7c0632c981
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-18 07:29:38 -0300

    aplicando exception

--- lib/core/common/use_case/pick_image_uc.dart
+++ lib/core/common/use_case/pick_image_uc.dart
@@ -3,6 +3,7 @@
 import 'package:demopico/core/common/data/interfaces/repository/i_pick_image_repository.dart';
 import 'package:demopico/core/common/data/models/upload_file_model.dart';
 import 'package:demopico/core/common/data/services/image_picker_service.dart';
+import 'package:demopico/core/common/errors/domain_failures.dart';
 
 class PickFileUC {
   static PickFileUC? _pickImageUC;
@@ -19,7 +20,7 @@ class PickFileUC {
     try {
       final files = await repositoryIMP.pickImages();
       
-      if (files.length > 3) throw Exception("Limite de 3 arquivos");
+      if (files.length > 3) throw FileLimitExceededFailure();
 
       return files;
     } catch (e) {
