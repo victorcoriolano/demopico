476c45ff3f5f718e5d039c76e5f6dd3bf0700d50
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-19 10:17:59 -0300

    TODO: implementar testes de get posts

--- test/test_post/infra/repository/post_repository_test.dart
+++ test/test_post/infra/repository/post_repository_test.dart
@@ -1,6 +1,7 @@
 import 'package:demopico/core/common/data/enums/collections.dart';
 import 'package:demopico/features/external/datasources/firebase/remote/crud_firebase.dart';
 import 'package:demopico/features/profile/infra/datasource/firebase_post_datasource.dart';
+import 'package:demopico/features/profile/infra/repository/post_repository.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:test/test.dart';
 
@@ -9,13 +10,23 @@ import '../../../mocks/mocks_post.dart';
 void main(){
   group("Deve testar o datasource de postagens ", (){
     late FirebasePostDatasource firebasePostDatasource;
+    late PostRepository postRepository;
     late FakeFirebaseFirestore firestore;
 
-    setUpAll((){
+    setUpAll(() async {
       firestore = FakeFirebaseFirestore();
       firebasePostDatasource = FirebasePostDatasource(
         crudFirebase: CrudFirebase.test(
-          table: Collections.posts, firestoreTest: firestore));
+          table: Collections.posts, firestoreTest: firestore
+        )
+      );
+
+      postRepository = PostRepository(postDatasource: firebasePostDatasource);
+
+      await Future.wait([
+          firestore.collection("posts").doc("123").set(listDto[0].data),
+          firestore.collection("posts").doc("456").set(listDto[1].data),
+        ]);
     });
 
     tearDown(() {
@@ -23,9 +34,9 @@ void main(){
     });
 
     test("deve criar um post corretamente", () async {
-      final dto = await firebasePostDatasource.createPost(listDto[0]);
-      expect(dto, isNotNull);
-      expect(dto.data["nome"], "João Silva");
+      final model = await postRepository.createPost(mockPost1);
+      expect(model, isNotNull);
+      expect(model.nome, "João Silva");
     });
 
     test("deve retornar uma lista de postagens", ()  async{
@@ -39,7 +50,7 @@ void main(){
       final dto = await firebasePostDatasource.updatePost(listDto[1]);
       expect(dto.data["nome"], "Tete da Silva");
     });
-
+    
     test("deve retornar uma postagem por id ", ()  async{
       await firestore.collection("posts").doc("123").set(listDto[0].data);
       final dto = await firebasePostDatasource.getPostbyID("123");
