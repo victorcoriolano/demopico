e729f953ff84f4ddb72f2f3b288d9a8a26168dc5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-30 16:38:12 -0300

    pagina de criação de post inicial

---
+++ lib/features/profile/presentation/pages/create_post_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CreatePostPage extends StatelessWidget {
+  const CreatePostPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Criar Postagem"),
+        
+      ),
+      body: Column(
+        children: [
+          //anexar imagem ou video
+
+          //anexar pico reference 
+
+          //descrição 
+
+        ],
+      ),
+    );
+  }
+}
