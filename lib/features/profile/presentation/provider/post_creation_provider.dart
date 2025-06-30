f42e5642e3fe4a2c6054dacdf67d05a5ca576821
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-30 19:19:57 -0300

    passando file model no provider

--- lib/features/profile/presentation/provider/post_creation_provider.dart
+++ lib/features/profile/presentation/provider/post_creation_provider.dart
@@ -1,18 +1,19 @@
 
+import 'package:demopico/core/common/data/models/file_model.dart';
 import 'package:flutter/material.dart';
 
 class PostCreationProvider extends ChangeNotifier {
-  final List<String> _mediaPaths = [];
+  final List<FileModel> _filesModels = [];
   String _description = '';
   String? _selectedSpotId;
 
-  List<String> get mediaPaths => _mediaPaths;
+  List<FileModel> get filesModels => _filesModels;
   String get description => _description;
   String? get selectedSpotId => _selectedSpotId;
 
   void removeMedia(int index) {
-    if (index >= 0 && index < _mediaPaths.length) {
-      _mediaPaths.removeAt(index);
+    if (index >= 0 && index < _filesModels.length) {
+      _filesModels.removeAt(index);
       notifyListeners();
     }
   }
