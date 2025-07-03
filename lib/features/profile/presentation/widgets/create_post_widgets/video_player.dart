bd8332b2c2e012b5e182e773953155295408116c
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-02 21:31:51 -0300

    widget do video

---
+++ lib/features/profile/presentation/widgets/create_post_widgets/video_player.dart
@@ -0,0 +1,64 @@
+import 'package:demopico/core/common/data/models/file_model.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class MyVideoPlayer extends StatefulWidget {
+  final FileModel? videoFile;
+  const MyVideoPlayer({super.key, required this.videoFile});
+
+  @override
+  State<MyVideoPlayer> createState() => _MyVideoPlayerState();
+}
+
+class _MyVideoPlayerState extends State<MyVideoPlayer> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoFile!.filePath!)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: Stack(children:[ 
+                    VideoPlayer(_controller),
+                    Positioned(
+                      bottom: 10,
+                      left: 10,
+                      child: IconButton(
+                        icon: Icon(
+                          _controller.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _controller.value.isPlaying
+                                ? _controller.pause()
+                                : _controller.play();
+                          });
+                        },
+                      ),
+                    ),])
+                )
+              : Container(),
+        ),
+    );
+  }
+}
