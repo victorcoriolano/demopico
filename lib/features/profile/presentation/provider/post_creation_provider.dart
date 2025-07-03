2c2a30312999cdfe43181fb50caa62715da0639f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-03 17:07:46 -0300

    implementando criar postagem passando o novo fluxo de upload

--- lib/features/profile/presentation/provider/post_creation_provider.dart
+++ lib/features/profile/presentation/provider/post_creation_provider.dart
@@ -2,8 +2,9 @@
 import 'package:demopico/core/common/files/models/file_model.dart';
 import 'package:demopico/core/common/errors/domain_failures.dart';
 import 'package:demopico/core/common/errors/failure_server.dart';
+import 'package:demopico/core/common/files/models/upload_result_file_model.dart';
+import 'package:demopico/core/common/files/services/upload_service.dart';
 import 'package:demopico/core/common/usecases/pick_files_uc.dart';
-import 'package:demopico/core/common/usecases/save_image_uc.dart';
 import 'package:demopico/features/profile/domain/models/post.dart';
 import 'package:demopico/features/profile/domain/usecases/create_post_uc.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
@@ -12,26 +13,30 @@ import 'package:flutter/material.dart';
 class PostCreationProvider extends ChangeNotifier {
 
   final CreatePostUc createPostUc;
+  final UploadService _uploadService;
   final PickFileUC pickFileUC;
-  final SaveImageUC saveImageUC;
 
   PostCreationProvider({
-    required this.saveImageUC,
-    required this.createPostUc, 
-    required this.pickFileUC});
+    required UploadService uploadService,
+    required this.pickFileUC,
+    required this.createPostUc,}): _uploadService = uploadService;
 
 
   final List<FileModel> _filesModels = [];
   String _description = '';
   String? _selectedSpotId;
   String? _messageError;
+  final List<String> _imgUrls = [];
+  double progress = 0.0;
+  final List<Post> _posts = [];
+  
+  
 
   String get messageError => _messageError ?? '';
   List<FileModel> get filesModels => _filesModels;
   String get description => _description;
   String? get selectedSpotId => _selectedSpotId;
-
-
+  
   void removeMedia(int index) {
     if (index >= 0 && index < _filesModels.length) {
       _filesModels.removeAt(index);
@@ -63,26 +68,65 @@ class PostCreationProvider extends ChangeNotifier {
       _messageError = e.toString();
     }
   }
-  Future<Post> createPost(UserM user) async{
+
+
+  Future<void> createPost(UserM user) async{
     try{
       if (user.id == null || user.pictureUrl == null || user.name == null) {
         _messageError = "Usuário inválido";
         throw InvalidUserFailure();
       }
+      final uploadResults = _uploadService.uploadFiles(_filesModels);
+      uploadResults.map((task) {
+        task.listen((event) {
+          if (event.state == UploadState.uploading) {
+            progress = event.progress;
+            notifyListeners();
+          } else if (event.state == UploadState.success) {
+            _imgUrls.add(event.url!);
+            debugPrint("Imagem enviada com sucesso: ${event.url}");
+          } else if (event.state == UploadState.failure) {
+            _messageError = "Erro ao fazer upload das imagens";
+          }
+        },
+        onDone: () {
+          debugPrint("Upload concluido com sucesso");
+          
+        } 
+        );
+      });
 
       final newPost = Post(
+        id: "",
         nome: user.name!, 
-        userId: user.id!, 
+        userId: user.id!,
+        spotID: _selectedSpotId ?? '', 
         urlUserPhoto: user.pictureUrl!, 
         description: description, 
-        urlMidia: urlMidia);
+        urlMidia: _imgUrls);
       
-      await createPostUc.execute(newPost)
+      final post = await createPostUc.execute(newPost);
+      _posts.add(post);
+      clear();
     }on Failure catch (e){
       _messageError = e.message;
       notifyListeners();
     }
   }
+  void clear() {
+    _filesModels.clear();
+    _description = '';
+    _selectedSpotId = null;
+    _messageError = null;
+    _imgUrls.clear();
+    progress = 0.0;
+    notifyListeners();
+  }
 
+  @override
+  void dispose() {
+    clear();
+    super.dispose();
+  }
 }
   
