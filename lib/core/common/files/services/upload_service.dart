fb0d2b5bd2fbaac20007b2f4a9c36fd84897669b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-05 19:34:11 -0300

    refatoração do service de upload q retorna as urls corretamente agora

--- lib/core/common/files/services/upload_service.dart
+++ lib/core/common/files/services/upload_service.dart
@@ -1,3 +1,4 @@
+import 'package:demopico/core/common/errors/repository_failures.dart';
 import 'package:demopico/core/common/files/models/file_model.dart';
 import 'package:demopico/core/common/files/models/upload_result_file_model.dart';
 import 'package:demopico/core/common/usecases/upload_file_uc.dart';
@@ -13,18 +14,25 @@ class UploadService {
     _instance ??= UploadService(uploadFileUC: UploadFileUC.getInstance);
     return _instance!;
   }
+  
 
-  List<Stream<UploadStateFileModel>> uploadFiles(List<FileModel> files) {
+  URLs uploadFiles(List<FileModel> files) async {
+    final urls = <String>[];
     try {
-    debugPrint("Iniciando upload de arquivos");
-    final listResultFileModel = uploadFileUC.saveFiles(files);
-    return listResultFileModel;
-  } on Exception catch (e) {
-    debugPrint("Erro ao fazer upload de arquivos: $e");
-    rethrow;
-  }catch (e) {
-    debugPrint("Erro desconhecido ao fazer upload de arquivos: $e");
-    rethrow;
-  }
+      final uploadTask = uploadFileUC.saveFiles(files);
+      
+      for (var task in uploadTask){
+        final result = await task.firstWhere((state) => state.state == UploadState.success);
+        urls.add(result.url!);
+      }
+
+      return urls;
+       
+    }catch (e) {
+      debugPrint("Erro ao fazer upload: $e");
+      throw UploadFileFailure();
+    }
   }
 }
+
+typedef URLs = Future<List<String>>;
