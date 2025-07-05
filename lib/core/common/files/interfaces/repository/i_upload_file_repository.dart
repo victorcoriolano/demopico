5bc9f1a6022bca4c506a2df88baff725514a8713
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-05 19:31:57 -0300

    renomeando repository

--- lib/core/common/files/interfaces/repository/i_upload_file_repository.dart
+++ lib/core/common/files/interfaces/repository/i_upload_file_repository.dart
@@ -4,7 +4,7 @@
 import 'package:demopico/core/common/files/interfaces/datasource/i_upload_task_datasource.dart';
 import 'package:demopico/core/common/files/models/file_model.dart';
 
-abstract class ISaveFileRepository {
-  List<UploadTaskInterface> saveFiles(List<FileModel> files);
+abstract class IUploadFileRepository {
+  ListUploadTask saveFiles(List<FileModel> files);
 }
 
