320d9264c2c4e3827aceaae8da5d915edbca3cc2
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-07 21:01:58 -0300

    criando objeto para facilitar o display de arquivos a partir da url passando o type

---
+++ lib/features/profile/presentation/view_objects/media_url_item.dart
@@ -0,0 +1,11 @@
+import 'package:demopico/core/common/files_manager/models/file_model.dart';
+
+class MediaUrlItem {
+  final String url;
+  final ContentType contentType;
+
+  MediaUrlItem({
+    required this.url,
+    required this.contentType,
+  });
+}
