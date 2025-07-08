3a82bd503694ab03b47f75068651e1a2d6f4115a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-08 07:39:21 -0300

    habilitando persistencia local no firebase

--- lib/features/external/datasources/firebase/remote/firestore.dart
+++ lib/features/external/datasources/firebase/remote/firestore.dart
@@ -4,6 +4,10 @@ class Firestore {
   static FirebaseFirestore? _firestore;
   static FirebaseFirestore get getInstance {
     _firestore ??= FirebaseFirestore.instance;
+    _firestore!.settings = const Settings(
+      persistenceEnabled: true,
+      cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+    );
     return _firestore!;
   }
 }
