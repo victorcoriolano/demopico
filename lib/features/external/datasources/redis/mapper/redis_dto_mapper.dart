eee946775ca1b6103c4e18fca413de55f2dafeb3
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-07-08 14:07:52 -0300

    altterações

--- lib/features/external/datasources/redis/mapper/redis_dto_mapper.dart
+++ lib/features/external/datasources/redis/mapper/redis_dto_mapper.dart
@@ -3,24 +3,23 @@ import 'package:demopico/features/external/datasources/redis/dto/redis_dto.dart'
 
 class RedisDtoMapper<Model> implements IMapperDto<Model, RedisDto> {
   // aplica as funções de comportamento na composição da classe 
-  final Model Function(Map<String, dynamic> map, String id) fromJson;
+  final Model Function(Map<String, dynamic> map) fromJson;
   final Map<String, dynamic> Function(Model model) toMap;
-  final String Function(Model model) getId;
+
 
   RedisDtoMapper({
     required this.fromJson,
     required this.toMap,
-    required this.getId,
   });
 
   @override
   Model toModel(RedisDto dto) {
-    return this.fromJson(dto.data, dto.id);
+    return this.fromJson(dto.data);
   }
 
   @override
   RedisDto toDTO(Model model) {
-    return RedisDto(id: getId(model), data: toMap(model));
+    return RedisDto(data: toMap(model));
   }
 
 }
