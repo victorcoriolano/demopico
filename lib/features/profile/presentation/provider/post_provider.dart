377cb82c8eb0022fa6fbb844296c6ac71a40f3a7
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-08 19:30:05 -0300

    formatação

--- lib/features/profile/presentation/provider/post_provider.dart
+++ lib/features/profile/presentation/provider/post_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:demopico/core/common/files_manager/models/file_model.dart';
 import 'package:demopico/core/common/util/file_manager/pick_files_uc.dart';
 import 'package:demopico/core/common/errors/domain_failures.dart';
@@ -14,7 +13,6 @@ import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:flutter/material.dart';
 
 class PostProvider extends ChangeNotifier {
-
   //TODO: Adicionar lógica de cache de imagens e videos para otimizar o carregamento
 
   final CreatePostUc _createPostUc;
@@ -22,16 +20,15 @@ class PostProvider extends ChangeNotifier {
   final GetPostUc _getPostUc;
   final DeletePostUc _deletePostUc;
 
-  PostProvider({
-    required DeletePostUc deleteUc,
-    required CreatePostUc createPostUc,
-    required PickFileUC pickFileUC,
-    required GetPostUc getPosts}) 
-    : _createPostUc = createPostUc,
-      _pickFileUC = pickFileUC,
-      _getPostUc = getPosts,
-      _deletePostUc = deleteUc;
-
+  PostProvider(
+      {required DeletePostUc deleteUc,
+      required CreatePostUc createPostUc,
+      required PickFileUC pickFileUC,
+      required GetPostUc getPosts})
+      : _createPostUc = createPostUc,
+        _pickFileUC = pickFileUC,
+        _getPostUc = getPosts,
+        _deletePostUc = deleteUc;
 
   final List<FileModel> _filesModels = [];
   final List<FileModel> _videos = [];
@@ -45,8 +42,6 @@ class PostProvider extends ChangeNotifier {
   double progress = 0.0;
   final List<Post> _posts = [];
   bool _isLoading = false;
-  
-  
 
   String get messageError => _messageError ?? '';
   List<FileModel> get filesModels => _filesModels;
@@ -54,7 +49,7 @@ class PostProvider extends ChangeNotifier {
   String? get selectedSpotId => _selectedSpotId;
   bool get isLoading => _isLoading;
   List<Post> get post => _posts;
-  
+
   void removeMedia(int index) {
     if (index >= 0 && index < _filesModels.length) {
       _filesModels.removeAt(index);
@@ -76,12 +71,13 @@ class PostProvider extends ChangeNotifier {
 
   //get media
   void getFile() async {
-    try{
+    try {
       _isLoading = true;
       notifyListeners();
       final files = await _pickFileUC.execute();
       _filesModels.addAll(files);
-      debugPrint("Adicionou: ${_filesModels.length} na lista e arquivos selecionados");
+      debugPrint(
+          "Adicionou: ${_filesModels.length} na lista e arquivos selecionados");
 
       for (var file in _filesModels) {
         // Mapeando os arquivos para imagens e vídeos
@@ -92,8 +88,7 @@ class PostProvider extends ChangeNotifier {
         } else if (file.contentType.isImage) {
           _images.add(file);
           debugPrint("Arquivo de imagem adicionado: ${file.fileName}");
-        }
-        else {
+        } else {
           debugPrint("Não foi possivel mapear o arquivo: ${file.fileName}");
         }
       }
@@ -110,32 +105,33 @@ class PostProvider extends ChangeNotifier {
   }
 
   List<MediaUrlItem> getMediaItemsFor(Post post) {
-  final items = <MediaUrlItem>[];
+    final items = <MediaUrlItem>[];
 
-  items.addAll(post.urlImages.map((e) =>MediaUrlItem(url: e, contentType: MediaType.image)));
-  items.addAll(post.urlVideos?.map((e) =>MediaUrlItem(url: e, contentType: MediaType.video)) ?? []);
+    items.addAll(post.urlImages
+        .map((e) => MediaUrlItem(url: e, contentType: MediaType.image)));
+    items.addAll(post.urlVideos
+            ?.map((e) => MediaUrlItem(url: e, contentType: MediaType.video)) ??
+        []);
 
-  return items;
-}
+    return items;
+  }
 
-
-  // Gateway carregar postagem para ui 
+  // Gateway carregar postagem para ui
   // retorna caso a requisição já tenha sido feita postagens ja tenha sido feitas
   Future<void> loadPosts(String userId) async {
     if (_posts.isNotEmpty) return;
     getPosts(userId);
   }
 
-Future<void> deletePost(Post postagem) async {
-  final urls = <String>[];
-  urls.addAll(postagem.urlImages);
-  if (postagem.urlVideos != null) urls.addAll(postagem.urlVideos!);
-  await _deletePostUc.execute(postagem.id, urls);
-}
+  Future<void> deletePost(Post postagem) async {
+    final urls = <String>[];
+    urls.addAll(postagem.urlImages);
+    if (postagem.urlVideos != null) urls.addAll(postagem.urlVideos!);
+    await _deletePostUc.execute(postagem.id, urls);
+  }
 
-
-  Future<void> createPost(UserM user, TypePost type) async{
-    try{
+  Future<void> createPost(UserM user, TypePost type) async {
+    try {
       _isLoading = true;
       notifyListeners();
       if (user.id == null || user.pictureUrl == null || user.name == null) {
@@ -143,38 +139,41 @@ Future<void> deletePost(Post postagem) async {
         _isLoading = false;
         throw InvalidUserFailure();
       }
-        //cria urls separadas para imagens e vídeos
-        final urlsimages = await UploadService.getInstance.uploadFiles(_images, "posts/images");
-        _imgUrls.addAll(urlsimages);
+      //cria urls separadas para imagens e vídeos
+      final urlsimages =
+          await UploadService.getInstance.uploadFiles(_images, "posts/images");
+      _imgUrls.addAll(urlsimages);
 
-        if (_videoUrls.isNotEmpty){
-          final urlvideos = await UploadService.getInstance.uploadFiles(_videos, "posts/videos");
-          _videoUrls.addAll(urlvideos);
-        }
-        
+      if (_videos.isNotEmpty) {
+        final urlvideos = await UploadService.getInstance
+            .uploadFiles(_videos, "posts/videos");
+        _videoUrls.addAll(urlvideos);
+      }
 
       final newPost = Post(
-        id: "",
-        typePost: type,
-        nome: user.name!, 
-        userId: user.id!,
-        spotID: _selectedSpotId ?? '', 
-        urlUserPhoto: user.pictureUrl!, 
-        urlVideos: _videoUrls.isEmpty ? null : _videoUrls,
-        description: description, 
-        urlImages: _imgUrls);
-      
+          id: "",
+          typePost: type,
+          nome: user.name!,
+          userId: user.id!,
+          spotID: _selectedSpotId ?? '',
+          urlUserPhoto: user.pictureUrl!,
+          urlVideos: _videoUrls.isEmpty ? null : _videoUrls,
+          description: description,
+          urlImages: _imgUrls);
+
       final post = await _createPostUc.execute(newPost);
       _posts.add(post);
       _isLoading = false;
       notifyListeners();
       clear();
-    }on Failure catch (e){
+    } on Failure catch (e) {
       _messageError = e.message;
       _isLoading = false;
       notifyListeners();
     }
   }
+
+  
 
   Future<void> getPosts(String userId) async {
     try {
@@ -191,17 +190,16 @@ Future<void> deletePost(Post postagem) async {
       _posts.addAll(myPosts);
       _isLoading = false;
       notifyListeners();
-    }on Failure catch (e) {
+    } on Failure catch (e) {
       _messageError = e.message;
       _isLoading = false;
       notifyListeners();
-    }catch (e) {
+    } catch (e) {
       _messageError = 'Erro desconhecido ao buscar postagens: ${e.toString()}';
       _isLoading = false;
       notifyListeners();
     }
   }
-
 
   void clear() {
     _filesModels.clear();
@@ -220,5 +218,3 @@ Future<void> deletePost(Post postagem) async {
     super.dispose();
   }
 }
-  
-
