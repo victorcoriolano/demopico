77585543c86451146f3f4443381d58296c883014
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-07-08 15:59:28 -0300

    controller widget

--- lib/features/profile/presentation/pages/user_controller_page.dart
+++ lib/features/profile/presentation/pages/user_controller_page.dart
@@ -1,28 +1,26 @@
-import 'package:demopico/features/profile/presentation/widgets/profile_navigator_widget.dart';
+import 'package:demopico/features/profile/presentation/pages/profile_page.dart';
+import 'package:demopico/features/profile/presentation/provider/screen_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:demopico/features/profile/presentation/widgets/profile_navigator_widget.dart';
 
-class UserControllerPage extends StatefulWidget {
+
+class UserControllerPage extends StatelessWidget {
   const UserControllerPage({super.key});
 
-  @override
-  State<UserControllerPage> createState() => _UserControllerPageState();
-}
+  final List<Widget> _pages = const [
+    ProfilePage(),
+    Center(child: Text('Procurar')),
+    Center(child: Text('Página Mensagens')),
+  ];
 
-class _UserControllerPageState extends State<UserControllerPage>{
-  ProfileNavigatorWidget profileNavigatorWidget = ProfileNavigatorWidget();
   @override
   Widget build(BuildContext context) {
-  
-    return Scaffold(
-      body: Column(
-        children: [
-         
+    final currentIndex = context.watch<ScreenProvider>().currentIndex;
 
-       
-        ],
-      ),
-      bottomNavigationBar: ProfileNavigatorWidget(),
+    return Scaffold(
+      body: _pages[currentIndex],
+      bottomNavigationBar: const ProfileNavigatorWidget(),
     );
   }
 }
-
