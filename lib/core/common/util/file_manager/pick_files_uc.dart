d6d2c5751765d83e28791caa84e2e844547c1e32
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-10 16:04:13 -0300

    adicionando debug prints

--- lib/core/common/util/file_manager/pick_files_uc.dart
+++ lib/core/common/util/file_manager/pick_files_uc.dart
@@ -1,9 +1,11 @@
 
 
 import 'package:demopico/core/common/errors/domain_failures.dart';
+import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/core/common/files_manager/interfaces/repository/i_pick_image_repository.dart';
 import 'package:demopico/core/common/files_manager/models/file_model.dart';
 import 'package:demopico/core/common/files_manager/services/image_picker_service.dart';
+import 'package:flutter/material.dart';
 
 class PickFileUC {
   static PickFileUC? _pickImageUC;
@@ -19,16 +21,21 @@ class PickFileUC {
 
   Future<List<FileModel>> execute() async {
     try {
-      
+      debugPrint("Chamou use case de pegar múltiplos arquivos");
+      debugPrint("Limite de files atuais: $limit");
 
       if(limit == 0) throw FileLimitExceededFailure();
 
       final files = await repositoryIMP.pickMultipleMedia(limit);
+
+      if(files.any((file) => file.contentType == ContentType.unavailable)) throw InvalidFormatFileFailure();
+
       limit -= files.length;
 
       return files;
-    } catch (e) {
-      throw Exception(e);
+    }on Failure catch (e) {
+      debugPrint("Erro ao selecionar file caiu no use case: $e");
+      rethrow;
     }
   }
 }
