79dc31954cea42a8db5b4096e011a4eabd0c5202
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-10 16:38:48 -0300

    adicioanando método de pegar apenas uma imagem ao repository de pegar arquivos

--- lib/core/common/files_manager/interfaces/repository/i_pick_image_repository.dart
+++ lib/core/common/files_manager/interfaces/repository/i_pick_image_repository.dart
@@ -4,5 +4,6 @@ import 'package:demopico/core/common/files_manager/models/file_model.dart';
 abstract interface class IPickFileRepository {
   Future<List<FileModel>> pickMultipleMedia(int limite);
   Future<List<FileModel>> pickImages(int limite);
+  Future<FileModel> pickImage();
   Future<FileModel> pickVideo();
 }
