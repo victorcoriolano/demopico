9ef98853c43c8fb38f0a7b4eb805e25fb85d1178
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-11 18:48:03 -0300

    removendo código q não ira ser usado mais

--- lib/features/profile/presentation/widgets/profile_data/profile_configure_widget.dart
+++ lib/features/profile/presentation/widgets/profile_data/profile_configure_widget.dart
@@ -1,15 +1,11 @@
-import 'package:demopico/core/app/home_page.dart';
-import 'package:demopico/features/profile/presentation/widgets/profile_data/about_page_widget.dart';
-import 'package:demopico/features/user/presentation/controllers/auth_user_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:provider/provider.dart';
 
 class ProfileConfigureWidget extends StatefulWidget {
-  final TextEditingController bioController;
+  final VoidCallback onPressed;
+
   const ProfileConfigureWidget({
     super.key,
-    required this.bioController,
+    required this.onPressed,
   });
 
   @override
@@ -17,88 +13,6 @@ class ProfileConfigureWidget extends StatefulWidget {
 }
 
 class _ProfileConfigureWidgetState extends State<ProfileConfigureWidget> {
-  void _showSettingsDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text(
-            'Configurações',
-            style: TextStyle(
-              color: Color.fromARGB(255, 0, 0, 0),
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          content: SizedBox(
-            height: 200,
-            width: 100,
-            child: ListView(
-              shrinkWrap: true,
-              itemExtent: 50,
-              children: [
-                ListTile(
-                  title: const Text('Sobre o Aplicativo'),
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => const AboutPage()),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: const Text('Fazer Logout'),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Logout'),
-                          content:
-                              const Text('Tem certeza que deseja sair da sua conta?'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text(
-                                'Cancelar',
-                                style:
-                                    TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                              ),
-                            ),
-                            TextButton(
-                              child: const Text('Sair'),
-                              onPressed: () async {
-                                await context.read<AuthUserProvider>().logout();
-                                Get.offAll(
-                                  () => const HomePage(),
-                                  transition: Transition.rightToLeftWithFade,
-                                );
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Voltar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -108,8 +22,9 @@ class _ProfileConfigureWidgetState extends State<ProfileConfigureWidget> {
         icon: const Icon(Icons.settings),
         iconSize: 30,
         color: const Color.fromARGB(255, 0, 0, 0),
-        onPressed: _showSettingsDialog,
+        onPressed: widget.onPressed,
       ),
     );
   }
 }
+
