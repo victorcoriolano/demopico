570d43db56ee84ed182884485fd1628db3b75dae
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-14 21:26:22 -0300

    usando type para livrar a abstração não conhecer o tipo do dto

--- lib/features/mapa/data/data_sources/interfaces/i_spot_datasource.dart
+++ lib/features/mapa/data/data_sources/interfaces/i_spot_datasource.dart
@@ -1,11 +1,11 @@
 
-import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
 import 'package:demopico/features/mapa/domain/entities/filters.dart';
 
-abstract class ISpotRemoteDataSource {
-  Future<FirebaseDTO> create(FirebaseDTO data);
-  Future<void> update(FirebaseDTO data);
-  Stream<List<FirebaseDTO>> load([Filters? filtro]);
-  Future<FirebaseDTO> getbyID(String id);
+abstract class ISpotRemoteDataSource<DTO> {
+  Future<DTO> create(DTO data);
+  Future<void> update(DTO data);
+  Stream<List<DTO>> load([Filters? filtro]);
+  Future<DTO> getbyID(String id);
+  Future<List<DTO>> getList(String id);
   Future<void> delete(String id);
 }
