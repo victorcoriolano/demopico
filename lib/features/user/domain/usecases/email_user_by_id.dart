33215148e75f8c33702aa40aec7c01a6c02d14c3
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-16 19:35:38 -0300

    removendo nullable - lançará uma exception

--- lib/features/user/domain/usecases/email_user_by_id.dart
+++ lib/features/user/domain/usecases/email_user_by_id.dart
@@ -1,11 +1,13 @@
+import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:flutter/foundation.dart';
 
 class EmailUserByIDUc {
   static EmailUserByIDUc? _emailUserByIDUc;
   static EmailUserByIDUc get getInstance {
     _emailUserByIDUc ??= EmailUserByIDUc(
-      userDatabaseRepositoryIMP: UserFirebaseRepository.getInstance,
+      userDatabaseRepositoryIMP: UserRepositoryImpl.getInstance,
     );
     return _emailUserByIDUc!;
   }
@@ -14,11 +16,12 @@ class EmailUserByIDUc {
 
   EmailUserByIDUc({required this.userDatabaseRepositoryIMP});
 
-  Future<String?> getEmail(String uid) async {
+  Future<String> getEmail(String uid) async {
     try {
       return await userDatabaseRepositoryIMP.getEmailByUserID(uid);
-    } catch (e) {
-      return null;
+    } on Failure catch (e) {
+      debugPrint("UC - Erro ao pegar o email: $e");
+      rethrow;
     }
   }
 }
