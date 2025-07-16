dd88dc7bb1b045c57a6f0e82f0771444f1734112
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-16 19:43:27 -0300

    passando classes de acesso de dados na pasta de data source

---
+++ lib/features/user/infra/datasource/remote/user_firebase_service.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demopico/core/common/errors/repository_failures.dart';
+import 'package:demopico/core/common/files_manager/enums/collections.dart';
+import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
+import 'package:demopico/features/external/datasources/firebase/remote/crud_firebase.dart';
+import 'package:demopico/features/external/datasources/firebase/remote/firestore.dart';
+import 'package:demopico/features/mapa/data/mappers/firebase_errors_mapper.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_service.dart';
+
+class UserFirebaseDataSource implements IUserDataSource<FirebaseDTO> {
+
+  static UserFirebaseDataSource? _userFirebaseService;
+  static UserFirebaseDataSource get getInstance {
+    _userFirebaseService ??=
+        UserFirebaseDataSource(datasource: CrudFirebase(collection: Collections.users, firestore: Firestore.getInstance));
+    return _userFirebaseService!;
+  }
+
+  final CrudFirebase _dataSource; //TODO: ARRUMAR UM JEITO DE USAR A INTERFACE 
+
+  UserFirebaseDataSource({
+    required CrudFirebase datasource,
+  }): _dataSource = datasource;
+
+
+  @override
+  Future<void> addUserDetails(FirebaseDTO newUser) async {
+    try {
+      
+      await _dataSource.setData(newUser.id, newUser);
+    } on FirebaseException catch (firebaseException) {
+      throw FirebaseErrorsMapper.map(firebaseException);
+    } on Exception catch (exception) {
+      throw UnknownFailure(originalException: exception);
+    } catch (unknown) {
+      throw UnknownFailure(unknownError: unknown);
+    }
+  }
+
+  @override
+  Future<FirebaseDTO> getUserDetails(String uid) async {
+    try {
+      final dto = await _dataSource.read(uid);
+      return dto;
+    } on FirebaseException catch (firebaseException) {
+      throw FirebaseErrorsMapper.map(firebaseException);
+    } on Exception catch (exception) {
+      throw UnknownFailure(originalException: exception);
+    } catch (unknown) {
+      throw UnknownFailure(unknownError: unknown);
+    }
+  }
+  
+  @override
+  Future<String> getUserData(String id, String field) async {
+    try {
+      final dto = await _dataSource.read(id);
+      return dto.data[field];
+    } on FirebaseException catch (firebaseException) {
+      throw FirebaseErrorsMapper.map(firebaseException);
+    } on Exception catch (exception) {
+      throw UnknownFailure(originalException: exception);
+    } catch (unknown) {
+      throw UnknownFailure(unknownError: unknown);
+    }
+  }
+  
+  @override
+  Future<FirebaseDTO> getUserByField(String field, value) async {
+    final query = await _dataSource.dataSource.collection("users").where(field, isEqualTo: value).get();
+    final data = query.docs.first;
+
+    final dto = FirebaseDTO(id: data.id, data: data.data());
+    return dto;
+  }
+
+  
+}
