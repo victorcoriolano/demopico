5d00a928a5450f886b7016a7468f9c602ed58d8a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-15 21:10:06 -0300

    retornando lista de picos ao carregar do repository

--- lib/features/mapa/domain/interfaces/i_spot_repository.dart
+++ lib/features/mapa/domain/interfaces/i_spot_repository.dart
@@ -1,11 +1,12 @@
 
 import 'package:demopico/features/mapa/domain/entities/filters.dart';
+import 'package:demopico/features/mapa/domain/entities/pico_entity.dart';
 import 'package:demopico/features/mapa/domain/models/pico_model.dart';
 
 abstract class ISpotRepository {
   Future<PicoModel> createSpot(PicoModel pico);
   Future<PicoModel> updateSpot(PicoModel pico);
-  Stream<List<PicoModel>> loadSpots([Filters? filtro]);
+  Stream<List<Pico>> loadSpots([Filters? filtro]);
   Future<PicoModel> getPicoByID(String id);
   Future<List<PicoModel>> getMySpot(String userID);
   Future<void> deleteSpot(String id);
