b76791b66531045009a5fff10a9e9ec585c97f48
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-16 19:10:26 -0300

    refatorações no crud firebase - adicionando método para ler com filtro, adicionando proteção do datasource e método get para pegar o datasource e métodod e setar dados em um id específico

--- lib/features/external/datasources/firebase/remote/crud_firebase.dart
+++ lib/features/external/datasources/firebase/remote/crud_firebase.dart
@@ -7,10 +7,10 @@ import 'package:demopico/features/external/interfaces/i_crud_datasource.dart';
 import 'package:demopico/features/mapa/data/mappers/firebase_errors_mapper.dart';
 import 'package:flutter/foundation.dart';
 
-class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
+class CrudFirebase implements ICrudDataSource<FirebaseDTO, FirebaseFirestore> {
 
   Collections collection;
-  final FirebaseFirestore firestore;
+  final FirebaseFirestore _firestore;
   
 
   //singleton
@@ -20,13 +20,9 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
     return _instance!;
   }
 
-  //for testing
-  CrudFirebase.test({
-    required this.collection, 
-    required FirebaseFirestore firestoreTest}): firestore = firestoreTest;
 
   //constructor
-  CrudFirebase({required this.collection, required this.firestore});
+  CrudFirebase({required this.collection, required FirebaseFirestore firestore}): _firestore = firestore;
     
 
   
@@ -38,7 +34,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Future<FirebaseDTO> create(FirebaseDTO model) async{
     try {
-      await firestore.collection(collection.name).add(model.data);
+      await _firestore.collection(collection.name).add(model.data);
       return model;
     } on FirebaseException catch (e) {
       throw FirebaseErrorsMapper.map(e);
@@ -48,7 +44,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Future<FirebaseDTO> read(String id)async{
     try {
-      final docRf = await firestore.collection(collection.name).doc(id).get(
+      final docRf = await _firestore.collection(collection.name).doc(id).get(
         GetOptions(source: Source.serverAndCache)
       );
       
@@ -63,7 +59,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Future<FirebaseDTO> update(FirebaseDTO firebaseDto) async {
     try {
-      await firestore.collection(collection.name).doc(firebaseDto.id).update(firebaseDto.data);
+      await _firestore.collection(collection.name).doc(firebaseDto.id).update(firebaseDto.data);
       return firebaseDto;
     } on FirebaseException catch (e) {
       throw FirebaseErrorsMapper.map(e);
@@ -72,7 +68,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Future<void> delete(String id) async {
     try {
-      await firestore.collection(collection.name).doc(id).delete();
+      await _firestore.collection(collection.name).doc(id).delete();
     } on FirebaseException catch (e) {
       throw FirebaseErrorsMapper.map(e);
     }
@@ -81,7 +77,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Future<List<FirebaseDTO>>   readAll() async {
     try{
-      final data = await firestore.collection(collection.name).get(
+      final data = await _firestore.collection(collection.name).get(
         GetOptions(source: Source.serverAndCache)
       );
       return data.docs.map((doc) =>
@@ -102,7 +98,7 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   @override
   Stream<List<FirebaseDTO>> watch() {
     try {
-      return firestore.collection(collection.name).snapshots().map((snapshot) =>
+      return _firestore.collection(collection.name).snapshots().map((snapshot) =>
         snapshot.docs.map((doc) => FirebaseDTO(
           id: doc.id,
           data: doc.data()
@@ -117,9 +113,42 @@ class CrudFirebase implements ICrudDataSource<FirebaseDTO> {
   }
   
   @override
-  Future<List<FirebaseDTO>> readWithFilter(String field, String value) async {
+  Future<List<FirebaseDTO>> readAllWithFilter(String field, String value) async {
     try {
-      final query = await firestore.collection(collection.name).where(field, isEqualTo: value).get();
+      final query = await _firestore.collection(collection.name).where(field, isEqualTo: value).get();
+      return query.docs.map((doc) {
+        return FirebaseDTO(id: doc.id, data: doc.data());
+      }).toList();
+    } on FirebaseException catch (firebaseException) {
+      throw FirebaseErrorsMapper.map(firebaseException);
+    } on Exception catch (exception) {
+      throw UnknownFailure(originalException: exception);
+    } catch (unknown) {
+      throw UnknownFailure(unknownError: unknown);
+    }
+  }
+  
+  @override
+  Future<FirebaseDTO> setData(String id, FirebaseDTO data) async {
+    try {
+      await _firestore.collection(collection.name).doc(id).set(data.data);
+      return data.copyWith(id: id);
+    } on FirebaseException catch (firebaseException) {
+      throw FirebaseErrorsMapper.map(firebaseException);
+    } on Exception catch (exception) {
+      throw UnknownFailure(originalException: exception);
+    } catch (unknown) {
+      throw UnknownFailure(unknownError: unknown);
+    }
+  }
+  
+  @override
+  FirebaseFirestore get dataSource => _firestore;
+  
+  @override
+  Future<List<FirebaseDTO>> readWithFilter(String field, String value) async {
+     try {
+      final query = await _firestore.collection(collection.name).where(field, isEqualTo: value).get();
       return query.docs.map((doc) {
         return FirebaseDTO(id: doc.id, data: doc.data());
       }).toList();
