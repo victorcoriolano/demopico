691f8a1da7bea5af019a2cdc570eb3ca6d6ad746
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 10:07:20 -0300

    repassando parâmetro nomeado nas failures do repository

--- lib/core/common/errors/repository_failures.dart
+++ lib/core/common/errors/repository_failures.dart
@@ -1,105 +1,110 @@
 import 'package:demopico/core/common/errors/failure_server.dart';
 
 abstract class RepositoryFailures extends Failure {
-  RepositoryFailures(super.message, {super.code, super.originalException, super.stackTrace});
+  RepositoryFailures({required super.message, super.code, super.originalException, super.stackTrace});
 }
 
 class NetworkFailure extends RepositoryFailures {
-  NetworkFailure({super.originalException}): super("Erro de conexão", code: 'NETWORK_FAILURE');
+  NetworkFailure({super.originalException}): super(message: "Erro de conexão", code: 'NETWORK_FAILURE');
 }
 
 class TimeoutFailure extends RepositoryFailures {
-  TimeoutFailure({super.originalException}): super("Tempo limite excedido",code: 'TIMEOUT_FAILURE');
+  TimeoutFailure({super.originalException}): super(message: "Tempo limite excedido",code: 'TIMEOUT_FAILURE');
 }
 
 class UnknownError extends RepositoryFailures {
-  UnknownError(super.message, {super.stackTrace}): super(code: 'UNKNOWN_ERROR');
+  UnknownError( {required super.message, super.stackTrace}): super(code: 'UNKNOWN_ERROR');
 }
 
 
 class CacheFailure extends RepositoryFailures {
-  CacheFailure(super.message, {super.originalException}): super(code: 'CACHE_FAILURE');
+  CacheFailure( {required super.message, super.originalException}): super(code: 'CACHE_FAILURE');
 }
 
 class DatabaseFailure extends RepositoryFailures {
-  DatabaseFailure(super.message, {super.originalException}): super(code: 'DATABASE_FAILURE');
+  DatabaseFailure(String s, {super.originalException}): super(message:"", code: 'DATABASE_FAILURE');
 }
 
 class UnavailableFailure extends RepositoryFailures {
-  UnavailableFailure({super.originalException}): super("Serviço indisponível",code: 'UNAVAILABLE');
+  UnavailableFailure({super.originalException}): super(message: "Serviço indisponível",code: 'UNAVAILABLE');
 }
 
 class UnauthorizedFailure extends RepositoryFailures {
-  UnauthorizedFailure({super.originalException}): super("Usuário não autorizado",code: 'UNAUTHORIZED');
+  UnauthorizedFailure({super.originalException}): super(message: "Usuário não autorizado",code: 'UNAUTHORIZED');
 }
 
 class UnauthenticatedFailure extends RepositoryFailures {
-  UnauthenticatedFailure({super.originalException}): super("Usuário não autenticado",code: 'UNAUTHENTICATED');
+  UnauthenticatedFailure({super.originalException}): super(message: "Usuário não autenticado",code: 'UNAUTHENTICATED');
 }
 
 class ExpiredTokenFailure extends RepositoryFailures {
-  ExpiredTokenFailure({super.originalException}): super("Token expirado, refaça login",code: 'EXPIRED_TOKEN');
+  ExpiredTokenFailure({super.originalException}): super(message: "Token expirado, refaça login",code: 'EXPIRED_TOKEN');
 }
 
 class AccountExistsFailure extends RepositoryFailures {
-  AccountExistsFailure({super.originalException}): super("Conta já existe",code: 'ACCOUNT_EXISTS');
+  AccountExistsFailure({super.originalException}): super(message: "Conta já existe",code: 'ACCOUNT_EXISTS');
 }
 
 class DataNotFoundFailure extends RepositoryFailures {
-  DataNotFoundFailure({super.originalException}): super("Dados não encontrados",code: 'DATA_NOT_FOUND');
+  DataNotFoundFailure({super.originalException}): super(message: "Dados não encontrados",code: 'DATA_NOT_FOUND');
 }
 
 // Pico
 class PicoNotFoundFailure extends RepositoryFailures {
-  PicoNotFoundFailure({super.originalException}): super("Pico não encontrado",code: 'PICO_NOT_FOUND');
+  PicoNotFoundFailure({super.originalException}): super(message: "Pico não encontrado",code: 'PICO_NOT_FOUND');
 }
 
 // Usuário
 class UserNotFoundFailure extends RepositoryFailures {
-  UserNotFoundFailure({super.originalException}): super("Usuário não encontrado", code: 'USER_NOT_FOUND');
+  UserNotFoundFailure({super.originalException}): super(message: "Usuário não encontrado", code: 'USER_NOT_FOUND');
 }
 
 // Arquivos 
 
 
 class UploadFileFailure extends RepositoryFailures {
-  UploadFileFailure({super.originalException}): super("Erro ao fazer upload do arquivo", code: 'UPLOAD_FILE_FAILURE');
+  UploadFileFailure({super.originalException}): super(message: "Erro ao fazer upload do arquivo", code: 'UPLOAD_FILE_FAILURE');
 }
 
 class FileNotFoundFailure extends RepositoryFailures {
-  FileNotFoundFailure({super.originalException}): super("Arquivo não encontrado", code: 'FILE_NOT_FOUND');
+  FileNotFoundFailure({super.originalException}): super(message: "Arquivo não encontrado", code: 'FILE_NOT_FOUND');
 }
 
 class LimitExceededFailure extends RepositoryFailures {
-  LimitExceededFailure({super.originalException}): super("Limite de arquivos excedido", code: 'LIMIT_EXCEEDED');
+  LimitExceededFailure({super.originalException}): super(message: "Limite de arquivos excedido", code: 'LIMIT_EXCEEDED');
 }
 
 class OperationCanceledFailure extends RepositoryFailures {
-  OperationCanceledFailure({super.originalException}): super("Operação cancelada", code: 'OPERATION_CANCELED');
+  OperationCanceledFailure({super.originalException}): super(message: "Operação cancelada", code: 'OPERATION_CANCELED');
 }
 
 // Erro desconhecido
 class UnknownFailure extends RepositoryFailures {
   final Object? unknownError;
-  UnknownFailure({super.originalException, super.stackTrace, this.unknownError}): super("Erro desconhecido", code: 'UNKNOWN');
+  UnknownFailure({super.originalException, super.stackTrace, this.unknownError}): super(message: "Erro desconhecido", code: 'UNKNOWN');
 }
 
 // Internal server error 
 class InternalServerErrorFailure extends RepositoryFailures {
-  InternalServerErrorFailure({super.originalException}): super("Erro interno do servidor", code: 'INTERNAL_SERVER_ERROR');
+  InternalServerErrorFailure({super.originalException}): super(message: "Erro interno do servidor", code: 'INTERNAL_SERVER_ERROR');
 }
 
 class EmailAlreadyInUseFailure extends RepositoryFailures {
-  EmailAlreadyInUseFailure({super.originalException}): super("Email já está em uso", code: 'EMAIL_ALREADY_IN_USE');  
+  EmailAlreadyInUseFailure({super.originalException}): super(message: "Email já está em uso", code: 'EMAIL_ALREADY_IN_USE');  
 }
 
 class InvalidEmailFailure extends RepositoryFailures {
-  InvalidEmailFailure({super.originalException}): super("Formato de e-mail inválido", code: 'INVALID_EMAIL');
+  InvalidEmailFailure({super.originalException}): super(message: "Formato de e-mail inválido", code: 'INVALID_EMAIL');
 }
 
 class TooManyAttemptsFailure extends RepositoryFailures {
-  TooManyAttemptsFailure({super.originalException}): super("Limite de tentativas excedido", code: 'TOO_MANY_ATTEMPTS');
+  TooManyAttemptsFailure({super.originalException}): super(message: "Limite de tentativas excedido", code: 'TOO_MANY_ATTEMPTS');
 } 
+
+class DuplicateFailure extends RepositoryFailures {
+
+  DuplicateFailure({required super.message}): super(code: "DUPLICATE_DATA_FAILURE");
+}
 
 
 
