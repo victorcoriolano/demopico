f2e1bbffa3c3639f3af44af593425fd6c851fcd6
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 10:18:00 -0300

    alterando métodos da  interface para métodos mais usuais e necessários

--- lib/features/user/domain/interfaces/i_user_database_repository.dart
+++ lib/features/user/domain/interfaces/i_user_database_repository.dart
@@ -1,9 +1,11 @@
+
 import 'package:demopico/features/user/domain/models/user.dart';
 
-abstract class IUserDatabaseRepository{
+abstract class IUserDataRepository{
   Future<void> addUserDetails(UserM newUser);
-  Future<UserM> getUserDetails(String uid);
-  Future<String> getUserIDByVulgo(String vulgo);
-  Future<String> getEmailByUserID(String uid);
+  Future<UserM> getUserDetailsByID(String uid);
   Future<String> getEmailByVulgo(String vulgo);
+  Future<UserM> updateUserDetails(UserM user);
+  Future<bool> validateDataUserAfter({required String data,required  String field});
+  Future<bool> validateDataUserBefore({required String data, required  String field});
 }
