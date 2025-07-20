38a1c6cce903583f1621a6a067b9855167a72291
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 10:33:06 -0300

    implementação correta do repository do serviço de autenticação - método de login refatorado

--- lib/features/user/infra/repositories/user_auth_firebase_repository.dart
+++ lib/features/user/infra/repositories/user_auth_firebase_repository.dart
@@ -1,10 +1,9 @@
 import 'package:demopico/features/user/domain/entity/user_credentials.dart';
-import 'package:demopico/features/user/domain/enums/sign_methods.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_auth_repository.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_auth_service.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
-import 'package:demopico/features/user/infra/repositories/user_firebase_repository.dart';
+import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 import 'package:demopico/features/user/infra/datasource/remote/user_auth_firebase_service.dart';
 
 
@@ -12,35 +11,15 @@ class UserAuthFirebaseRepository implements IUserAuthRepository {
   static UserAuthFirebaseRepository? _userAuthFirebaseRepository;
 
   static UserAuthFirebaseRepository get getInstance {
-    _userAuthFirebaseRepository ??= UserAuthFirebaseRepository(userAuthServiceIMP: UserAuthFirebaseService.getInstance, userDatabaseRepositoryIMP: UserRepositoryImpl.getInstance);
+    _userAuthFirebaseRepository ??= UserAuthFirebaseRepository(userAuthServiceIMP: UserAuthFirebaseService.getInstance, userDatabaseRepositoryIMP: UserDataRepositoryImpl.getInstance);
     return _userAuthFirebaseRepository!;
   }
 
   UserAuthFirebaseRepository({required this.userDatabaseRepositoryIMP, required this.userAuthServiceIMP});
 
   final IUserAuthService userAuthServiceIMP;
-  final IUserDatabaseRepository userDatabaseRepositoryIMP;
-  @override
-  Future<void> loginByEmail(UserCredentialsSignIn loginCredentials) async {
-  
-      await userAuthServiceIMP.loginByEmail(loginCredentials);
-  
-     
-  }
+  final IUserDataRepository userDatabaseRepositoryIMP;
 
-  @override
-  Future<void> loginByVulgo(UserCredentialsSignInVulgo loginCredentials) async {
-      String vulgo = loginCredentials.vulgo;
-      String email = await userDatabaseRepositoryIMP.getEmailByVulgo(vulgo);
-
-      final credentials = UserCredentialsSignIn(
-        email: email,
-        signMethod: SignMethods.email,
-        password: loginCredentials.password,
-      );
-      
-      return await userAuthServiceIMP.loginByEmail(credentials);
-  }
 
   @override
   Future<UserM> signUp(UserCredentialsSignUp cadastroCredentials) async {
@@ -51,5 +30,11 @@ class UserAuthFirebaseRepository implements IUserAuthRepository {
   @override
   Future<void> logout() async {
     return await userAuthServiceIMP.logout();
+  }
+  
+  @override
+  Future<UserM> login(UserCredentialsSignIn loginCredentials) {
+    // TODO: implement login
+    throw UnimplementedError();
   }
 }
