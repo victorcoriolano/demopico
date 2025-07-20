da6c0a53d5abee22d03b7f87a18201010f815b7f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 10:53:50 -0300

    retorno de string na assinatura da interface do repository do auth

--- lib/features/user/domain/interfaces/i_user_auth_repository.dart
+++ lib/features/user/domain/interfaces/i_user_auth_repository.dart
@@ -3,6 +3,6 @@ import 'package:demopico/features/user/domain/models/user.dart';
 
 abstract class IUserAuthRepository {
   Future<UserM> signUp(UserCredentialsSignUp cadastroCredentials);
-  Future<UserM> login(UserCredentialsSignIn loginCredentials);
+  Future<String> login(UserCredentialsSignIn loginCredentials);
   Future<void> logout();
 }
