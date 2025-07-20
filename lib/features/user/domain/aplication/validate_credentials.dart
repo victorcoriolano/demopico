219f781416f112a5baebb5d13dab719f82b905ab
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 10:10:31 -0300

    classe de aplication para validar credenciais de usuário

---
+++ lib/features/user/domain/aplication/validate_credentials.dart
@@ -0,0 +1,30 @@
+import 'package:demopico/core/common/errors/domain_failures.dart';
+import 'package:demopico/features/user/domain/entity/user_credentials.dart';
+import 'package:demopico/features/user/domain/enums/identifiers.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
+
+class ValidateUserCredentials {
+
+  final IUserDataRepository repository;
+  ValidateUserCredentials({required this.repository});
+
+  static ValidateUserCredentials? _instance;
+  static ValidateUserCredentials get instance => _instance ??= 
+    ValidateUserCredentials(repository: UserDataRepositoryImpl.getInstance);
+
+  Future<UserCredentialsSignIn> validateForLogin(UserCredentialsSignIn credentials) async {
+    bool isValid = false;
+    if (credentials.identifier == Identifiers.vulgo){
+      isValid = await repository.validateDataUserAfter(data: credentials.login, field: "vulgo");
+    }else {
+      isValid = await repository.validateDataUserAfter(data: credentials.login, field: "email");
+    }
+
+    if (isValid) {
+        return credentials;
+      } else {
+        throw InvalidCredentialsFailure();
+      }
+  }
+}
