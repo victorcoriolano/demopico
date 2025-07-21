b1b683504be782cc4464a837bd2bdccb57f62d22
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-20 21:01:00 -0300

    adicionando auto validador no form - não sei por que tiraram

--- lib/features/user/presentation/widgets/login_form.dart
+++ lib/features/user/presentation/widgets/login_form.dart
@@ -1,4 +1,3 @@
-
 import 'package:demopico/features/user/domain/entity/user_credentials.dart';
 import 'package:demopico/features/user/presentation/controllers/auth_user_provider.dart';
 import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
@@ -21,135 +20,136 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> with Validators {
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
-
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Form(
-
+        key: formKey,
         child: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(70.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-                height: 200,
-                width: 250,
-                child: Image(
-                  image: AssetImage('assets/images/skatepico-icon.png'),
-                )),
+          child: Padding(
+            padding: const EdgeInsets.all(70.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                    height: 200,
+                    width: 250,
+                    child: Image(
+                      image: AssetImage('assets/images/skatepico-icon.png'),
+                    )),
 
-            const SizedBox(
-              height: 30,
-            ),
+                const SizedBox(
+                  height: 30,
+                ),
 
-            const Text(
-              "Bem vindo de volta",
-              style: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-                fontSize: 25,
-              ),
-            ),
+                const Text(
+                  "Bem vindo de volta",
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
 
-            const SizedBox(
-              height: 15,
-            ),
+                const SizedBox(
+                  height: 15,
+                ),
 
-            SwithTypeLogin(),
+                SwithTypeLogin(),
 
-            const SizedBox(
-              height: 15,
-            ),
+                const SizedBox(
+                  height: 15,
+                ),
 
-            //email ou vulgo
-            Consumer<AuthUserProvider>(
-              builder: (context, provider, child) {
-                return TextFormField(
-                  decoration: customTextField(provider.isEmail ? "Digite seu Email": "Digite seu vulgo"),
+                //email ou vulgo
+                Consumer<AuthUserProvider>(builder: (context, provider, child) {
+                  return TextFormField(
+                    decoration: customTextField(provider.isEmail
+                        ? "Digite seu Email"
+                        : "Digite seu vulgo"),
+                    cursorColor: Colors.white,
+                    style: const TextStyle(color: Colors.white),
+                    controller: _loginController,
+                    validator: (value) => combineValidators([
+                      () => isNotEmpty(value),
+                      () => isValidEmail(value),
+                    ]),
+                  );
+                }),
+
+                const SizedBox(
+                  height: 20,
+                ),
+                //senha
+                TextFormField(
+                  decoration: customTextField("Senha"),
                   cursorColor: Colors.white,
                   style: const TextStyle(color: Colors.white),
-                  controller: _loginController,
+                  obscureText: true,
+                  controller: _senhaController,
                   validator: (value) => combineValidators([
                     () => isNotEmpty(value),
-                    () => isValidEmail(value),
+                    () => isValidPassword(value),
                   ]),
-                );
-              }
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-            //senha
-            TextFormField(
-              decoration: customTextField("Senha"),
-              cursorColor: Colors.white,
-              style: const TextStyle(color: Colors.white),
-              obscureText: true,
-              controller: _senhaController,
-              validator: (value) => combineValidators([
-                () => isNotEmpty(value),
-                () => isValidPassword(value),
-              ]),
-            ),
-            // text input(esqueceu senha)
-            TextButton(
-                onPressed: () {
-                  //TODO: IMPLEMENTAR LÓGICA DE ESQUECEU A SENHA 
-                },
-                child: const Text(
-                  "Esqueceu senha",
-                  style: TextStyle(color: Color.fromARGB(255, 189, 198, 214)),
-                )),
-
-            
-            const SizedBox(
-              height: 12,
-            ),
-
-            // button (entrar)
-            ElevatedButton(
-              onPressed: () async {                
-                final authProvider = context.read<AuthUserProvider>();
-                final dataUserProvider = context.read<UserDatabaseProvider>();
-
-                final credential = UserCredentialsSignIn(
-                  identifier: authProvider.identifier, 
-                  login: _loginController.text.trim(), 
-                  senha: _senhaController.text.trim());
-                
-                final user = await authProvider.login(credential);
-                dataUserProvider.setUser=user;
-              },
-
-              style: buttonStyle(),
-              child: const Text(
-                "Entrar",
-                style: TextStyle(
-                  color: Colors.white,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            //button (fazer parte)
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => const RegisterPage(),
-                    transition: Transition.circularReveal,
-                    duration: const Duration(seconds: 1));
-              },
-              style: buttonStyle(),
-              child: const Text("FAZER PARTE",
-                  style: TextStyle(color: Colors.white)),
-            ),
-          ],
-        ),
-      ),
-    ));
-  }
+                // text input(esqueceu senha)
+                TextButton(
+                    onPressed: () {
+                      //TODO: IMPLEMENTAR LÓGICA DE ESQUECEU A SENHA
+                    },
+                    child: const Text(
+                      "Esqueceu senha",
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 189, 198, 214)),
+                    )),
 
+                const SizedBox(
+                  height: 12,
+                ),
+
+                // button (entrar)
+                ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      final authProvider = context.read<AuthUserProvider>();
+                      final dataUserProvider =
+                          context.read<UserDatabaseProvider>();
+
+                      final credential = UserCredentialsSignIn(
+                          identifier: authProvider.identifier,
+                          login: _loginController.text.trim(),
+                          senha: _senhaController.text.trim());
+
+                      final user = await authProvider.login(credential);
+                      dataUserProvider.setUser = user;
+                    }
+                  },
+                  style: buttonStyle(),
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                //button (fazer parte)
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(() => const RegisterPage(),
+                        transition: Transition.circularReveal,
+                        duration: const Duration(seconds: 1));
+                  },
+                  style: buttonStyle(),
+                  child: const Text("FAZER PARTE",
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
 }
