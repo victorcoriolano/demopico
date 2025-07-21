56e815216d2ba1fd2c6ba2c763179d01c5e4bf89
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-21 07:20:03 -0300

    adicionando consumer e implementando validate form

--- lib/features/user/presentation/widgets/register_form.dart
+++ lib/features/user/presentation/widgets/register_form.dart
@@ -1,10 +1,12 @@
 
 
+import 'package:demopico/features/user/presentation/controllers/auth_user_provider.dart';
 import 'package:demopico/features/user/presentation/widgets/button_custom.dart';
 import 'package:demopico/features/user/presentation/widgets/tipo_conta_dropdown.dart';
 import 'package:demopico/features/user/presentation/widgets/textfield_decoration.dart';
 import 'package:demopico/features/user/presentation/widgets/form_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
@@ -41,114 +43,124 @@ class _RegisterFormState extends State<RegisterForm> with Validators {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formkey,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(70.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                    height: 200,
-                    width: 250,
-                    child: Image(
-                      image: AssetImage('assets/images/skatepico-icon.png'),
-                    )),
-                const Text(
-                  "Bem vindo ao PICO!",
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
+    return Consumer<AuthUserProvider>(
+      builder: (context, provider, child) {
+        return Form(
+          autovalidateMode: AutovalidateMode.onUnfocus,
+            key: _formkey,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(70.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                        height: 200,
+                        width: 250,
+                        child: Image(
+                          image: AssetImage('assets/images/skatepico-icon.png'),
+                        )),
+                    const Text(
+                      "Bem vindo ao PICO!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+        
+                    // vulgo
+                    TextFormField(
+                      decoration: customTextField("Vulgo"),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(color: Colors.white),
+                      controller: _vulgoCadastro,
+                      validator: isNotEmpty,
+                    ),
+        
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //email
+                    TextFormField(
+                      decoration: customTextField("Email"),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(color: Colors.white),
+                      controller: _emailController,
+                      validator: (value) { 
+                        final stringError = combineValidators([
+                          () => isNotEmpty(value),
+                          () => isValidEmail(value),
+                        ]);
+                        return stringError;
+                      }
+                    ),
+        
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //senha
+                    TextFormField(
+                      decoration: customTextField("Senha"),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(color: Colors.white),
+                      obscureText: true,
+                      controller: _senhaController,
+                      validator: (value) => combineValidators([
+                        () => isNotEmpty(value),
+                        () => isValidPassword(value),
+                      ]),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+        
+                    // confirmar senha
+                    TextFormField(
+                      decoration: customTextField("Confirmar senha "),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(color: Colors.white),
+                      obscureText: true,
+                      controller: _senhaController2,
+                      validator: (value) => combineValidators([
+                        () => isNotEmpty(value),
+                        () => checkPassword(_senhaController.text, value),
+                      ]),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //tipo de onta
+                    TipoContaDropdownButton(
+                        onChanged: (String newValue) =>
+                            _onTipoContaChanged(newValue),
+                        validator: (value) => isNotEmpty(value)),
+        
+                    const SizedBox(
+                      height: 12,
+                    ),
+        
+                    // button (cadastrar)
+                    ElevatedButton(
+                      onPressed: () async {
+                        if(_formkey.currentState!.validate()){
+                          
+                        }
+                      },
+                      style: buttonStyle(),
+                      child: const Text(
+                        "Cadastrar",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 40,
-                ),
-
-                // vulgo
-                TextFormField(
-                  decoration: customTextField("Vulgo"),
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.white),
-                  controller: _vulgoCadastro,
-                  validator: isNotEmpty,
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-                //email
-                TextFormField(
-                  decoration: customTextField("Email"),
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.white),
-                  controller: _emailController,
-                  validator: (value) => combineValidators([
-                    () => isNotEmpty(value),
-                    () => isValidEmail(value),
-                  ]),
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-                //senha
-                TextFormField(
-                  decoration: customTextField("Senha"),
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.white),
-                  obscureText: true,
-                  controller: _senhaController,
-                  validator: (value) => combineValidators([
-                    () => isNotEmpty(value),
-                    () => isValidPassword(value),
-                  ]),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                // confirmar senha
-                TextFormField(
-                  decoration: customTextField("Confirmar senha "),
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.white),
-                  obscureText: true,
-                  controller: _senhaController2,
-                  validator: (value) => combineValidators([
-                    () => isNotEmpty(value),
-                    () => checkPassword(_senhaController.text, value),
-                  ]),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                //tipo de onta
-                TipoContaDropdownButton(
-                    onChanged: (String newValue) =>
-                        _onTipoContaChanged(newValue),
-                    validator: (value) => isNotEmpty(value)),
-
-                const SizedBox(
-                  height: 12,
-                ),
-
-                // button (cadastrar)
-                ElevatedButton(
-                  onPressed: () async {
-                    // TODO: IMPLEMENTAR CRIAÇÃO DE USER
-                  },
-                  style: buttonStyle(),
-                  child: const Text(
-                    "Cadastrar",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
+              ),
+            ));
+      }
+    );
   }
 }
