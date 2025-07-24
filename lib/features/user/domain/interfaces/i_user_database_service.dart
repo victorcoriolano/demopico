542a4e4bcee2550d5f7b8936970f9c10da40f840
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-23 21:25:00 -0300

    unificando também no datasource

--- lib/features/user/domain/interfaces/i_user_database_service.dart
+++ lib/features/user/domain/interfaces/i_user_database_service.dart
@@ -4,6 +4,5 @@ abstract class IUserDataSource<DTO>{
   Future<DTO> getUserDetails(String uid);
   Future<String> getUserData(String id, String field);
   Future<DTO> getUserByField(String field, String value);
-  Future<bool> validateDataBefore(String field, String value);
-  Future<bool> validateDataAfter(String field, String value);
+  Future<bool> validateExistsData(String field, String value);
 }
