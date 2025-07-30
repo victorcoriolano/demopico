b0eedbd6e65f63e073531ba2e5956f2f97a09cc5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-29 22:11:14 -0300

    renomeação

--- lib/features/mapa/presentation/pages/my_spots_page.dart
+++ lib/features/mapa/presentation/pages/my_spots_page.dart
@@ -1,5 +1,5 @@
 import 'package:demopico/core/app/theme/theme.dart';
-import 'package:demopico/features/mapa/presentation/controllers/spot_controller.dart';
+import 'package:demopico/features/mapa/presentation/controllers/spots_controller.dart';
 import 'package:demopico/features/mapa/presentation/widgets/pico_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
