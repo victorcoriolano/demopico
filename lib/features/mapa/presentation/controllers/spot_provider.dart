c91532ffb338824a49ee453d1c5a3074a9605355
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-30 11:59:34 -0300

    adicionando get instance ao provider

--- lib/features/mapa/presentation/controllers/spot_provider.dart
+++ lib/features/mapa/presentation/controllers/spot_provider.dart
@@ -15,82 +15,86 @@ class SpotProvider with ChangeNotifier {
   final DeleteSpotUC _deleteSpotUC;
   final DeleteFileUc _deleteFileUc;
 
-  SpotProvider(
-    {
-      required DeleteFileUc deletefile,
-      required DeleteSpotUC deleteUc,
-      required WatchSpotUc watchUc,
-      required AvaliarSpotUc avaliarUC,}
-  )
-  : _avaliarSpotUc = avaliarUC,
-    _watchSpot = watchUc,
-    _deleteSpotUC = deleteUc,
-    _deleteFileUc = deletefile;
+  static SpotProvider? _instance;
+  static SpotProvider get instance => _instance ??= SpotProvider(
+        deletefile: DeleteFileUc.instance,
+        deleteUc: DeleteSpotUC.instance,
+        watchUc: WatchSpotUc.instance,
+        avaliarUC: AvaliarSpotUc.getInstance,
+      );
+
+  SpotProvider({
+    required DeleteFileUc deletefile,
+    required DeleteSpotUC deleteUc,
+    required WatchSpotUc watchUc,
+    required AvaliarSpotUc avaliarUC,
+  })  : _avaliarSpotUc = avaliarUC,
+        _watchSpot = watchUc,
+        _deleteSpotUC = deleteUc,
+        _deleteFileUc = deletefile;
 
   // states
   // initial state for classification is respect
   ClassificationSpot classification = ClassificationSpot.respect;
   double rate = 0;
-  // error 
+  // error
   String? error;
   //loading
   bool isLoading = false;
   //subscription for management of stream
   StreamSubscription? spotSubscription;
-  //Spot in question 
+  //Spot in question
   Pico? pico;
-  
-  void updateClassification(double rating){
+
+  void updateClassification(double rating) {
     classification = ClassificationSpot.fromRate(rating);
     rate = rating;
     notifyListeners();
   }
 
-  
-
   void initializeWatch(String idPico) {
+    isLoading = true;
     spotSubscription = _watchSpot.execute(idPico).listen(
-      //on data
-      (data){
-        pico = data;
-        notifyListeners();
-      },
-      onError: (error) {
-        error = error.toString();
-
-      }
-    );
+        //on data
+        
+        (data) {
+          pico = data;
+          debugPrint("pico: $pico");
+          isLoading = false;
+          notifyListeners();
+    }, onError: (error) {
+      isLoading = false;
+      error = error.toString();
+    });
   }
 
-  
   Future<void> avaliarSpot() async {
     isLoading = true;
     notifyListeners();
-    if (pico == null){
+    if (pico == null) {
       error = "Pico não identificado";
       return;
     }
     try {
       await _avaliarSpotUc.executar(pico!, rate);
-    }on Failure catch (e){
+    } on Failure catch (e) {
       error = e.message;
-    }catch (e){
+    } catch (e) {
       error = e.toString();
     } finally {
       isLoading = false;
       notifyListeners();
     }
   }
-  
+
   Future<void> deletarPico(Pico pico) async {
-    try{
-     await Future.wait([
-      _deleteFileUc.deletarFile(pico.imgUrls),
-      _deleteSpotUC.callDelete(pico.id),
-     ]);
-    }catch (e) {
-      error = e.toString(); 
+    try {
+      await Future.wait([
+        _deleteFileUc.deletarFile(pico.imgUrls),
+        _deleteSpotUC.callDelete(pico.id),
+      ]);
+    } catch (e) {
+      error = e.toString();
     }
   }
 }
-
