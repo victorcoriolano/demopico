6090c4f3b7b73d4ac3413d0894c5c0db63e3694a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-30 11:20:09 -0300

    info user creator widget

---
+++ lib/features/mapa/presentation/widgets/spot_info_widgets/photo_and_name_widget.dart
@@ -0,0 +1,49 @@
+import 'package:demopico/core/app/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class PhotoAndNameWidget extends StatelessWidget {
+  final String nameUserCreator;
+  final String? urlImageUser;
+  const PhotoAndNameWidget({super.key, required this.nameUserCreator, required this.urlImageUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 80),
+      child: Column(
+        children: [
+          Container(
+            width: 52,
+            height: 52,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: kRedAccent,
+                width: 0.5, 
+              ),
+            ),
+            child: const CircleAvatar(
+              foregroundColor: kWhite,
+              backgroundColor: kLightRed,
+              radius: 25,
+              child: Icon(
+                Icons.person,
+                size: 38,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            nameUserCreator, 
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Color.fromARGB(255, 93, 93, 93),
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
