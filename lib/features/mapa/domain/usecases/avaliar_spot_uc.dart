bb64a64e62edd20d75be4054dd47bd7a2e6e653f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-30 11:11:11 -0300

    refatorando usecase

--- lib/features/mapa/domain/usecases/avaliar_spot_uc.dart
+++ lib/features/mapa/domain/usecases/avaliar_spot_uc.dart
@@ -1,6 +1,8 @@
 
 import 'package:demopico/features/mapa/data/repositories/spot_repository_impl.dart';
+import 'package:demopico/features/mapa/domain/entities/pico_entity.dart';
 import 'package:demopico/features/mapa/domain/interfaces/i_spot_repository.dart';
+import 'package:demopico/features/mapa/domain/models/pico_model.dart';
 
 class AvaliarSpotUc {
 
@@ -15,8 +17,8 @@ class AvaliarSpotUc {
 
   AvaliarSpotUc({required this.notaRepositoryIMP});
 
-  Future<void> executar(String idPico) async {
-    return await notaRepositoryIMP.evaluateSpot(idPico);
+  Future<void> executar(Pico pico, double newRating) async {
+    return await notaRepositoryIMP.evaluateSpot(PicoModel.fromEntity(pico), newRating);
   }
   
 }
