3289b5b4c33474173f8e3f945dff913c59126030
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-30 11:06:14 -0300

    organizando interface de crud datasource e assinando novo método

--- lib/features/external/interfaces/i_crud_datasource.dart
+++ lib/features/external/interfaces/i_crud_datasource.dart
@@ -1,12 +1,24 @@
 abstract class ICrudDataSource<DTO, DataSource> {
   DataSource get dataSource;
-  Future<DTO> setData(String id, DTO data);
+  
+  
+  //create
   Future<DTO> create(DTO dto);
+  Future<DTO> setData(String id, DTO data);
+  
+  //read
   Future<DTO> read(String id);
-  Future<DTO> update(DTO dto);
-  Future<void> delete(String id);
   Future<List<DTO>> readAll();
-  Stream<List<DTO>> watch(); 
   Future<List<DTO>> readAllWithFilter(String field, String value);
   Future<List<DTO>> readWithFilter(String field, String value);
+
+  //update
+  Future<DTO> update(DTO dto);
+
+  //watch
+  Stream<List<DTO>> watch(); 
+  Stream<DTO> watchDoc(String id);
+
+  //delete
+  Future<void> delete(String id);  
 }
