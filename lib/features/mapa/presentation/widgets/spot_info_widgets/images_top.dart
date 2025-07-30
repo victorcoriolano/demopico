4852b59c58e05bd57090a7e9ddc5d3930401faac
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-30 11:17:59 -0300

    REFATORANDO WIDGET DE IMAGENS DO SHOW PICO WIDGET

--- lib/features/mapa/presentation/widgets/spot_info_widgets/images_top.dart
+++ lib/features/mapa/presentation/widgets/spot_info_widgets/images_top.dart
@@ -1,94 +1,68 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:demopico/core/app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ImagesTop extends StatefulWidget {
   final List<String> images;
-  const ImagesTop({ super.key , required this.images });
+  final bool isMine;
+  const ImagesTop({super.key, required this.images, required this.isMine});
 
   @override
   State<ImagesTop> createState() => _ImagesTopState();
 }
 
 class _ImagesTopState extends State<ImagesTop> {
-    int currentIndex = 0;
-   @override
-   Widget build(BuildContext context) {
-    // TODO: REFATORAR ESSA WIDGET
-       return Stack(
-                  children: [
-
-                              PageView.builder(
-                                itemCount: widget.images.length,
-                                onPageChanged: (int page) {
-                                  setState(() {
-                                    currentIndex = page;
-                                  });
-                                },
-                                itemBuilder: (context, pagePosition) {
-                                  
-                                      
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          IconButton(
-                                            icon: const Icon(
-                                              Icons.close,
-                                              color: Color.fromARGB(
-                                                  255, 243, 243, 243),
-                                            ),
-                                            padding: const EdgeInsets.all(10),
-                                            iconSize: 36,
-                                            onPressed: () {
-                                              Navigator.pop(
-                                                  context); // Retorna para a tela anterior
-                                            },
-                                          ),
-                                          Visibility(
-                                            visible: isMine(),
-                                            child: IconButton(
-                                              onPressed: () {
-                                                ModalHelper.openModalDeleteSpot(context, widget.pico);
-                                              } ,
-                                              icon: const Icon(
-                                                Icons.delete,
-                                                color: kBlack,
-                                              ),
-                                              padding:
-                                                  const EdgeInsets.all(10),
-                                            ),
-                                          ),
-                                        ],
-                                      
-                                  );
-                                      return null;
-                                },
-                              ),
-                              //images
-                              Container(
-                                        decoration: const BoxDecoration(
-                                          borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(30)),
-                                        ),
-                                        clipBehavior: Clip.hardEdge,
-                                        child: Image.network(
-                                          widget.images[pagePosition],
-                                          fit: BoxFit.cover,
-                                          width: double.infinity,
-                                        ),
-                                      ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        height: 5,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 240, 238, 238),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ],
+  
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: widget.images.length,
+      onPageChanged: (int page) {
+        setState(() {
+          currentIndex = page;
+        });
+      },
+      itemBuilder: (context, pagePosition) {
+        return Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.close, color: kAlmostWhite),
+                  padding: const EdgeInsets.all(10),
+                  iconSize: 36,
+                  onPressed: () {
+                    Get.back(); // Retorna para a tela anterior
+                  },
                 ),
+              ],
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: CachedNetworkImage(
+                imageUrl: widget.images[pagePosition],
+                fit: BoxFit.cover,
+                width: double.infinity,
+                progressIndicatorBuilder: (context, url, progress) => CircularProgressIndicator(value: progress.progress),
+                errorWidget: (context, url, error) => Center(
+                  child: Column(
+                    children: [
+                      Icon(Icons.broken_image_rounded),
+                      Text(url),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
