5ce54e88b22c8060f1d32e0f2d2536bc28d502bb
Gabriel Pires <dasaybarreto724@gmail.com>
2025-07-31 08:53:37 -0300

    verificação de imagens

--- lib/features/mapa/presentation/widgets/spot_info_widgets/images_top.dart
+++ lib/features/mapa/presentation/widgets/spot_info_widgets/images_top.dart
@@ -17,12 +17,11 @@ class _ImagesTopState extends State<ImagesTop> {
   int currentIndex = 0;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _pageController = PageController(
       initialPage: 0,
     );
-    
   }
 
   @override
@@ -31,88 +30,89 @@ class _ImagesTopState extends State<ImagesTop> {
       height: 300,
       child: Stack(
         children: [
-          PageView.builder(
-            controller: _pageController,
-            itemCount: widget.images.length,
-            onPageChanged: (int page) {
-              setState(() {
-                currentIndex = page;
-              });
-            },
-            itemBuilder: (context, pagePosition) {
-              return Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          widget.images.isEmpty
+              ? Center(
+                  child: Text("Sem Imagens disponíveis"),
+                )
+              : PageView.builder(
+                  controller: _pageController,
+                  itemCount: widget.images.length,
+                  onPageChanged: (int page) {
+                    setState(() {
+                      currentIndex = page;
+                    });
+                  },
+                  itemBuilder: (context, pagePosition) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30)),
+                      ),
+                      clipBehavior: Clip.hardEdge,
+                      child: CachedNetworkImage(
+                        imageUrl: widget.images[pagePosition],
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        progressIndicatorBuilder: (context, url, progress) =>
+                            CircularProgressIndicator(value: progress.progress),
+                        errorWidget: (context, url, error) => Center(
+                          child: Column(
+                            children: [
+                              Icon(Icons.broken_image_rounded),
+                              Text(url),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                clipBehavior: Clip.hardEdge,
-                child: CachedNetworkImage(
-                  imageUrl: widget.images[pagePosition],
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  progressIndicatorBuilder: (context, url, progress) => CircularProgressIndicator(value: progress.progress),
-                  errorWidget: (context, url, error) => Center(
-                    child: Column(
-                      children: [
-                        Icon(Icons.broken_image_rounded),
-                        Text(url),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
           Align(
             alignment: Alignment.topRight,
             child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    
-                    IconButton(
-                      icon: const Icon(Icons.close, color: kRed),
-                      padding: const EdgeInsets.all(10),
-                      iconSize: 36,
-                      onPressed: () {
-                        Get.back(); // Retorna para a tela anterior
-                      },
-                    ),
-                  ],
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.close, color: kRed),
+                  padding: const EdgeInsets.all(10),
+                  iconSize: 36,
+                  onPressed: () {
+                    Get.back(); // Retorna para a tela anterior
+                  },
                 ),
-          ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              
-              child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(widget.images.length, (index) {
-                              final isActive = currentIndex == index;
-                              return AnimatedContainer(
-                                duration: const Duration(milliseconds: 300),
-                                margin: const EdgeInsets.only(bottom: 12, right: 6),
-                                width: isActive ? 12 : 8,
-                                height: isActive ? 12 : 8,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: isActive
-                                      ? kBlack
-                                      : kMediumGrey
-                                ),
-                              );
-                            }),
-                          ),
+              ],
             ),
-             Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        height: 5,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 240, 238, 238),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),  
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(widget.images.length, (index) {
+                final isActive = currentIndex == index;
+                return AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  margin: const EdgeInsets.only(bottom: 12, right: 6),
+                  width: isActive ? 12 : 8,
+                  height: isActive ? 12 : 8,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: isActive ? kBlack : kMediumGrey),
+                );
+              }),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: const EdgeInsets.only(top: 10),
+              height: 5,
+              width: 80,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 240, 238, 238),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
         ],
       ),
     );
