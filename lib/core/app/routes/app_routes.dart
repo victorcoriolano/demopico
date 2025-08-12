6b1c8748e5160efafe844ba0ea945e5ac7507b4e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-12 08:07:51 -0300

    arquivo de routes

---
+++ lib/core/app/routes/app_routes.dart
@@ -0,0 +1,15 @@
+
+
+abstract class Paths {
+  static const home = '/home';
+  static const hub = '/hub';
+  static const login = '/login';
+  static const profile = '/profile';
+  static const chat = '/chat-room';
+  static const searchProfile = '/search';
+  static const favoriteSpot = '/favorites-spot';
+  static const editProfile = '/edit-profile';
+  static const map = '/map-page';
+  static const historySpot = '/spot-history';
+
+}
