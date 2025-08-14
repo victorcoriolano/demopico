c03df0a40ce2983f75b9773aca388db354fa2a6a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-14 12:15:44 -0300

    refatoração do home page

--- lib/core/app/home_page.dart
+++ lib/core/app/home_page.dart
@@ -1,10 +1,8 @@
 // home_page.dart
-import 'package:demopico/core/app/auth_wrapper.dart';
-import 'package:demopico/core/app/controller/controller_home_page.dart';
-import 'package:demopico/features/mapa/presentation/pages/map_page.dart';
+import 'package:demopico/core/app/routes/app_routes.dart';
 import 'package:demopico/features/home/presentation/pages/central_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,37 +15,23 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    context.read<ControllerHomePage>().initialize();
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Consumer<ControllerHomePage>(
-          builder: (contex, controller, child) {
-            return PopScope(
-              canPop: controller.currentPage == 1,
-              onPopInvokedWithResult: (didPop, value) async {
-                if (!didPop) {
-                  await controller.handlePop();
-                }
-              },
-              child: PageView(
-                physics: controller.currentPage == 0
-                    ? const NeverScrollableScrollPhysics() // Desativa a rolagem no mapa
-                    : const BouncingScrollPhysics(),
-                onPageChanged: controller.onPageChanged,
-                controller: controller.pageController ,
-                children: const [
-                  MapPage(),
-                  CentralPage(),
-                  AuthWrapper(),
-                ],
-              ),
-            );
-          },
-        ),
+          child: GestureDetector(
+        onHorizontalDragEnd: (details) {
+          if (details.primaryVelocity! > 0) {
+            Get.toNamed(Paths.map);
+
+          } else if (details.primaryVelocity! < 0) {
+            Get.toNamed(Paths.profile);
+          }
+        },
+        child: CentralPage()
+      ),
       ),
     );
   }
