9d25123253ac873a2c238bc5820bcb7c1ec1172b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-14 07:03:45 -0300

    inserindo a instancia do provider do user diretamente no main pra conseguir encontrar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:demopico/core/app/app.dart';
 import 'package:demopico/core/common/inject_dependencies.dart';
+import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
 import 'package:demopico/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +14,6 @@ void main() async {
   );
   await dotenv.load(fileName: '.env');
   await init();
-
+  Get.put(UserDatabaseProvider.getInstance);
   runApp(const MyAppWidget());
 }
