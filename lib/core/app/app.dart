62eac37177bf8789282cc774a2a03fbed983779e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-14 12:13:49 -0300

    colocando as instancias no get para poder usar depois

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -2,7 +2,9 @@
 import 'package:demopico/core/app/providers/providers.dart';
 import 'package:demopico/core/app/routes/app_pages.dart';
 import 'package:demopico/core/app/routes/app_routes.dart';
-import 'package:demopico/core/app/theme/theme.dart';  
+import 'package:demopico/core/app/theme/theme.dart';
+import 'package:demopico/features/profile/presentation/bidings/auth_biding.dart';
+import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';  
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +15,9 @@ class MyAppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.put<AuthBiding>(AuthBiding());
+    Get.put<UserDatabaseProvider>(UserDatabaseProvider.getInstance);
+    
     return MultiProvider(
       providers: myProviders,
       child: GetMaterialApp(
