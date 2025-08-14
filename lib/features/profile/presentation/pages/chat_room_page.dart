7ace47e2ed8e286dc0a5f50710e19a159e3ec85a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-14 10:13:53 -0300

    tela de mensagens

--- lib/features/profile/presentation/pages/chat_room_page.dart
+++ lib/features/profile/presentation/pages/chat_room_page.dart
@@ -1,3 +1,5 @@
+import 'package:demopico/features/profile/presentation/widgets/chat_room_widgets.dart/in_message.dart';
+import 'package:demopico/features/profile/presentation/widgets/chat_room_widgets.dart/out_message.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoomPage extends StatefulWidget {
@@ -14,7 +16,16 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text(''),),
-           body: Container(),
+           body: ListView(
+            children: const [
+              InMessage(message: 'Hello'),
+              OutMessage(message: 'Hi there'),
+              OutMessage(message: 'How it going?'),
+              InMessage(message: 'Everything is OK'),
+              OutMessage(message: 'Goodbye'),
+              InMessage(message: 'See you soon')
+            ],
+          ),
        );
   }
 }
