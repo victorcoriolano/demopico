7cf53d177893daa49dad129ad161b0f4f4b7dd5e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-16 20:51:38 -0300

    refatoranção do provider do home

--- lib/features/home/presentation/provider/home_provider.dart
+++ lib/features/home/presentation/provider/home_provider.dart
@@ -1,15 +1,16 @@
+import 'package:demopico/features/home/domain/usecases/get_recent_communiques.dart';
 import 'package:demopico/features/hub/domain/entities/communique.dart';
-import 'package:demopico/features/hub/domain/usecases/listar_comunicados_uc.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomeProvider extends ChangeNotifier {
-  ListarComunicado listarComunicado;
-  HomeProvider({required this.listarComunicado});
-  
+  final GetRecentCommuniques _listarComunicado;
+  HomeProvider({required GetRecentCommuniques listarComunicado})
+      : _listarComunicado = listarComunicado;
+
   static HomeProvider? _homeProvider;
 
   static HomeProvider get getInstance{
-    _homeProvider ??= HomeProvider(listarComunicado: ListarComunicado.getInstance);
+    _homeProvider ??= HomeProvider(listarComunicado: GetRecentCommuniques.instance);
     return _homeProvider!;
   }
 
@@ -17,11 +18,9 @@ class HomeProvider extends ChangeNotifier {
 
     List<Communique?> get allCommuniques  => _allCommuniques;
 
-  Future<void> getAllCommuniques() async {
-    final allCommuniquesFromDb = await listarComunicado.listar();
-    _allCommuniques = allCommuniquesFromDb;
+  Future<void> fetchRecentCommuniques() async {
+    _allCommuniques = await _listarComunicado.call();
     notifyListeners();
   }
 
-   
 }
