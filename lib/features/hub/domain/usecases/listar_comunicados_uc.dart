a70687adc7f551fd1ff8815cb4126901a9614caf
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-16 20:55:05 -0300

    método de listar agora com stream para atualizar em tempo real

--- lib/features/hub/domain/usecases/listar_comunicados_uc.dart
+++ lib/features/hub/domain/usecases/listar_comunicados_uc.dart
@@ -1,24 +1,21 @@
 import 'package:demopico/features/hub/domain/entities/communique.dart';
-import 'package:demopico/features/hub/domain/interfaces/i_hub_repository.dart';
-import 'package:demopico/features/hub/infra/repository/hub_repository.dart';
+import 'package:demopico/features/hub/domain/interfaces/i_communique_repository.dart';
+import 'package:demopico/features/hub/infra/repository/communique_repository.dart';
 
 class ListarComunicado{
-  IHubRepository hubRepositoryIMP;
+  ICommuniqueRepository hubRepositoryIMP;
   ListarComunicado({required this.hubRepositoryIMP});
 
     static ListarComunicado? _listarComunicado;
 
     static ListarComunicado get getInstance{
-      _listarComunicado ??= ListarComunicado(hubRepositoryIMP: HubRepository.getInstance);
+      _listarComunicado ??= ListarComunicado(hubRepositoryIMP: CommuniqueRepository.getInstance);
       return _listarComunicado!;
     }
 
 
-  Future<List<Communique>> listar() async {
-    var dados = await hubRepositoryIMP.getAllCommuniques();
-    if (dados.isEmpty) {
-      throw Exception('Não foi possível listar os comunicados');
-    }
+  Stream<List<Communique>> listar() {
+    var dados = hubRepositoryIMP.watchCommuniques();
     return dados;
   }
 }
