b6cb27caec6885717e65cf4c2cdb747f82201172
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-16 21:07:08 -0300

    subindo teste ainda com erros

--- test/ui/cross_navigation_test.dart
+++ test/ui/cross_navigation_test.dart
@@ -1,21 +1,12 @@
 // test/home_page_test.dart
-import 'package:demopico/core/app/app.dart';
 import 'package:demopico/core/app/home_page.dart';
-import 'package:demopico/core/app/providers/providers.dart';
-import 'package:demopico/features/home/infra/http_climate_service.dart';
 import 'package:demopico/features/home/presentation/pages/central_page.dart';
 import 'package:demopico/features/home/presentation/provider/home_provider.dart';
 import 'package:demopico/features/home/presentation/provider/weather_provider.dart';
-import 'package:demopico/features/hub/domain/interfaces/i_hub_repository.dart';
-import 'package:demopico/features/hub/domain/usecases/listar_comunicados_uc.dart';
 import 'package:demopico/features/mapa/presentation/controllers/spots_controller.dart';
 import 'package:demopico/features/mapa/presentation/pages/map_page.dart';
 import 'package:demopico/features/profile/presentation/pages/profile_page.dart';
-import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
-import 'package:demopico/features/user/domain/usecases/pegar_dados_user_uc.dart';
-import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mocktail/mocktail.dart';
@@ -23,15 +14,13 @@ import 'package:provider/provider.dart';
 
 import '../features/mocks/mocks_profile.dart';
 
-class MockControllerHomePage extends Mock implements ControllerHomePage {}
-
 class MockOpenWeatherProvider extends Mock implements OpenWeatherProvider {}
 
 class MockHomeProvider extends Mock implements HomeProvider {}
 
 class MockUserDatabaseProvider extends Mock implements UserDatabaseProvider {}
 
-class MockSpotControllerProvider extends Mock implements SpotControllerProvider {}
+class MockSpotControllerProvider extends Mock implements SpotsControllerProvider {}
 
 // TODO: CORRIGIR ESSES TESTES
 void main() {
@@ -39,7 +28,7 @@ void main() {
     late HomeProvider homeProvider;
     late UserDatabaseProvider userProvider;
     late OpenWeatherProvider openWeatherProvider;
-    late SpotControllerProvider spotControllerProvider;
+    late SpotsControllerProvider spotControllerProvider;
 
     setUpAll(() {
       homeProvider = MockHomeProvider();
@@ -53,13 +42,9 @@ void main() {
 
     testWidgets('Deve renderizar a CentralPage na inicialização',
         (WidgetTester tester) async {
-      when(() => userProvider.user).thenReturn(null);
-      when(() => controller.currentPage,).thenReturn(1);
-      when(() => controller.pageController,).thenReturn(PageController());
-      // Injeta o controlador antes de renderizar o widget.
+      
       await tester.pumpWidget(MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => controller),
           ChangeNotifierProvider(create: (_) => homeProvider),
           ChangeNotifierProvider(create: (_) => openWeatherProvider),
           ChangeNotifierProvider(create: (_) => userProvider),
@@ -72,64 +57,30 @@ void main() {
       expect(find.byType(CentralPage), findsOneWidget);
     });
 
-    testWidgets('Deve exibir a MapPage quando a página muda para 0',
+    testWidgets('Deve exibir a MapPage quando deslizar pra direita ',
         (WidgetTester tester) async {
       when(() => userProvider.user).thenReturn(null);
       
       await tester.pumpWidget(const GetMaterialApp(home: HomePage()));
 
       // Simula a mudança de página diretamente no controlador.
-      controller.onPageChanged(0);
       await tester.pumpAndSettle(); // Aguarda a UI reconstruir.
 
       // Verifica se a MapPage é exibida.
       expect(find.byType(MapPage), findsOneWidget);
     });
 
-    testWidgets('Deve exibir a ProfilePage quando a página muda para 2',
+    testWidgets('Deve exibir a ProfilePage quando deslizar pra esquerda',
         (WidgetTester tester) async {
       when(() => userProvider.user).thenReturn(mockUserProfile);
       await tester.pumpWidget(const GetMaterialApp(home: HomePage()));
 
       // Simula a mudança de página para o índice 2.
-      controller.onPageChanged(2);
       await tester.pumpAndSettle();
 
       // Verifica se a ProfilePage é exibida.
       expect(find.byType(ProfilePage), findsOneWidget);
     });
 
-    testWidgets('Deve permitir o pop na página central',
-        (WidgetTester tester) async {
-      when(() => userProvider.user).thenReturn(null);
-      await tester.pumpWidget(const GetMaterialApp(home: HomePage()));
-
-      // A página inicial é a central, então canPop deve ser true.
-      expect(controller.currentPage, 1);
-      final didPop = await controller.handlePop();
-
-      // Verifica se o método de pop do controlador retorna true.
-      expect(didPop, true);
-    });
-
-    testWidgets('Não deve permitir o pop fora da página central',
-        (WidgetTester tester) async {
-      when(() => userProvider.user).thenReturn(null);
-      await tester.pumpWidget(const GetMaterialApp(home: HomePage()));
-
-      // Simula a mudança de página para o índice 0.
-      controller.onPageChanged(0);
-      await tester.pumpAndSettle();
-
-      // canPop deve ser falso agora.
-      expect(controller.currentPage, 0);
-      final didPop = await controller.handlePop();
-
-      // Verifica se o método de pop do controlador retorna false.
-      expect(didPop, false);
-
-      // O controlador deve ter retornado para a página 1 (central).
-      expect(controller.currentPage, 1);
-    });
   });
 }
