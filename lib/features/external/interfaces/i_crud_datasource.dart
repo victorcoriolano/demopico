111a20e512798f82ddb16eab8b56b12cb78e531e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-18 09:12:36 -0300

    método para ler com dois filtros no crud

--- lib/features/external/interfaces/i_crud_datasource.dart
+++ lib/features/external/interfaces/i_crud_datasource.dart
@@ -1,24 +1,26 @@
 abstract class ICrudDataSource<DTO, DataSource> {
   DataSource get dataSource;
-  
-  
+
   //create
   Future<DTO> create(DTO dto);
   Future<DTO> setData(String id, DTO data);
-  
+
   //read
   Future<DTO> read(String id);
   Future<List<DTO>> readAll();
   Future<List<DTO>> readAllWithFilter(String field, String value);
-  Future<List<DTO>> readWithFilter(String field, String value);
-
+  Future<List<DTO>> readWithTwoFilters(
+      {required String field1,
+      required String value1,
+      required String field2,
+      required String value2});
   //update
   Future<DTO> update(DTO dto);
 
   //watch
-  Stream<List<DTO>> watch(); 
+  Stream<List<DTO>> watch();
   Stream<DTO> watchDoc(String id);
 
   //delete
-  Future<void> delete(String id);  
+  Future<void> delete(String id);
 }
