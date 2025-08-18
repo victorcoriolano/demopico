e9a15d683cd1bfb6fd07033c4bc11708480f9921
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-18 09:12:13 -0300

    método para setar o id no dto

--- lib/features/external/datasources/firebase/dto/firebase_dto.dart
+++ lib/features/external/datasources/firebase/dto/firebase_dto.dart
@@ -1,17 +1,23 @@
 class FirebaseDTO {
-  final String id;
+  String _id;
   final Map<String, dynamic> data;
 
-  FirebaseDTO({required this.id, required this.data});
+  FirebaseDTO({required String id, required this.data}) : _id = id;
 
   FirebaseDTO copyWith({
     String? id,
     Map<String, dynamic>? data,
   }) {
     return FirebaseDTO(
-      id: id ?? this.id,
+      id: id ?? _id,
       data: data ?? this.data,
     );
+  }
+
+  String get id => _id;
+
+  set setId(String newId) {
+    _id = newId;
   }
 
 }
