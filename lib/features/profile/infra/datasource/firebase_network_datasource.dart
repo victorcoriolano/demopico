ec78ad9c50eb11ed83b0f63d1f0fd705932e78ab
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-18 09:16:40 -0300

    implementando datasource de rede social

--- lib/features/profile/infra/datasource/firebase_network_datasource.dart
+++ lib/features/profile/infra/datasource/firebase_network_datasource.dart
@@ -3,6 +3,7 @@ import 'package:demopico/core/common/files_manager/enums/collections.dart';
 import 'package:demopico/features/external/datasources/firebase/crud_firebase.dart';
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_network_datasource.dart';
+import 'package:demopico/features/profile/domain/models/connection.dart';
 
 class FirebaseNetworkDatasource implements INetworkDatasource<FirebaseDTO>{
 
@@ -21,17 +22,22 @@ class FirebaseNetworkDatasource implements INetworkDatasource<FirebaseDTO>{
   }
 
   @override
-  Future<void> connectionUser(FirebaseDTO dto) async {
-    await _crudFirebaseBoilerplate.create(dto);
+  Future<FirebaseDTO> connectUser(FirebaseDTO dto) async {
+    return await _crudFirebaseBoilerplate.create(dto);
   }
 
   @override
   Future<List<FirebaseDTO>> getConnections(String userID) {
-    return _crudFirebaseBoilerplate.readWithFilter("userID", userID);
+    return _crudFirebaseBoilerplate.readAllWithFilter("userID", userID);
 
   }
   @override
-  Future<void> disconnectionUser(FirebaseDTO dto) async{
+  Future<void> disconnectUser(FirebaseDTO dto) async{
     await _crudFirebaseBoilerplate.delete(dto.id);
+  }
+  
+  @override
+  Future<List<FirebaseDTO>> getConnectionRequests(String userID) {
+    return _crudFirebaseBoilerplate.readWithTwoFilters(field1: 'userID', value1: userID, field2: 'status', value2: RequestConnectionStatus.pending.name);
   }
 }
