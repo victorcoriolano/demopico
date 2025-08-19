c5e42eadb1e8076dfccb1dff6c53da4c679d5346
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-19 08:57:09 -0300

    passando classe ao invés de tipos primitivos

--- lib/features/profile/domain/interfaces/i_network_repository.dart
+++ lib/features/profile/domain/interfaces/i_network_repository.dart
@@ -4,6 +4,6 @@ import 'package:demopico/features/user/domain/models/user.dart';
 abstract class INetworkRepository {
   Future<List<UserM>> getConnections(String userID);
   Future<List<Connection>> getConnectionRequests(String userID);
-  Future<void> connectUser(String userId, String followerId);
-  Future<void> disconnectUser(String userId, String followerId);
+  Future<void> connectUser(Connection connection);
+  Future<void> disconnectUser(Connection connection);
 }
