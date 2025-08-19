4cf1d3645dc4d0e0da159877791eaf513786e6df
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-19 08:56:37 -0300

    renomeando método

--- lib/features/profile/domain/interfaces/i_network_datasource.dart
+++ lib/features/profile/domain/interfaces/i_network_datasource.dart
@@ -1,6 +1,6 @@
 abstract class INetworkDatasource<DTO> {
   Future<List<DTO>> getConnections(String userID);
-  Future<List<DTO>> getConnectionRequests(String userID);
+  Future<List<DTO>> fetchRequestConnections(String userID);
   Future<DTO> connectUser(DTO dto);
   Future<void> disconnectUser(DTO dto);
 }
