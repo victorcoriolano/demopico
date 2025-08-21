bdc01ab723ee2f3cac4327c37aaa90b3c643b14f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-21 08:02:41 -0300

    metodos pra atualizar e checkar conexão

--- lib/features/profile/domain/interfaces/i_network_datasource.dart
+++ lib/features/profile/domain/interfaces/i_network_datasource.dart
@@ -3,4 +3,6 @@ abstract class INetworkDatasource<DTO> {
   Future<List<DTO>> fetchRequestConnections(String userID);
   Future<DTO> connectUser(DTO dto);
   Future<void> disconnectUser(DTO dto);
+  Future<DTO> updateConnection(DTO dto);
+  Future<bool> checkConnection(String idConnection);
 }
