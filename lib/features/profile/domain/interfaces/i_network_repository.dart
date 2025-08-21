73be557527cc8a4f965e7a3586fd1cadd9605fe9
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-21 08:02:55 -0300

    adicionando métodos no repository

--- lib/features/profile/domain/interfaces/i_network_repository.dart
+++ lib/features/profile/domain/interfaces/i_network_repository.dart
@@ -2,6 +2,8 @@ import 'package:demopico/features/profile/domain/models/connection.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 
 abstract class INetworkRepository {
+  Future<bool> checkConnection(String idConnection);
+  Future<Connection> updateConnection(Connection connection);
   Future<List<UserM>> getConnections(String userID);
   Future<List<Connection>> getConnectionRequests(String userID);
   Future<void> connectUser(Connection connection);
