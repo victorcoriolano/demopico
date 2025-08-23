1f108717582acc8c764450e37fe8de694f37df71
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-23 09:42:21 -0300

    retornando a conexão do ao checkar se existe

--- lib/features/profile/domain/interfaces/i_network_repository.dart
+++ lib/features/profile/domain/interfaces/i_network_repository.dart
@@ -2,7 +2,7 @@ import 'package:demopico/features/profile/domain/models/connection.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 
 abstract class INetworkRepository {
-  Future<bool> checkConnection(String idConnection);
+  Future<Connection> checkConnection(String idConnection);
   Future<Connection> updateConnection(Connection connection);
   Future<List<UserM>> getConnections(String userID);
   Future<List<Connection>> getConnectionRequests(String userID);
