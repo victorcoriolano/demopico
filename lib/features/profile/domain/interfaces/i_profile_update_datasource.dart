712ebc2cf32a0e13cef482a88c2ab748f176164e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-23 09:39:51 -0300

    removendo dead code

--- lib/features/profile/domain/interfaces/i_profile_update_datasource.dart
+++
@@ -1,7 +0,0 @@
-abstract class IProfileUpdateDatasource{
-  Future<void> updateContributions(String uid);
-  Future<void> updateFollowers(String uid);
-  Future<void> updateBio(String newBio, String uid);
-  Future<void> updatePhoto(String newPhoto, String uid);
- 
-}
