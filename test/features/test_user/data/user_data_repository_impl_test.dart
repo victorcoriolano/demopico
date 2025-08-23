7b53f7396d64b2177f9dfb701385ea5429bcc0f1
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-23 16:45:38 -0300

    corrigindo teste de repository

--- test/features/test_user/data/user_data_repository_impl_test.dart
+++ test/features/test_user/data/user_data_repository_impl_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
 import 'package:demopico/features/user/domain/interfaces/i_user_database_service.dart';
+
+import '../../mocks/mocks_profile.dart';
 
 class MockUserDataSource extends Mock implements IUserDataSource<FirebaseDTO> {}
 
@@ -17,42 +18,40 @@ void main() {
     repository = UserDataRepositoryImpl(userFirebaseService: mockDataSource);
   });
 
-  group('UserDataRepositoryImpl', () {
+  setUpAll(() {
+    registerFallbackValue(listDtosUser[1]);
+  });
+
+  group('UserDataRepositoryImpl test', () {
     test('addUserDetails sets local user and calls data source', () async {
       when(() => mockDataSource.addUserDetails(any())).thenAnswer((_) async {});
-      await repository.addUserDetails(user);
-      expect(repository.localUser, user);
+      await repository.addUserDetails(mockUserProfile);
+      expect(repository.localUser, mockUserProfile);
       verify(() => mockDataSource.addUserDetails(any())).called(1);
     });
 
-    test('getUserDetailsByID returns local user if set', () async {
-      repository._userLocalDetails = user;
-      final result = await repository.getUserDetailsByID('123');
-      expect(result, user);
-      verifyNever(() => mockDataSource.getUserDetails(any()));
-    });
 
     test('getUserDetailsByID fetches and sets local user if not set', () async {
-      when(() => mockDataSource.getUserDetails('123')).thenAnswer((_) async => dto);
-      final result = await repository.getUserDetailsByID('123');
-      expect(result.id, '123');
-      expect(repository.localUser?.id, '123');
-      verify(() => mockDataSource.getUserDetails('123')).called(1);
+      when(() => mockDataSource.getUserDetails('1')).thenAnswer((_) async => listDtosUser[0]);
+      final result = await repository.getUserDetailsByID('1');
+      expect(result.id, '1');
+      expect(repository.localUser?.id, '1');
+      verify(() => mockDataSource.getUserDetails('1')).called(1);
     });
 
     test('updateUserDetails updates local user and calls data source', () async {
       when(() => mockDataSource.update(any())).thenAnswer((_) async {});
-      final result = await repository.updateUserDetails(user);
-      expect(result, user);
-      expect(repository.localUser, user);
+      final result = await repository.updateUserDetails(mockUserProfile);
+      expect(result, mockUserProfile);
+      expect(repository.localUser, mockUserProfile);
       verify(() => mockDataSource.update(any())).called(1);
     });
 
     test('getEmailByVulgo returns email from mapped model', () async {
-      when(() => mockDataSource.getUserByField('name', 'Test')).thenAnswer((_) async => dto);
-      final email = await repository.getEmailByVulgo('Test');
+      when(() => mockDataSource.getUserByField(any(), any())).thenAnswer((_) async => listDtosUser.first);
+      final email = await repository.getEmailByVulgo('artu');
       expect(email, 'test@email.com');
-      verify(() => mockDataSource.getUserByField('name', 'Test')).called(1);
+      verify(() => mockDataSource.getUserByField('name', 'artu')).called(1);
     });
 
     test('validateExist calls data source and returns result', () async {
