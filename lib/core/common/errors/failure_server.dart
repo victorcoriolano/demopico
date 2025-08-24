0cdcf395be4d7cbcfc8a5ef0911c00fc965835d2
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-24 08:43:31 -0300

    get Error

--- lib/core/common/errors/failure_server.dart
+++ lib/core/common/errors/failure_server.dart
@@ -1,5 +1,6 @@
 // Classe base para representar falhas
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 abstract class Failure implements Exception {
   final String message;
@@ -36,6 +37,16 @@ class SnackBarFailure extends SnackBar {
       duration: const Duration(seconds: 5),
       shape: const RoundedRectangleBorder(),
       elevation: 5,
+    );
+  }
+}
+
+class FailureServer {
+  static void showError(Failure failure, [String title = "Error"]) {
+    Get.snackbar(
+      title,
+      failure.message,
+      snackPosition: SnackPosition.BOTTOM,
     );
   }
 }
