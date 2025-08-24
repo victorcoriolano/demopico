c82a444ceaf492f51da1b3f888cef60c93bb19f1
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-24 11:13:13 -0300

    consumer pra utilizar os dados do provider

--- lib/features/profile/presentation/widgets/search_page_widgets/container_suggestion_widget.dart
+++ lib/features/profile/presentation/widgets/search_page_widgets/container_suggestion_widget.dart
@@ -1,5 +1,7 @@
+import 'package:demopico/features/profile/presentation/provider/network_view_model.dart';
 import 'package:demopico/features/profile/presentation/widgets/search_page_widgets/suggetions_profiles_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ContainerSuggestionWidget extends StatelessWidget {
 
@@ -10,20 +12,24 @@ class ContainerSuggestionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.75,
-      padding: const EdgeInsets.all(8.0),
-      color: const Color.fromARGB(255, 231, 231, 231),
-      width: MediaQuery.of(context).size.width * 1,
-      child:ListView.builder(
-        itemCount: 10, // Número de sugestões
-        itemBuilder: (context, index) {
-          return SuggestionProfilesWidget(
-            name: 'Usuário $index',
-            imageUrl: 'https://via.placeholder.com/150', // URL de exemplo
-          );
-        },
-      ),
+    return Consumer<NetworkViewModel>(
+      builder: (context, viewModel, child) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.75,
+          padding: const EdgeInsets.all(8.0),
+          color: const Color.fromARGB(255, 231, 231, 231),
+          width: MediaQuery.of(context).size.width * 1,
+          child:ListView.builder(
+            itemCount: viewModel.sugestions.length, // Número de sugestões
+            itemBuilder: (context, index) {
+              return SuggestionProfilesWidget(
+                name: viewModel.sugestions[index].name,
+                imageUrl: viewModel.sugestions[index].pictureUrl ?? '',
+              );
+            },
+          ),
+        );
+      }
     );
   }
 
