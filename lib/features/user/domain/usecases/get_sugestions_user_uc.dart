7088bab2ca053e552e23154fce26219b0ebfce93
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-25 10:11:40 -0300

    corrigindo use case

--- lib/features/user/domain/usecases/get_sugestions_user_uc.dart
+++ lib/features/user/domain/usecases/get_sugestions_user_uc.dart
@@ -2,7 +2,6 @@
 import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
-import 'package:flutter/cupertino.dart';
 
 class GetSugestionsUserUc {
   final IUserDataRepository _repository;
@@ -21,10 +20,7 @@ class GetSugestionsUserUc {
   Future<List<UserM>> execute(UserM user) async {
     if (user.connections.isEmpty) {
       //não tem conexões então a lista de sugestões será a lista de usuários
-      final listUsers = await _repository.getUsers();
-      debugPrint("Lista de usuários: ${listUsers.length}");
-      listUsers.remove(user);
-      debugPrint("Lista de usuários após remover o usuário atual: ${listUsers.length} - removeu $user");
+      final listUsers = await _repository.getUsersExcept(user.id);
       return listUsers;
     }
     return await _repository.getSuggestions(user.connections);
