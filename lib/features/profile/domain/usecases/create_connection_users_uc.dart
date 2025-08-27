5688a6b3aad8b60f881a081d5f207ebb24a8d81f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-26 22:07:56 -0300

    implementando get instance no use case

--- lib/features/profile/domain/usecases/create_connection_users_uc.dart
+++ lib/features/profile/domain/usecases/create_connection_users_uc.dart
@@ -1,9 +1,19 @@
 
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
 import 'package:demopico/features/profile/domain/models/connection.dart';
+import 'package:demopico/features/profile/infra/repository/network_repository.dart';
 
 class CreateConnectionUsersUc {
   final INetworkRepository _repository;
+
+  static CreateConnectionUsersUc? _instance;
+
+  static CreateConnectionUsersUc get instance {
+    _instance ??= CreateConnectionUsersUc(
+      repository: NetworkRepository.instance,
+    );
+    return _instance!;
+  }
 
   CreateConnectionUsersUc({required INetworkRepository repository})
       : _repository = repository;
