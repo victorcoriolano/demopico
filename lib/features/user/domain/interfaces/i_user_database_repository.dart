caa4921e81a8783e1be6fe7b43012521b0d10c27
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-26 22:09:13 -0300

    nomes de métodos mais descritivos

--- lib/features/user/domain/interfaces/i_user_database_repository.dart
+++ lib/features/user/domain/interfaces/i_user_database_repository.dart
@@ -8,7 +8,7 @@ abstract class IUserDataRepository{
   Future<String> getEmailByVulgo(String vulgo);
   Future<UserM> updateUserDetails(UserM user);
   Stream<List<UserM>> searchUsers(String query);
-  Future<List<UserM>> getUsersExcept(String uid);
-  Future<List<UserM>> getSuggestions(List<String> arguments);
+  Future<List<UserM>> getSuggestionsProfileExcept(String uid);
+  Future<List<UserM>> getSuggestionsExceptConnections(Set<String> connectionsExcept);
   Future<bool> validateExist({required String data,required  String field});
 }
