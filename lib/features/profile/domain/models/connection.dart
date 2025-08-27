be3fee36f14a1d8494de6b7a90b92a367f53a48d
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-26 22:07:38 -0300

    adicionando data da criação de conexão na model

--- lib/features/profile/domain/models/connection.dart
+++ lib/features/profile/domain/models/connection.dart
@@ -4,9 +4,9 @@ class Connection {
   String userID;
   String connectedUserID;
   RequestConnectionStatus status;
+  DateTime createdAt;
 
-
-  Connection({required this.id, required this.userID, required this.connectedUserID, required this.status});
+  Connection({required this.id, required this.userID, required this.connectedUserID, required this.status, required this.createdAt});
 
   Map<String, dynamic> toJson() {
     return {
@@ -14,12 +14,14 @@ class Connection {
       'userID': userID,
       'connectedUserID': connectedUserID,
       'status': status.name,
+      'createdAt': createdAt.toIso8601String(),
     };
   }
 
   factory Connection.fromJson(Map<String, dynamic> json, String id) {
     return Connection(
       id: id,
+      createdAt: DateTime.parse(json['createdAt']),
       userID: json['userID'],
       status: RequestConnectionStatus.fromString(json['status']),
       connectedUserID: json['connectedUserID'],
