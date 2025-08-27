2526b28ef099d73b0d943297ff2d4ad55ac42847
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-26 22:08:30 -0300

    implementando create connections

--- lib/features/profile/presentation/provider/network_view_model.dart
+++ lib/features/profile/presentation/provider/network_view_model.dart
@@ -1,4 +1,6 @@
 import 'package:demopico/core/common/errors/failure_server.dart';
+import 'package:demopico/features/profile/domain/models/connection.dart';
+import 'package:demopico/features/profile/domain/usecases/create_connection_users_uc.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:demopico/features/user/domain/usecases/get_sugestions_user_uc.dart';
 import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
@@ -6,17 +8,21 @@ import 'package:flutter/material.dart';
 
 class NetworkViewModel extends ChangeNotifier {
   final GetSugestionsUserUc _getSugestionsUser;
+  final CreateConnectionUsersUc _createConnectionUsers;
+
 
   static NetworkViewModel? _instance;
   static NetworkViewModel get instance {
     _instance ??= NetworkViewModel(
+      createConnectionUsers: CreateConnectionUsersUc.instance,
       getSugestionsUser: GetSugestionsUserUc.instance,
     );
     return _instance!;
   }
 
-  NetworkViewModel({required GetSugestionsUserUc getSugestionsUser})
-      : _getSugestionsUser = getSugestionsUser;
+  NetworkViewModel({required GetSugestionsUserUc getSugestionsUser, required CreateConnectionUsersUc createConnectionUsers})
+      : _getSugestionsUser = getSugestionsUser,
+        _createConnectionUsers = createConnectionUsers;
 
   List<UserM> get sugestions => _sugestions;
 
@@ -28,6 +34,26 @@ class NetworkViewModel extends ChangeNotifier {
 
     try{
       _sugestions = await _getSugestionsUser.execute(user);
+    } on Failure catch (e) {
+      FailureServer.showError(e);
+    }
+    notifyListeners();
+  }
+
+  Future<void> createConnections(UserM user) async {
+    final userLogged = UserDatabaseProvider.getInstance.user;
+    if (userLogged == null) return;
+
+    final connection = Connection(
+      id: '',
+      userID: userLogged.id,
+      connectedUserID: user.id,
+      status: RequestConnectionStatus.pending,
+      createdAt: DateTime.now(),
+    );
+
+    try{
+      await _createConnectionUsers.execute(connection);
     } on Failure catch (e) {
       FailureServer.showError(e);
     }
