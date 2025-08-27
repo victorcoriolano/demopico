1cdfaf5c996bc37292cfdc4d8781d85995d1eda9
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-26 22:09:50 -0300

    utilizando set pra não fazer requisição com ids duplicados

--- lib/features/user/domain/interfaces/i_user_database_service.dart
+++ lib/features/user/domain/interfaces/i_user_database_service.dart
@@ -6,7 +6,7 @@ abstract class IUserDataSource<DTO>{
   Future<DTO> getUserDetails(String uid);
   Future<String> getUserData(String id, String field);
   Future<DTO> getUserByField(String field, String value);
-  Future<List<DTO>> getSuggestions(List<String> arguments);
+  Future<List<DTO>> getSuggestions(Set<String> connectionsUserExcept);
   Future<List<DTO>> getUsersExcept(String uid);
   //SEARCH
   Stream<List<DTO>> searchUsers(String query);
