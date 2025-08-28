65e558bf036ab6cbf19abfb237aa34558d63c96e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-28 09:16:55 -0300

    use case de pegar as solicitações de conexão

---
+++ lib/features/profile/domain/usecases/get_connections_requests_uc.dart
@@ -0,0 +1,24 @@
+
+import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
+import 'package:demopico/features/profile/domain/models/connection.dart';
+import 'package:demopico/features/profile/infra/repository/network_repository.dart';
+
+class GetConnectionsRequestsUc {
+
+  static GetConnectionsRequestsUc? _instance;
+  static GetConnectionsRequestsUc get instance {
+    _instance ??= GetConnectionsRequestsUc(
+      networkRepository: NetworkRepository.instance,
+    );
+    return _instance!;
+  }
+
+  final INetworkRepository _networkRepository;
+
+  GetConnectionsRequestsUc({required INetworkRepository networkRepository})
+      : _networkRepository = networkRepository;
+
+  Future<List<Connection>> execute(String uid) async {
+    return await _networkRepository.getConnectionRequests(uid);
+  }
+}
