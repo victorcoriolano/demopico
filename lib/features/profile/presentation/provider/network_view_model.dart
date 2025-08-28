b944b91f4eab6dded252dc375297f2020e5133ba
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-28 09:18:06 -0300

    adicionando use cases na view model

--- lib/features/profile/presentation/provider/network_view_model.dart
+++ lib/features/profile/presentation/provider/network_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/features/profile/domain/models/connection.dart';
+import 'package:demopico/features/profile/domain/usecases/accept_connection_uc.dart';
 import 'package:demopico/features/profile/domain/usecases/create_connection_users_uc.dart';
+import 'package:demopico/features/profile/domain/usecases/get_connections_requests_uc.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:demopico/features/user/domain/usecases/get_sugestions_user_uc.dart';
 import 'package:demopico/features/user/presentation/controllers/user_database_provider.dart';
@@ -9,6 +11,8 @@ import 'package:flutter/material.dart';
 class NetworkViewModel extends ChangeNotifier {
   final GetSugestionsUserUc _getSugestionsUser;
   final CreateConnectionUsersUc _createConnectionUsers;
+  final GetConnectionsRequestsUc _getConnectionsRequests;
+  final AcceptConnectionUc _acceptConnection;     
 
 
   static NetworkViewModel? _instance;
@@ -16,17 +20,27 @@ class NetworkViewModel extends ChangeNotifier {
     _instance ??= NetworkViewModel(
       createConnectionUsers: CreateConnectionUsersUc.instance,
       getSugestionsUser: GetSugestionsUserUc.instance,
+      getConnectionsRequests: GetConnectionsRequestsUc.instance,
+      acceptConnection: AcceptConnectionUc.instance,
     );
     return _instance!;
   }
 
-  NetworkViewModel({required GetSugestionsUserUc getSugestionsUser, required CreateConnectionUsersUc createConnectionUsers})
-      : _getSugestionsUser = getSugestionsUser,
-        _createConnectionUsers = createConnectionUsers;
+  NetworkViewModel({
+    required GetSugestionsUserUc getSugestionsUser,
+    required CreateConnectionUsersUc createConnectionUsers,
+    required GetConnectionsRequestsUc getConnectionsRequests,
+    required AcceptConnectionUc acceptConnection,
+  })  : _getSugestionsUser = getSugestionsUser,
+        _createConnectionUsers = createConnectionUsers,
+        _getConnectionsRequests = getConnectionsRequests,
+        _acceptConnection = acceptConnection;
 
   List<UserM> get sugestions => _sugestions;
+  List<Connection> get connections => _connectionsRequests;
 
   List<UserM> _sugestions = [];  
+  List<Connection> _connectionsRequests = [];
 
   Future<void> fetchSugestions() async {
     final user = UserDatabaseProvider.getInstance.user;
@@ -34,6 +48,18 @@ class NetworkViewModel extends ChangeNotifier {
 
     try{
       _sugestions = await _getSugestionsUser.execute(user);
+    } on Failure catch (e) {
+      FailureServer.showError(e);
+    }
+    notifyListeners();
+  }
+
+  Future<void> fetchConnections(UserM user) async {
+    final userLogged = UserDatabaseProvider.getInstance.user;
+    if (userLogged == null) return;
+
+    try {
+      _connectionsRequests = await _getConnectionsRequests.execute(userLogged.id);
     } on Failure catch (e) {
       FailureServer.showError(e);
     }
@@ -50,10 +76,21 @@ class NetworkViewModel extends ChangeNotifier {
       connectedUserID: user.id,
       status: RequestConnectionStatus.pending,
       createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
     );
 
     try{
       await _createConnectionUsers.execute(connection);
+    } on Failure catch (e) {
+      FailureServer.showError(e);
+    }
+    notifyListeners();
+  }
+
+  Future<void> acceptConnection(Connection connection) async {
+    try {
+      
+      await _acceptConnection.execute(connection);
     } on Failure catch (e) {
       FailureServer.showError(e);
     }
