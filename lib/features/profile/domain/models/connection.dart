11cf08009df5cf9766b359ec4e89043beb636acf
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-29 07:54:16 -0300

    adicionando available para o enum de request connection

--- lib/features/profile/domain/models/connection.dart
+++ lib/features/profile/domain/models/connection.dart
@@ -1,4 +1,5 @@
 
+
 class Connection {
   String id;
   String userID;
@@ -36,6 +37,7 @@ class Connection {
 enum RequestConnectionStatus{
   pending,
   accepted,
+  available,
   rejected;
 
   String get name {
@@ -46,11 +48,28 @@ enum RequestConnectionStatus{
         return 'accepted';
       case RequestConnectionStatus.rejected:
         return 'rejected';
+      case RequestConnectionStatus.available:
+        return 'available';
+    }
+  }
+
+  String get statusForSuggestions {
+    switch (this) {
+      case RequestConnectionStatus.pending:
+        return 'Enviado';
+      case RequestConnectionStatus.accepted:
+        return 'Conectado';
+      case RequestConnectionStatus.rejected:
+        return 'Conectar';
+      case RequestConnectionStatus.available:
+        return 'Conectar';
     }
   }
 
   factory RequestConnectionStatus.fromString(String value) {
     switch (value) {
+      case 'available':
+        return RequestConnectionStatus.available;
       case 'pending':
         return RequestConnectionStatus.pending;
       case 'accepted':
