8e3c35acec36748a5f4af43e87fe8785aa8b6641
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-29 21:47:51 -0300

    utilizando a outra interface

--- lib/features/user/domain/usecases/get_sugestions_user_uc.dart
+++ lib/features/user/domain/usecases/get_sugestions_user_uc.dart
@@ -1,31 +1,36 @@
 
-import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/profile/presentation/view_objects/suggestion_profile.dart';
+import 'package:demopico/features/user/domain/interfaces/i_users_repository.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
-import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
+import 'package:demopico/features/user/infra/repositories/users_repository.dart';
 
 class GetSugestionsUserUc {
-  final IUserDataRepository _repository;
+  final IUsersRepository _repository;
 
   static GetSugestionsUserUc? _instance;
   static GetSugestionsUserUc get instance {
     _instance ??= GetSugestionsUserUc(
-      repository: UserDataRepositoryImpl.getInstance,
+      repository: UsersRepository.getInstance,
     );
     return _instance!;
   }
 
-  GetSugestionsUserUc({required IUserDataRepository repository})
+  GetSugestionsUserUc({required IUsersRepository repository})
       : _repository = repository;
 
-  Future<List<UserM>> execute(UserM user) async {
-    if (user.connections.length < 10) {
+  Future<List<SuggestionProfile>> execute(UserM user) async {
+    if (user.connections.isEmpty) {
       final listUsers = await _repository.getSuggestionsProfileExcept(user.id);
-      return listUsers;
+      return listUsers.map((user) {
+        return SuggestionProfile.fromUser(user);
+      }).toList();
     }
     final listExcept = user.connections;
     listExcept.add(user.id);
 
     final suggestions  = await _repository.getSuggestionsExceptConnections(listExcept.toSet());
-    return suggestions;
+    return suggestions.map((user) {
+      return SuggestionProfile.fromUser(user);
+    }).toList();
   }
 }
