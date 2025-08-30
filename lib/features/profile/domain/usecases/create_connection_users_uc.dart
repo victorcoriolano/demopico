0bb0edc796a91f9fc983d4312d36bd2a032e25f2
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-29 21:45:04 -0300

    decompondo pra poder alterar mais facilmente

--- lib/features/profile/domain/usecases/create_connection_users_uc.dart
+++ lib/features/profile/domain/usecases/create_connection_users_uc.dart
@@ -2,23 +2,29 @@
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
 import 'package:demopico/features/profile/domain/models/connection.dart';
 import 'package:demopico/features/profile/infra/repository/network_repository.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 
 class CreateConnectionUsersUc {
   final INetworkRepository _repository;
+  final IUserDataRepository _userDataRepository;
 
   static CreateConnectionUsersUc? _instance;
 
   static CreateConnectionUsersUc get instance {
     _instance ??= CreateConnectionUsersUc(
       repository: NetworkRepository.instance,
+      userDataRepository: UserDataRepositoryImpl.getInstance,
     );
     return _instance!;
   }
 
-  CreateConnectionUsersUc({required INetworkRepository repository})
-      : _repository = repository;
+  CreateConnectionUsersUc({required INetworkRepository repository, required IUserDataRepository userDataRepository})
+      : _repository = repository,
+        _userDataRepository = userDataRepository;
 
   Future<Connection> execute(Connection connection) {
-    return _repository.createConnection(connection);
+    final output = _repository.createConnection(connection);
+    return output;
   }
 }
