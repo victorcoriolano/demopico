bcd04004c62f0ae1e83635c7e51c5b43a79f432e
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-30 21:17:35 -0300

    assinatura do método para ler todos os documentos

--- lib/features/user/domain/interfaces/i_users_repository.dart
+++ lib/features/user/domain/interfaces/i_users_repository.dart
@@ -2,6 +2,7 @@ import 'package:demopico/features/user/domain/models/user.dart';
 
 abstract class IUsersRepository {
   Stream<List<UserM>> searchUsers(String query);
+  Future<List<UserM>> findAll();  
   Future<List<UserM>> getSuggestionsProfileExcept(String uid);
   Future<List<UserM>> getSuggestionsExceptConnections(Set<String> connectionsExcept);
 }
