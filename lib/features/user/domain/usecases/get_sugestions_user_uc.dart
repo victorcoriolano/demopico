fb67de09e238c07e0a7e30c9d504e98816d73510
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-30 21:18:10 -0300

    refatorando getsuggestions para passar a lógica corretamente

--- lib/features/user/domain/usecases/get_sugestions_user_uc.dart
+++ lib/features/user/domain/usecases/get_sugestions_user_uc.dart
@@ -1,36 +1,38 @@
 
+import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
+import 'package:demopico/features/profile/infra/repository/network_repository.dart';
 import 'package:demopico/features/profile/presentation/view_objects/suggestion_profile.dart';
 import 'package:demopico/features/user/domain/interfaces/i_users_repository.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:demopico/features/user/infra/repositories/users_repository.dart';
 
 class GetSugestionsUserUc {
-  final IUsersRepository _repository;
+  final IUsersRepository _userRepository;
+  final INetworkRepository _networkRepository;
 
   static GetSugestionsUserUc? _instance;
   static GetSugestionsUserUc get instance {
     _instance ??= GetSugestionsUserUc(
       repository: UsersRepository.getInstance,
+      networkRepository: NetworkRepository.instance,
     );
     return _instance!;
   }
 
-  GetSugestionsUserUc({required IUsersRepository repository})
-      : _repository = repository;
+  GetSugestionsUserUc({required IUsersRepository repository, required INetworkRepository networkRepository})
+      : _userRepository = repository,
+        _networkRepository = networkRepository;
 
-  Future<List<SuggestionProfile>> execute(UserM user) async {
-    if (user.connections.isEmpty) {
-      final listUsers = await _repository.getSuggestionsProfileExcept(user.id);
-      return listUsers.map((user) {
-        return SuggestionProfile.fromUser(user);
-      }).toList();
-    }
-    final listExcept = user.connections;
-    listExcept.add(user.id);
+  Future<List<SuggestionProfile>> execute(String currentUserId) async {
+    final allUsers = await _userRepository.findAll();
+    final myConnections = await _networkRepository.getConnections(currentUserId);
 
-    final suggestions  = await _repository.getSuggestionsExceptConnections(listExcept.toSet());
-    return suggestions.map((user) {
-      return SuggestionProfile.fromUser(user);
-    }).toList();
+    final connectedIds = myConnections
+        .map((c) => c.requesterUserID == currentUserId ? c.addresseeID : c.requesterUserID)
+        .toSet();
+
+    return allUsers
+        .where((u) => u.id != currentUserId && !connectedIds.contains(u.id))
+        .map((u) => SuggestionProfile.fromUser(u))
+        .toList();
   }
 }
