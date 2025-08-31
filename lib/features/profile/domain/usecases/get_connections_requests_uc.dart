6b55ff3097715f5010ef26f6ec3270e0dd0b3fc3
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-31 12:19:56 -0300

    retornando retornando os perfis do usuários

--- lib/features/profile/domain/usecases/get_connections_requests_uc.dart
+++ lib/features/profile/domain/usecases/get_connections_requests_uc.dart
@@ -2,6 +2,11 @@
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 import 'package:demopico/features/profile/infra/repository/network_repository.dart';
+import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
+import 'package:demopico/features/user/domain/interfaces/i_users_repository.dart';
+import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
+import 'package:demopico/features/user/infra/repositories/users_repository.dart';
 
 class GetConnectionsRequestsUc {
 
@@ -9,16 +14,20 @@ class GetConnectionsRequestsUc {
   static GetConnectionsRequestsUc get instance {
     _instance ??= GetConnectionsRequestsUc(
       networkRepository: NetworkRepository.instance,
+      userDataRepository: UsersRepository.getInstance,
     );
     return _instance!;
   }
 
   final INetworkRepository _networkRepository;
+  final IUsersRepository _userDataRepository;
 
-  GetConnectionsRequestsUc({required INetworkRepository networkRepository})
-      : _networkRepository = networkRepository;
+  GetConnectionsRequestsUc({required INetworkRepository networkRepository, required IUsersRepository userDataRepository})
+      : _networkRepository = networkRepository,
+        _userDataRepository = userDataRepository;
 
-  Future<List<Relationship>> execute(String uid) async {
-    return await _networkRepository.getConnectionRequests(uid);
+  Future<List<UserM>> execute(String uid) async {
+    final relationship = await _networkRepository.getConnectionRequests(uid);
+    return await _userDataRepository.getUsersByIds(relationship.map((e) => e.requesterUserID).toList());
   }
 }
