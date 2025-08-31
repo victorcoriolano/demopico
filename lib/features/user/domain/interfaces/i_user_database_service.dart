f9899a8a7533de93616972a27e7473795e58ea7d
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-30 21:17:03 -0300

    metodo para ler todos os documentos

--- lib/features/user/domain/interfaces/i_user_database_service.dart
+++ lib/features/user/domain/interfaces/i_user_database_service.dart
@@ -3,9 +3,11 @@ abstract class IUserDataSource<DTO>{
   //CREATE
   Future<void> addUserDetails(DTO newUser);
   //GET
+  Future<List<DTO>> findAll();
   Future<DTO> getUserDetails(String uid);
   Future<String> getUserData(String id, String field);
   Future<DTO> getUserByField(String field, String value);
+
   Future<List<DTO>> getSuggestions(Set<String> connectionsUserExcept);
   Future<List<DTO>> getUsersExcept(String uid);
   //SEARCH
