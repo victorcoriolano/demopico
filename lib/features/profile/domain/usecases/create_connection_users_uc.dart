953d49a8f315ddc6224be6b9c673760cfbcce6be
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-30 21:16:11 -0300

    removendo lógica desnecessária ao use case

--- lib/features/profile/domain/usecases/create_connection_users_uc.dart
+++ lib/features/profile/domain/usecases/create_connection_users_uc.dart
@@ -1,32 +1,25 @@
 
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
-import 'package:demopico/features/profile/domain/models/connection.dart';
+import 'package:demopico/features/profile/domain/models/relationship.dart';
 import 'package:demopico/features/profile/infra/repository/network_repository.dart';
-import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
-import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 
 class CreateConnectionUsersUc {
   final INetworkRepository _repository;
-  final IUserDataRepository _userDataRepository;
 
   static CreateConnectionUsersUc? _instance;
 
   static CreateConnectionUsersUc get instance {
     _instance ??= CreateConnectionUsersUc(
       repository: NetworkRepository.instance,
-      userDataRepository: UserDataRepositoryImpl.getInstance,
     );
     return _instance!;
   }
 
-  CreateConnectionUsersUc({required INetworkRepository repository, required IUserDataRepository userDataRepository})
-      : _repository = repository,
-        _userDataRepository = userDataRepository;
+  CreateConnectionUsersUc({required INetworkRepository repository})
+      : _repository = repository;
 
-  Future<Connection> execute(Connection connection, UserM user) async {
-    user.connections.add(connection.userID);
-    await _userDataRepository.updateUserDetails(user);
+  Future<Relationship> execute(Relationship connection, UserM user) async {
     final output = await _repository.createConnection(connection);
     return output;
   }
