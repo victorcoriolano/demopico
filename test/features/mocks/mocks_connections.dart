03bd0cffb05bfdd3e4c7a5a146fb0146ff3546e7
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-01 22:12:03 -0300

    atualizações nos mocks de teste

--- test/features/mocks/mocks_connections.dart
+++ test/features/mocks/mocks_connections.dart
@@ -2,11 +2,11 @@
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 
 var dummyConnections = [
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUserID: 'userID1', addresseeID: 'userID4', status: RequestConnectionStatus.pending, updatedAt: DateTime.now()),
+  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID1', addressed: 'userID4', status: RequestConnectionStatus.pending, updatedAt: DateTime.now()),
 
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUserID: 'userID2', addresseeID: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now()),
+  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID2', addressed: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now()),
 
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUserID: 'userID3', addresseeID: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now())
-  ,Relationship(id: "id",createdAt: DateTime.now(), requesterUserID: 'userID4', addresseeID: 'userID3', status: RequestConnectionStatus.rejected, updatedAt: DateTime.now())
+  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID3', addressed: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now())
+  ,Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID4', addressed: 'userID3', status: RequestConnectionStatus.rejected, updatedAt: DateTime.now())
 
 ];
