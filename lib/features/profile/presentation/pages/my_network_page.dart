e1b00dbe58c5e4ba1ee9fb6cd3b592a136268b28
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-02 10:27:39 -0300

    corrigindo ui

--- lib/features/profile/presentation/pages/my_network_page.dart
+++ lib/features/profile/presentation/pages/my_network_page.dart
@@ -1,6 +1,7 @@
 import 'package:demopico/core/app/theme/theme.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 import 'package:demopico/features/profile/presentation/provider/network_view_model.dart';
+import 'package:demopico/features/profile/presentation/widgets/search_page_widgets/connection_action_card.dart';
 import 'package:demopico/features/user/domain/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -63,7 +64,7 @@ enum ActionType {
 }
 
 class ProfileList extends StatelessWidget {
-  final List<Relationship> userProfiles;
+  final List<ReciverRequesterBase> userProfiles;
   final ActionType actionType;
 
   const ProfileList({super.key, required this.userProfiles, required this.actionType});
@@ -76,76 +77,25 @@ class ProfileList extends StatelessWidget {
       itemBuilder: (context, index) {
         return Card(
           margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-          child: ListTile(
-            leading: const CircleAvatar(
-              child: Icon(Icons.person),
-            ),
-            title: Text(
-              userProfiles[index].name,
-              style: const TextStyle(fontWeight: FontWeight.w600),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Condicional para exibir os botões de ação corretos
-                if (actionType == ActionType.accept) 
-                  ActionButton(
-                    icon: Icons.check,
-                    color: Colors.green,
-                    tooltip: 'Aceitar',
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Solicitação aceita!'),
-                          duration: Duration(seconds: 1),
-                        ),
-                      );
-                    },
-                  ),
-                
-                  ActionButton(
-                    icon: Icons.cancel,
-                    color: Colors.grey,
-                    tooltip: 'Cancelar',
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Solicitação cancelada!'),
-                          duration: Duration(seconds: 1),
-                        ),
-                      );
-                    },
-                  ),
-              ],
-            ),
-          ),
-        );
+          child: ConnectionActionCard(
+            user: userProfiles[index],
+            actionButton: actionType == ActionType.accept 
+              ? ElevatedButton(
+                  onPressed: () {
+                    // TODO IMPLEMENTAR ACEITAR SOLICITAÇÃO DE CONEXÃO
+                  },
+                  child: const Text('Aceitar'),
+                )
+              : ElevatedButton(
+                  onPressed: () {
+                    // TODO IMPLEMENTAR CANCELAR SOLICITAÇÃO DE CONEXÃO
+                  },
+                  child: const Text('Cancelar'),
+                ),
+              ),
+            );
       },
     );
   }
 }
 
-// Widget separado para o botão de ação, para evitar duplicação de código
-class ActionButton extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final String tooltip;
-  final VoidCallback onPressed;
-
-  const ActionButton({
-    super.key,
-    required this.icon,
-    required this.color,
-    required this.tooltip,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(icon, color: color),
-      tooltip: tooltip,
-      onPressed: onPressed,
-    );
-  }
-}
