cb11635a23a1b40e219c5412a09035dbe9c1d9e6
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-01 22:03:23 -0300

    corrigindo repository

--- lib/features/profile/domain/interfaces/i_network_repository.dart
+++ lib/features/profile/domain/interfaces/i_network_repository.dart
@@ -1,10 +1,10 @@
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 
 abstract class INetworkRepository {
-  Future<Relationship> checkConnection(String idConnection);
   Future<Relationship> updateConnection(Relationship connection);
   Future<List<Relationship>> getConnections(String userID);
   Future<List<Relationship>> getConnectionRequests(String userID);
+  Future<List<Relationship>> getConnectionsSent(String userID);
   Future<Relationship> createConnection(Relationship connection);
   Future<void> disconnectUser(Relationship connection);
 }
