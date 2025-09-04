4903f16ac87b6718c9ad8bdb137de1a32d4618df
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-04 08:25:34 -0300

    corrigindo mocks com a nova estrutura

--- test/features/mocks/mocks_connections.dart
+++ test/features/mocks/mocks_connections.dart
@@ -1,12 +1,51 @@
-
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 
 var dummyConnections = [
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID1', addressed: 'userID4', status: RequestConnectionStatus.pending, updatedAt: DateTime.now()),
+  Relationship(
+      id: "id",
+      createdAt: DateTime.now(),
+      requesterUser: ConnectionRequester(
+          id: "userID1", name: "name", profilePictureUrl: "profilePictureUrl"),
+      addressed: ConnectionReceiver(
+          id: "addressedId",
+          name: "addressedName",
+          profilePictureUrl: "addressedProfilePictureUrl"),
+      status: RequestConnectionStatus.pending,
+      updatedAt: DateTime.now()),
 
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID2', addressed: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now()),
+  Relationship(
+      id: "id",
+      createdAt: DateTime.now(),
+      requesterUser: ConnectionRequester(
+          id: "userID2", name: "name", profilePictureUrl: "profilePictureUrl"),
+      addressed: ConnectionReceiver(
+          id: "addressedId2",
+          name: "addressedName2",
+          profilePictureUrl: "addressedProfilePictureUrl2"),
+      status: RequestConnectionStatus.accepted,
+      updatedAt: DateTime.now()),
 
-  Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID3', addressed: 'userID1', status: RequestConnectionStatus.accepted, updatedAt: DateTime.now())
-  ,Relationship(id: "id",createdAt: DateTime.now(), requesterUser: 'userID4', addressed: 'userID3', status: RequestConnectionStatus.rejected, updatedAt: DateTime.now())
+  Relationship(
+      id: "id",
+      createdAt: DateTime.now(),
+      requesterUser: ConnectionRequester(
+          id: "userID3", name: "name", profilePictureUrl: "profilePictureUrl"),
+      addressed: ConnectionReceiver(
+          id: "addressedId3",
+          name: "addressedName3",
+          profilePictureUrl: "addressedProfilePictureUrl3"),
+      status: RequestConnectionStatus.accepted,
+      updatedAt: DateTime.now()),
 
+  Relationship(
+      id: "id",
+      createdAt: DateTime.now(),
+      requesterUser: ConnectionRequester(
+          id: "userID4", name: "name", profilePictureUrl: "profilePictureUrl"),
+      addressed: ConnectionReceiver(
+          id: "addressedId4",
+          name: "addressedName4",
+          profilePictureUrl: "addressedProfilePictureUrl4"),
+      status: RequestConnectionStatus.rejected,
+      updatedAt: DateTime.now())
 ];
