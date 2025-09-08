7091ab8b2620fac14f8bc18569ce5cd333b2ddba
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-08 09:59:39 -0300

    método inutilizado

--- lib/core/common/auth/domain/interfaces/i_auth_repository.dart
+++ lib/core/common/auth/domain/interfaces/i_auth_repository.dart
@@ -1,5 +1,4 @@
 import 'package:demopico/core/common/auth/domain/entities/auth_result.dart';
-import 'package:demopico/core/common/auth/domain/entities/auth_tokens.dart';
 import 'package:demopico/core/common/auth/domain/entities/user_entity.dart';
 import 'package:demopico/features/user/domain/enums/auth_state.dart';
 
@@ -7,7 +6,6 @@ abstract class IAuthRepository {
   Future<AuthResult> signInWithEmail(String email, String password);
   Future<AuthResult> signInWithGoogle(); // new provider
   Future<void> signOut();
-  Future<AuthTokens?> refreshTokensIfNeeded(); // returns null if unauthenticated
   Stream<AuthState> get authState; // emits Authenticated(User) or Unauthenticated
   TypeUser? get currentUser; // in-memory domain user
 }
