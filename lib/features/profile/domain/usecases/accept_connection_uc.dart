54694879f50b5b39bc1a84608ad61142f35a6931
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-09-10 17:54:53 -0300

    passando regra de negocio teste

--- lib/features/profile/domain/usecases/accept_connection_uc.dart
+++ lib/features/profile/domain/usecases/accept_connection_uc.dart
@@ -2,6 +2,7 @@
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 import 'package:demopico/features/profile/infra/repository/network_repository.dart';
+import 'package:demopico/features/profile/presentation/view_objects/relationship_vo.dart';
 
 class AcceptConnectionUc {
   static AcceptConnectionUc? _instance;
@@ -17,7 +18,8 @@ class AcceptConnectionUc {
   AcceptConnectionUc({required INetworkRepository networkRepository})
       : _networkRepository = networkRepository;
 
-  Future<Relationship> execute(Relationship connection) async {
+  Future<Relationship> execute(RelationshipVo connection) async {
+    connection.status = RequestConnectionStatus.accepted;
     return await _networkRepository.updateRelationship(connection);
   }
 }
