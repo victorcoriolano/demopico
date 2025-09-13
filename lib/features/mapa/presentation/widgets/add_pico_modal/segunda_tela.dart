d331be891574f12035aeb920b0de041b60457f4b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-13 11:13:23 -0300

    corrigindo bugs na segunda tela de postar pico

--- lib/features/mapa/presentation/widgets/add_pico_modal/segunda_tela.dart
+++ lib/features/mapa/presentation/widgets/add_pico_modal/segunda_tela.dart
@@ -1,7 +1,5 @@
 import 'package:demopico/core/app/theme/theme.dart';
-import 'package:demopico/features/mapa/domain/value_objects/attributes_vo.dart';
 import 'package:demopico/features/mapa/presentation/controllers/add_pico_view_model.dart';
-import 'package:demopico/features/mapa/presentation/widgets/spot_info_widgets/atribute_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,87 +15,89 @@ class _SegundaTelaState extends State<SegundaTela> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AddPicoViewModel>(
-      builder: (context, provider, child) => Scaffold(
-          backgroundColor: Colors.grey[200],
-          body: Center(
-            child: SingleChildScrollView(
-              child: AttributesWidget(attributesVO: provider.attributesVO)
-            ),
-          )),
-    );
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: Center(
+          child: SingleChildScrollView(
+            child: AttributesWidget(),
+          ),
+        ));
   }
 
   // Widget para construir cada linha de atributo
   }
 
 class AttributesWidget extends StatelessWidget {
-  final AttributesVO attributesVO;
-  const AttributesWidget({super.key, required this.attributesVO});
+  
+  const AttributesWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os itens
-      children: [
-        ...List.generate(attributesVO.getAttributesList.length, (index) {
-          final nameAttribute = attributesVO.getAttributesList[index];
-          final attributeEntry = attributesVO.attributes.entries.elementAt(index);
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os itens
-      children: [
-        Text(
-          '$nameAttribute:',
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        const SizedBox(height: 5),
-
-        // Avaliação por imagens customizadas
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center, // Centraliza os ícones
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: List.generate(5, (index) {
-            return Padding(
-              padding: const EdgeInsets.all(
-                  0.0), // Ajustado para um espaçamento menor
-              child: IconButton(
-                  icon: Image.asset(
-                    height:
-                        40.0,
-                    width: 42.0,
-                    'assets/images/iconPico.png', 
-                    color: index < attributesVO.attributes.values.toList()[index]
-                        ? kRed
-                        : Colors.grey[
-                            350],
-                  ),
-            
-                  onPressed: () {
-                    attributesVO.updateRate(attributeEntry.key, index + 1); // Atualiza o valor do atributo
-                  }),
-            );
-          }),
-        ),
-
-        // Descrição abaixo dos ícones
-        Text(
-          attributesVO.obterDescricao(attributeEntry.key, attributeEntry.value),
-          style: const TextStyle(
-            fontSize: 13,
-            color: Colors.black54,
-          ),
-        ),
-      ],
-          );
-        } ),
-        // Nome do atributo
+    return Consumer<AddPicoViewModel>(
+      builder: (context, viewModel, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os itens
+          children: [
+            ...List.generate(viewModel.attributesVO.getAttributesList.length, (index) {
+              //final nameAttribute = attributesVO.getAttributesList[index];
+              final attributeEntry = viewModel.attributesVO.attributes.entries.elementAt(index);
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center, // Centraliza os itens
+          children: [
+            Text(
+              '${viewModel.attributesVO.getAttributesList[index]}:',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 5),
         
-        const SizedBox(height: 7),
-      ],
+            // Avaliação por imagens customizadas
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // Centraliza os ícones
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List.generate(5, (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(
+                      0.0), // Ajustado para um espaçamento menor
+                  child: IconButton(
+                      icon: Image.asset(
+                        height:
+                            40.0,
+                        width: 42.0,
+                        'assets/images/iconPico.png', 
+                        color: index < attributeEntry.value
+                            ? kRed
+                            : Colors.grey[
+                                350],
+                      ),
+                
+                      onPressed: () {
+                        viewModel.updateAttribute(attributeEntry.key, index + 1); // Atualiza o valor do atributo
+                      }),
+                );
+              }),
+            ),
+        
+            // Descrição abaixo dos ícones
+            Text(
+              viewModel.getDescription(attributeEntry.key, attributeEntry.value),
+              style: const TextStyle(
+                fontSize: 13,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+              );
+            } ),
+            // Nome do atributo
+            
+            const SizedBox(height: 7),  
+          ],
+        );
+      }
     );
   }
 }
