f2e136b123f6e2982419062becfc222a05c189d5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-18 11:05:35 -0300

    removendo interface que não sera mais utilizada

--- lib/features/user/domain/interfaces/i_user_auth_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:demopico/core/common/auth/domain/entities/user_credentials.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
-
-abstract class IUserAuthRepository {
-  Future<UserM> signUp(UserCredentialsSignUp cadastroCredentials);
-  Future<String> login(UserCredentialsSignIn loginCredentials);
-  Future<void> logout();
-}
