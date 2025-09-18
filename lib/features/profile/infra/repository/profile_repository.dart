e7e7a665f7146bf41eb55b8337e5a055bde56b54
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-18 11:01:15 -0300

    repository do profile

---
+++ lib/features/profile/infra/repository/profile_repository.dart
@@ -0,0 +1,37 @@
+
+import 'package:demopico/core/common/auth/domain/entities/profile_result.dart';
+import 'package:demopico/core/common/auth/domain/entities/profile_user.dart';
+import 'package:demopico/core/common/auth/domain/interfaces/i_profile_repository.dart';
+
+class ProfileRepositoryImpl implements IProfileRepository{
+    static ProfileRepositoryImpl? _instance;
+
+  static ProfileRepositoryImpl get instance => _instance ??
+      ProfileRepositoryImpl();
+
+      
+  @override
+  Future<ProfileResult> createProfile(Profile profile) {
+    // TODO: implement createProfile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteProfile(String idUser) {
+    // TODO: implement deleteProfile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ProfileResult> getProfileByUser(String id) {
+    // TODO: implement getProfileByUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ProfileResult> updateProfile(Profile profile) {
+    // TODO: implement updateProfile
+    throw UnimplementedError();
+  }
+  
+}
