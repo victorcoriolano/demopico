54822df64377bf90ff2b499e8894e5e6a48932ca
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-18 11:01:41 -0300

    tela de editar com novas alterações

--- lib/features/profile/presentation/pages/edit_profile_page.dart
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:demopico/core/app/theme/theme.dart';
 import 'package:demopico/features/profile/presentation/widgets/profile_data/editable_custom_field.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:demopico/features/user/domain/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
