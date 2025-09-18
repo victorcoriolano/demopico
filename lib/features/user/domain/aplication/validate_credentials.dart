9faead6967f854389b5c196af972ff49763f6ad2
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-18 11:04:19 -0300

    validação de credenciais refatorada

--- lib/features/user/domain/aplication/validate_credentials.dart
+++ lib/features/user/domain/aplication/validate_credentials.dart
@@ -1,83 +1,76 @@
+import 'package:demopico/core/common/auth/domain/interfaces/i_user_repository.dart';
 import 'package:demopico/core/common/errors/domain_failures.dart';
 import 'package:demopico/core/common/errors/repository_failures.dart';
 import 'package:demopico/core/common/auth/domain/entities/user_credentials.dart';
-import 'package:demopico/features/user/domain/enums/identifiers.dart';
-import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
 import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 import 'package:flutter/foundation.dart';
 
 class ValidateUserCredentials {
-  final IUserDataRepository repository;
+  final IUserRepository repository;
   ValidateUserCredentials({required this.repository});
 
   static ValidateUserCredentials? _instance;
   static ValidateUserCredentials get instance => _instance ??=
       ValidateUserCredentials(repository: UserDataRepositoryImpl.getInstance);
 
-  Future<UserCredentialsSignIn> validateForLogin(
-      UserCredentialsSignIn credentials) async {
-    
-    bool exists = false;
-    switch (credentials.identifier){
-      case (Identifiers.email):
-        exists = await repository.validateExist(
-          data: credentials.login, field: "email");
+  Future<EmailCredentialsSignIn> validateEmailExist(
+      EmailCredentialsSignIn credentials) async {
+    final bool exists;
+    exists = await repository.validateExist(
+        data: credentials.identifier.value, field: "email");
+    if (exists) return credentials;
+    throw InvalidCredentialsFailure();
+  }
 
-      case (Identifiers.vulgo):
-        exists = await repository.validateExist(
-          data: credentials.login, field: "name");    
-    }
+  Future<EmailCredentialsSignIn> validateVulgoExist(
+      EmailCredentialsSignIn credentials) async {
+    final bool exists;
+    exists = await repository.validateExist(
+        data: credentials.identifier.value, field: "name");
+    if (exists) return credentials;
+    throw InvalidCredentialsFailure();
+  }
 
-    if (exists) {
+  Future<NormalUserCredentialsSignUp> validateForSignUp(
+      NormalUserCredentialsSignUp credentials) async {
+    try {
+      debugPrint("Validando email");
+
+      final existEmail = await repository.validateExist(
+        data: credentials.email.value,
+        field: "email",
+      );
+      debugPrint("Email já existe: $existEmail");
+
+      debugPrint("Validando vulgo");
+      final existVulgo = await repository.validateExist(
+        data: credentials.vulgo.value,
+        field: "name",
+      );
+      debugPrint("Vulgo já existe: $existVulgo");
+
+      if (existVulgo) {
+        debugPrint("Lançando exception para vulgo inválido");
+        throw VulgoAlreadyExistsFailure();
+      }
+
+      if (existEmail) {
+        debugPrint("Lançando exception para email inválido");
+        throw EmailAlreadyInUseFailure();
+      }
+
       debugPrint("Credenciais válidas");
       return credentials;
-    } else {
-      debugPrint("Credenciais inválidas");
-      throw InvalidCredentialsFailure();
+    } on EmailAlreadyInUseFailure catch (e) {
+      debugPrint("Erro ao validar: email já está em uso - $e");
+      rethrow;
+    } on VulgoAlreadyExistsFailure catch (e) {
+      debugPrint("Erro ao validar: vulgo já existente - $e");
+      rethrow;
+    } catch (e, st) {
+      // Tratamento genérico para outras falhas conhecidas
+      debugPrint("Erro genérico ao validar credenciais: $e");
+      throw UnknownFailure(unknownError: e, stackTrace: st);
     }
   }
-
- Future<UserCredentialsSignUp> validateForSignUp(UserCredentialsSignUp credentials) async {
-  try {
-
-    debugPrint("Validando email");
-
-    final existEmail = await repository.validateExist(
-      data: credentials.email, 
-      field: "email",
-    );
-    debugPrint("Email já existe: $existEmail");
-
-    debugPrint("Validando vulgo");
-    final existVulgo = await repository.validateExist(
-      data: credentials.nome, 
-      field: "name",
-    );
-    debugPrint("Vulgo já existe: $existVulgo");
-    
-    if (existVulgo) {
-      debugPrint("Lançando exception para vulgo inválido");
-      throw VulgoAlreadyExistsFailure() ;
-    }
-
-    if (existEmail) {
-      debugPrint("Lançando exception para email inválido");
-      throw EmailAlreadyInUseFailure();
-    }
-
-    debugPrint("Credenciais válidas");
-    return credentials;
-  } on EmailAlreadyInUseFailure catch (e) {
-    debugPrint("Erro ao validar: email já está em uso - $e");
-    rethrow;
-  } on VulgoAlreadyExistsFailure catch (e) {
-    debugPrint("Erro ao validar: vulgo já existente - $e");
-    rethrow;
-  } catch (e,st) {
-    // Tratamento genérico para outras falhas conhecidas
-    debugPrint("Erro genérico ao validar credenciais: $e");
-    throw UnknownFailure(unknownError: e, stackTrace: st);
-  }
-}
-
 }
