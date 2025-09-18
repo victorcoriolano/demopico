5e583f6a676df31bb07a973857cf350b3860bd05
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-18 11:10:37 -0300

    lógica de atualizar dados do user refatorada

--- lib/features/user/domain/usecases/update_data_user_uc.dart
+++ lib/features/user/domain/usecases/update_data_user_uc.dart
@@ -1,21 +1,21 @@
 
-import 'package:demopico/features/user/domain/interfaces/i_user_database_repository.dart';
-import 'package:demopico/features/user/domain/models/user.dart';
+import 'package:demopico/core/common/auth/domain/interfaces/i_user_repository.dart';
+import 'package:demopico/features/user/domain/models/user_model.dart';
 import 'package:demopico/features/user/infra/repositories/user_data_repository_impl.dart';
 
-class UpdateDataUserUc {
-  final IUserDataRepository _repository;
+class UpdateUserUc {
+  final IUserRepository _repository;
 
-  static UpdateDataUserUc? _instance;
-  static UpdateDataUserUc get getInstance {
-    _instance ??= UpdateDataUserUc(repository: UserDataRepositoryImpl.getInstance);
+  static UpdateUserUc? _instance;
+  static UpdateUserUc get getInstance {
+    _instance ??= UpdateUserUc(repository: UserDataRepositoryImpl.getInstance);
     return _instance!;
   }
 
-  UpdateDataUserUc({required IUserDataRepository repository})
+  UpdateUserUc({required IUserRepository repository})
       : _repository = repository;
 
   Future<void> call(UserM user) async {
-    await _repository.updateUserDetails(user);
+    await _repository.update(user);
   }
 }
