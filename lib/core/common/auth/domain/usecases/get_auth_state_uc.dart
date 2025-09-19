17572be1886bbd5681f212ea168f69a287bc7d08
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-19 20:16:00 -0300

    get instance no auth state

--- lib/core/common/auth/domain/usecases/get_auth_state_uc.dart
+++ lib/core/common/auth/domain/usecases/get_auth_state_uc.dart
@@ -1,14 +1,24 @@
 
 import 'package:demopico/core/common/auth/domain/interfaces/i_auth_repository.dart';
+import 'package:demopico/core/common/auth/infra/repositories/firebase_auth_repository.dart';
 import 'package:demopico/features/user/domain/enums/auth_state.dart';
 
 class GetAuthStateUc   {
   final IAuthRepository _authRepository;
 
+  
+    static GetAuthStateUc? _instance;
+    
+    static GetAuthStateUc get instance =>
+      _instance ??= GetAuthStateUc(
+        authRepository: FirebaseAuthRepository.instance
+      );
+  
+
   GetAuthStateUc({required IAuthRepository authRepository})
     : _authRepository = authRepository;
 
-  Stream<AuthState> execute() {
-    return _authRepository.authState;
+  AuthState execute() {
+    return _authRepository.currentAuthState;
   }
 }
