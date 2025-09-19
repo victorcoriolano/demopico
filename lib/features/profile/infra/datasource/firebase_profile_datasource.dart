426d9fc914c49789dd009981983eaae5ec5fd9f2
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-19 17:36:48 -0300

    implementando datasource de profile

--- lib/features/profile/infra/datasource/firebase_profile_datasource.dart
+++ lib/features/profile/infra/datasource/firebase_profile_datasource.dart
@@ -1,31 +1,43 @@
 
+import 'package:demopico/core/common/collections/collections.dart';
+import 'package:demopico/features/external/datasources/firebase/crud_firebase.dart';
+import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_profile_data_source.dart';
 import 'package:demopico/features/profile/domain/models/profile_result.dart';
 import 'package:demopico/features/profile/domain/models/profile_user.dart';
 
-class FirebaseProfileDatasource implements IProfileDataSource{
+class FirebaseProfileDatasource implements IProfileDataSource<FirebaseDTO>{
+  static FirebaseProfileDatasource? _instance;
+  static FirebaseProfileDatasource get getInstance {
+    return _instance ??= FirebaseProfileDatasource(
+      crud: CrudFirebase.getInstance..setcollection(Collections.profile)
+    );
+  }
+
+
+  final CrudFirebase _crudFirebase;
+  
+  
+  FirebaseProfileDatasource({required CrudFirebase crud}): _crudFirebase = crud;
+
   @override
-  Future<ProfileResult> createProfile(Profile profile) {
-    // TODO: implement createProfile
-    throw UnimplementedError();
+  Future<FirebaseDTO> createProfile(FirebaseDTO profile) async  {
+    return await _crudFirebase.create(profile);
   }
 
   @override
-  Future<void> deleteProfile(String idUser) {
-    // TODO: implement deleteProfile
-    throw UnimplementedError();
+  Future<void> deleteProfile(String idUser) async  {
+    return await _crudFirebase.delete(idUser);
   }
 
   @override
-  Future<ProfileResult> getProfileByUser(String id) {
-    // TODO: implement getProfileByUser
-    throw UnimplementedError();
+  Future<FirebaseDTO> getProfileByUser(String id) async  {
+    return await _crudFirebase.read(id);
   }
 
   @override
-  Future<ProfileResult> updateProfile(Profile profile) {
-    // TODO: implement updateProfile
-    throw UnimplementedError();
+  Future<FirebaseDTO> updateProfile(FirebaseDTO profile) async  {
+    return await _crudFirebase.update(profile);
   }
   
 }
