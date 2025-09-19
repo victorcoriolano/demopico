a4aad20ca4f5677e59f572a1c55b6ce7f12a2757
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-19 08:52:27 -0300

    get instance no usecase de deletar conta

--- lib/core/common/auth/domain/usecases/delete_account_uc.dart
+++ lib/core/common/auth/domain/usecases/delete_account_uc.dart
@@ -1,7 +1,13 @@
 
 import 'package:demopico/core/common/auth/domain/interfaces/i_user_account_repository.dart';
+import 'package:demopico/core/common/auth/infra/repositories/firebase_account_repository.dart';
 
 class DeleteAccountUc {
+    static DeleteAccountUc? _deleteAccountUc;
+  static DeleteAccountUc get getInstance {
+  _deleteAccountUc ??= DeleteAccountUc(accountRepository: FirebaseAccountRepository.instance);
+    return _deleteAccountUc!;
+  }
   final IUserAccountRepository _accountRepository;
 
   DeleteAccountUc({required IUserAccountRepository accountRepository})
