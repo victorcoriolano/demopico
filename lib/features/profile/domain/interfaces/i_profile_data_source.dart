ec9d6f01559aaa01fbcb2ce58aafb2137ee7fa0f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-19 17:35:44 -0300

    adcionando métodos a interface do datasource

--- lib/features/profile/domain/interfaces/i_profile_data_source.dart
+++ lib/features/profile/domain/interfaces/i_profile_data_source.dart
@@ -1,3 +1,10 @@
-abstract class IProfileDataSource {
+abstract class IProfileDataSource<DTO> {
+  Future<DTO> createProfile(DTO profile);
 
+  
+  Future<void> deleteProfile(String idUser);
+  
+  Future<DTO> getProfileByUser(String id);
+  
+ Future<DTO> updateProfile(DTO profile);
 }
