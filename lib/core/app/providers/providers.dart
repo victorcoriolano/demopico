fbedf8a5dd6302599b963eea5c23b434c2332ff3
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-04 15:12:53 -0300

    providers organizados

--- lib/core/app/providers/providers.dart
+++ lib/core/app/providers/providers.dart
@@ -27,36 +27,55 @@ import 'package:demopico/features/user/presentation/controllers/auth_view_model_
 import 'package:demopico/features/user/presentation/controllers/auth_view_model_sign_in.dart';
 import 'package:demopico/features/user/presentation/controllers/auth_view_model_sign_up.dart';
 import 'package:demopico/features/user/presentation/controllers/profile_view_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 
 final myProviders = [
-  ChangeNotifierProvider(create: (_) => NetworkViewModel.instance),
-  ChangeNotifierProvider(create: (_) => OpenWeatherProvider()),
-  ChangeNotifierProvider(
-      create: (_) =>
-          ForecastProvider(null, climaService: HttpClimateService())),
+// Providers de autenticação primeiro
+  ChangeNotifierProvider(create: (_) => AuthViewModelAccount.instance),
   ChangeNotifierProvider(create: (_) => AuthViewModelSignIn.getInstance),
   ChangeNotifierProvider(create: (_) => AuthViewModelSignUp.getInstance),
-    ChangeNotifierProvider(create: (_) => AuthViewModelAccount.instance),
-  ChangeNotifierProvider(create: (_) => ScreenProvider()),
+  
+  // Provider de perfil
+  ChangeNotifierProvider(create: (_) => ProfileViewModel.getInstance),
+  
+  // Providers de clima
+  ChangeNotifierProvider(create: (_) {
+    debugPrint('criou o OpenWeatherProvider');
+    return OpenWeatherProvider();
+  }),
+  
+  ChangeNotifierProvider(
+      create: (_) => ForecastProvider(null, climaService: HttpClimateService())),
+
   StreamProvider<AuthState>(
     create: (_) => FirebaseAuthRepository.instance.authState,
     initialData: AuthUnauthenticated(),
   ),
+  
+  // Providers de UI/estado
+  ChangeNotifierProvider(create: (_) => NetworkViewModel.instance),
+  ChangeNotifierProvider(create: (_) => ScreenProvider()),
+  ChangeNotifierProvider(create: (_) => HomeProvider.getInstance),
+  
+  // Providers de mapa
   ChangeNotifierProvider(create: (_) => AddPicoViewModel.getInstance),
   ChangeNotifierProvider(create: (_) => MapControllerProvider()),
   ChangeNotifierProvider(create: (_) => FavoriteSpotController.getInstance),
   ChangeNotifierProvider(create: (_) => SpotsControllerProvider.getInstance),
-  ChangeNotifierProvider(create: (_) => ProfileViewModel.getInstance),
+  ChangeNotifierProvider(create: (_) => SpotProvider.instance),
   ChangeNotifierProvider(create: (_) => HistoricoController.getInstance),
+  ChangeNotifierProvider(create: (_) => CommentController.getInstance),
+  
+  // Providers de hub
   ChangeNotifierProvider(
     create: (_) => HubProvider(
       postarComunicado: PostarComunicado.getInstance,
       listarComunicado: ListarComunicado.getInstance,
     ),
   ),
-  ChangeNotifierProvider(create: (_) => HomeProvider.getInstance),
-  ChangeNotifierProvider(create: (_) => CommentController.getInstance),
+  
+  // Provider de posts
   ChangeNotifierProvider(
       create: (_) => PostProvider(
             createPostUc: CreatePostUc.instace,
@@ -66,5 +85,4 @@ final myProviders = [
             updateUc: UpdatePostUc.instance,
             pickVideo: PickVideoUC.getInstance,
           )),
-  ChangeNotifierProvider(create: (_) => SpotProvider.instance)
 ];
