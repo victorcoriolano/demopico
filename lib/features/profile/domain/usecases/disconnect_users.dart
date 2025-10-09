92f0acc6b517f310c7bb52736e404a80d3711c63
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-09 09:22:06 -0300

    use case de desconectar usuários

---
+++ lib/features/profile/domain/usecases/disconnect_users.dart
@@ -0,0 +1,44 @@
+
+import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
+import 'package:demopico/features/profile/domain/interfaces/i_profile_repository.dart';
+import 'package:demopico/features/profile/domain/models/relationship.dart';
+import 'package:demopico/features/profile/infra/repository/network_repository.dart';
+import 'package:demopico/features/profile/infra/repository/profile_repository.dart';
+
+class DisconnectUsers {
+  static DisconnectUsers? _instance;
+  static DisconnectUsers get instance {
+    _instance ??= DisconnectUsers(
+      networkRepository: NetworkRepository.instance,
+      profileRepository: ProfileRepositoryImpl.getInstance,
+    );
+    return _instance!;
+  }
+
+  final INetworkRepository _networkRepository;
+  final IProfileRepository _profileRepository;
+
+  DisconnectUsers({
+    required INetworkRepository networkRepository,
+    required IProfileRepository profileRepository})
+      : _networkRepository = networkRepository,
+        _profileRepository =profileRepository ;
+
+  Future<void> execute(Relationship connection) async {
+    await _networkRepository.deleteRelationship(connection.id);
+    // atualizando os dois perfis 
+    var profileRequesterUser = await _profileRepository.getProfileByUser(connection.requesterUser.id);
+    var profileAddressedUser = await _profileRepository.getProfileByUser(connection.addressed.id);
+    
+    profileAddressedUser.profile!.connections.remove(connection.requesterUser.id);
+    profileRequesterUser.profile!.connections.remove(connection.addressed.id);
+
+    await Future.wait(
+      [
+        _profileRepository.updateProfile(profileAddressedUser.profile!),
+        _profileRepository.updateProfile(profileRequesterUser.profile!)
+      ]
+    );
+
+  }
+}
