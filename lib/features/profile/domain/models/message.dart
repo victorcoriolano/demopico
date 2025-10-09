da02ce09ff74a15468dbea1338a6c3c03886dd42
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-09 07:38:46 -0300

    modelagem de mensagem da aplicação

---
+++ lib/features/profile/domain/models/message.dart
@@ -0,0 +1,46 @@
+import 'package:demopico/features/profile/domain/models/relationship.dart';
+
+class Message {
+  final String content;
+  final BasicInfoUser infoUser;
+  final DateTime dateTime;
+  final bool isRead;
+  final bool isSent;
+  final String? spotId;
+  final String? postId;  
+
+  const Message({
+    required this.isSent,
+    required this.content,
+    required this.dateTime,
+    required this.infoUser,
+    required this.isRead,
+    this.postId,
+    this.spotId,
+  });
+
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      content: map['content'] as String,
+      infoUser: BasicInfoUser.fromJson(map['infoUser'] as Map<String,dynamic>),
+      dateTime: DateTime.parse(map['dateTime'] as String),
+      isRead: map['isRead'] as bool,
+      isSent: map['isSent'] as bool,
+      spotId: map['spotId'] as String?,
+      postId: map['postId'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'content': content,
+      'infoUser': infoUser.toJson(),
+      'dateTime': dateTime.toIso8601String(),
+      'isRead': isRead,
+      'isSent': isSent,
+      'spotId': spotId,
+      'postId': postId,
+    };
+  }
+}
