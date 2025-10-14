750ee36b425fb5e6b6275b6d32d58e0c992f6ba9
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-14 17:11:04 -0300

    renomeando pasta

--- lib/features/profile/presentation/widgets/chat_widgets.dart/in_message.dart
+++ lib/features/profile/presentation/widgets/chat_widgets.dart/in_message.dart
@@ -1,36 +1,43 @@
-import 'package:demopico/core/app/theme/theme.dart';
-import 'package:demopico/features/profile/presentation/widgets/chat_room_widgets.dart/triangle.dart';
+import 'dart:math';
+
+import 'package:demopico/features/profile/presentation/widgets/chat_widgets.dart/triangle.dart';
 import 'package:flutter/material.dart';
 
-class OutMessage extends StatelessWidget {
+class InMessage extends StatelessWidget {
   final String message;
-  const OutMessage({super.key, required this.message});
+  const InMessage({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(pi),
+          child: CustomPaint(
+            painter: Triangle(Colors.grey.shade300),
+          ),
+        ),
         Flexible(
           child: Container(
             padding: const EdgeInsets.all(15),
             margin: const EdgeInsets.only(bottom: 5),
             decoration: BoxDecoration(
-              color: kRed,
+              color: Colors.grey.shade300,
               borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(19),
+                topRight: Radius.circular(19),
                 bottomLeft: Radius.circular(19),
                 bottomRight: Radius.circular(19),
               ),
             ),
             child: Text(
               message,
-              style: const TextStyle(color: Colors.white, fontSize: 15),
+              style: const TextStyle(color: Colors.black, fontSize: 15),
             ),
           ),
         ),
-        CustomPaint(painter: Triangle(kRed)),
       ],
     );
   }
--- lib/features/profile/presentation/widgets/chat_widgets.dart/out_message.dart
+++ lib/features/profile/presentation/widgets/chat_widgets.dart/out_message.dart
@@ -1,43 +1,36 @@
-import 'dart:math';
-
-import 'package:demopico/features/profile/presentation/widgets/chat_room_widgets.dart/triangle.dart';
+import 'package:demopico/core/app/theme/theme.dart';
+import 'package:demopico/features/profile/presentation/widgets/chat_widgets.dart/triangle.dart';
 import 'package:flutter/material.dart';
 
-class InMessage extends StatelessWidget {
+class OutMessage extends StatelessWidget {
   final String message;
-  const InMessage({super.key, required this.message});
+  const OutMessage({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.rotationY(pi),
-          child: CustomPaint(
-            painter: Triangle(Colors.grey.shade300),
-          ),
-        ),
         Flexible(
           child: Container(
             padding: const EdgeInsets.all(15),
             margin: const EdgeInsets.only(bottom: 5),
             decoration: BoxDecoration(
-              color: Colors.grey.shade300,
+              color: kRed,
               borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(19),
+                topLeft: Radius.circular(19),
                 bottomLeft: Radius.circular(19),
                 bottomRight: Radius.circular(19),
               ),
             ),
             child: Text(
               message,
-              style: const TextStyle(color: Colors.black, fontSize: 15),
+              style: const TextStyle(color: Colors.white, fontSize: 15),
             ),
           ),
         ),
+        CustomPaint(painter: Triangle(kRed)),
       ],
     );
   }
