9026fe71bf65b9f3d0882429fe547116b4564588
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-14 20:12:40 -0300

    criando repository

--- lib/features/profile/infra/repository/message_repository.dart
+++ lib/features/profile/infra/repository/message_repository.dart
@@ -1,33 +1,38 @@
 
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto_mapper.dart';
+import 'package:demopico/features/external/datasources/firebase/firestore.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_message_datasource.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_message_repository.dart';
 import 'package:demopico/features/profile/domain/models/chat.dart';
 import 'package:demopico/features/profile/domain/models/message.dart';
+import 'package:demopico/features/profile/infra/datasource/firebase_message_datasourece.dart';
 
 class MessageRepository implements ImessageRepository {
   final IMessageDatasource<FirebaseDTO> _datasource;
-final chatMapper = FirebaseDtoMapper<Chat>(
-  
-  fromJson: (map, id) => Chat.fromMap(map, id),
-  
+final chatConversationMapper = FirebaseDtoMapper<Conversation>(
+  fromJson: (map, id) => Conversation.fromMap(map, id),
   toMap: (chat) => chat.toJson(),
-  
   getId: (chat) => chat.id,
 );
 
 final messageMapper = FirebaseDtoMapper<Message>(
-  
   fromJson: (map, id) => Message.fromMap(map, id), 
   toMap: (message) => message.toJson(),
-  
   getId: (message) => message.id, 
 );
 
   MessageRepository({
     required IMessageDatasource<FirebaseDTO> datasource,
   })  : _datasource = datasource;
+
+    static MessageRepository? _instance;
+
+
+  static MessageRepository get instance =>
+    _instance ??= MessageRepository(
+      datasource: MessageFirestoreDatasource(firestore: Firestore.getInstance)
+    );
 
   @override
   Stream<List<Message>> getMessagesForChat(String idChat) {
@@ -46,7 +51,7 @@ final messageMapper = FirebaseDtoMapper<Message>(
     final dtoList = await _datasource.getChatForUser(idUser);
 
     
-    return dtoList.map((dto) => chatMapper.toModel(dto)).toList();
+    return dtoList.map((dto) => chatConversationMapper.toModel(dto)).toList();
   }
 
   @override
