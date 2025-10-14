794caee38635928eaf003a8de2c70f670723a9e5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-14 20:13:06 -0300

    pagina de screens refatorada

--- lib/features/profile/presentation/pages/screens_profile.dart
+++ lib/features/profile/presentation/pages/screens_profile.dart
@@ -1,4 +1,4 @@
-import 'package:demopico/features/profile/presentation/pages/chat_room_page.dart';
+import 'package:demopico/features/profile/presentation/pages/chat_list_page.dart';
 import 'package:demopico/features/profile/presentation/pages/my_profile_page.dart';
 import 'package:demopico/features/profile/presentation/pages/search_profile_page.dart';
 import 'package:demopico/features/profile/presentation/view_model/screen_provider.dart';
@@ -7,21 +7,28 @@ import 'package:provider/provider.dart';
 import 'package:demopico/features/profile/presentation/widgets/profile_navigator_widget.dart';
 
 
-class ScreensProfile extends StatelessWidget {
-  const ScreensProfile({super.key});
+class ScreensProfile extends StatefulWidget {
+  const ScreensProfile({super.key,});
 
-  final List<Widget> _pages = const [
-    MyProfilePage(),
-    SearchProfilePage(),
-    ChatRoomPage(),
-  ];
+  @override
+  State<ScreensProfile> createState() => _ScreensProfileState();
+}
+
+class _ScreensProfileState extends State<ScreensProfile> {
+
 
   @override
   Widget build(BuildContext context) {
+
+      final List<Widget> pages = [
+    MyProfilePage(),
+    SearchProfilePage(),
+    ChatListPage(),
+  ];
     final currentIndex = context.watch<ScreenProvider>().currentIndex;
 
     return Scaffold(
-      body: _pages[currentIndex],
+      body: pages[currentIndex],
       bottomNavigationBar: const ProfileNavigatorWidget(),
     );
   }
