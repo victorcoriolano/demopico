25d8be953b05bd1a6cd5c6dc597fa0c3db1a392b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-14 17:15:45 -0300

    entidade de chat

--- lib/features/profile/domain/models/chat.dart
+++ lib/features/profile/domain/models/chat.dart
@@ -1,28 +1,59 @@
+import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/features/profile/domain/models/message.dart';
 
-class Chat {
-  final List<String> participants;
+sealed class Chat {}
+
+class Conversation extends Chat {
+  final List<String> participantsIds;
   final String id;
+  final UserIdentification user1;
+  final UserIdentification user2;
+  final Message lastReadMessage;
   final Message lastMessage;
+  final List<UserIdentification> participants;
   
-  Chat({
-    required this.id,
+  Conversation({
     required this.lastMessage,
     required this.participants,
-  });
+    required this.id,
+    required this.lastReadMessage,
+    required this.participantsIds,
+  }): user1 = participants[0], user2 = participants[1];
 
-   factory Chat.fromMap(Map<String, dynamic> map, String id) {
-    return Chat(
+   factory Conversation.fromMap(Map<String, dynamic> map, String id) {
+    return Conversation(
+      lastMessage: Message.fromMap(map['lastReadMessage'] as Map<String, dynamic>, map["lastReadMessage.id"]),
       id: id,
-      lastMessage: Message.fromMap(map['lastMessage'] as Map<String, dynamic>, map["lastMessage.id"]),
-      participants: List<String>.from(map['participants'] as List),
+      lastReadMessage: Message.fromMap(map['lastReadMessage'] as Map<String, dynamic>, map["lastReadMessage.id"]),
+      participantsIds: List<String>.from(map['participantsIds'] as List),
+      participants: (map['participants'] as List).map((user) { 
+        user = user as Map<String, dynamic>;
+        return UserIdentification.fromJson(user);
+      }).toList(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'lastMessage': lastMessage.toJson(),
-      'participants': participants,
+      'lasMessage': lastMessage.toJson(),
+      'lastReadMessage': lastReadMessage.toJson(),
+      'participantsIds': participantsIds,
+      'participants': participants.map((user) => user.toJson()),
     };
   }
+}
+
+class GroupChat extends Chat{
+  final String nameGroup;
+  final List<UserIdentification> participants;
+  final List<String> participantsIds;
+  final Message lastMessage;
+
+  GroupChat({
+    required this.lastMessage,
+    required this.nameGroup,
+    required this.participants,
+    required this.participantsIds,
+  });
+
 }
