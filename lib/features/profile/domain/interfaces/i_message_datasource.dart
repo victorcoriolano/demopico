528c1dca704fd0c63eb89289b42977192208ad2d
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 19:45:12 -0300

    retornando dto

--- lib/features/profile/domain/interfaces/i_message_datasource.dart
+++ lib/features/profile/domain/interfaces/i_message_datasource.dart
@@ -1,6 +1,7 @@
 abstract class IMessageDatasource<DTO> {
     Stream<List<DTO>> getMessagesForChat(String idChat);
     Future<List<DTO>> getChatForUser(String idUser);
-    Future<void> sendMessage(String idChat, DTO message);
+    Future<DTO> createChatForUser(DTO initialChat);
+    Future<DTO> sendMessage(String idChat, DTO message);
     Future<void> readMessage(String idChat, DTO message);
 }
