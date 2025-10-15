42ffe1d76cbd2e0feea88b7f1e3f3e2dfb5f67c8
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 20:01:09 -0300

    acertando os mocks

--- test/features/mocks/mocks_connections.dart
+++ test/features/mocks/mocks_connections.dart
@@ -1,12 +1,13 @@
+import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 
 var dummyConnections = [
   Relationship(
       id: "id",
       createdAt: DateTime.now(),
-      requesterUser: BasicInfoUser(
+      requesterUser: UserIdentification(
           id: "userIDnelson", name: "name", profilePictureUrl: "profilePictureUrl"),
-      addressed: BasicInfoUser(
+      addressed: UserIdentification(
           id: "addressedId",
           name: "addressedName",
           profilePictureUrl: "addressedProfilePictureUrl"),
@@ -15,9 +16,9 @@ var dummyConnections = [
   Relationship(
       id: "id2",
       createdAt: DateTime.now(),
-      requesterUser: BasicInfoUser(
+      requesterUser: UserIdentification(
           id: "userID1", name: "name", profilePictureUrl: "profilePictureUrl"),
-      addressed: BasicInfoUser(
+      addressed: UserIdentification(
           id: "addressedId",
           name: "addressedName",
           profilePictureUrl: "addressedProfilePictureUrl"),
@@ -26,9 +27,9 @@ var dummyConnections = [
   Relationship(
       id: "id3",
       createdAt: DateTime.now(),
-      requesterUser: BasicInfoUser(
+      requesterUser: UserIdentification(
           id: "userID2", name: "name", profilePictureUrl: "profilePictureUrl"),
-      addressed: BasicInfoUser(
+      addressed: UserIdentification(
           id: "addressedId2",
           name: "addressedName2",
           profilePictureUrl: "addressedProfilePictureUrl2"),
@@ -38,9 +39,9 @@ var dummyConnections = [
   Relationship(
       id: "id4",
       createdAt: DateTime.now(),
-      requesterUser: BasicInfoUser(
+      requesterUser: UserIdentification(
           id: "userID3", name: "name", profilePictureUrl: "profilePictureUrl"),
-      addressed: BasicInfoUser(
+      addressed: UserIdentification(
           id: "addressedId3",
           name: "addressedName3",
           profilePictureUrl: "addressedProfilePictureUrl3"),
@@ -50,9 +51,9 @@ var dummyConnections = [
   Relationship(
       id: "id5",
       createdAt: DateTime.now(),
-      requesterUser: BasicInfoUser(
+      requesterUser: UserIdentification(
           id: "userID4", name: "name", profilePictureUrl: "profilePictureUrl"),
-      addressed: BasicInfoUser(
+      addressed: UserIdentification(
           id: "addressedId4",
           name: "addressedName4",
           profilePictureUrl: "addressedProfilePictureUrl4"),
