b63655b072aa691d8eb9ccf6aa632a14a34c2999
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 19:59:04 -0300

    basic info substituida pela user identification

--- lib/features/profile/presentation/widgets/search_page_widgets/connection_action_card.dart
+++ lib/features/profile/presentation/widgets/search_page_widgets/connection_action_card.dart
@@ -1,10 +1,11 @@
+import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
 import 'package:demopico/features/profile/presentation/pages/profile_page_user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ConnectionActionCard extends StatefulWidget {
-  final BasicInfoUser user;
+  final UserIdentification user;
   final Widget actionButton;
 
   const ConnectionActionCard({ super.key, required this.actionButton, required this.user  });
