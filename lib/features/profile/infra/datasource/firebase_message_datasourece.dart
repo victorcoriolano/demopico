4a89ed9ef421ba4f1440970ded74b83a11a20750
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 19:49:06 -0300

    corrigindo bugs

--- lib/features/profile/infra/datasource/firebase_message_datasourece.dart
+++ lib/features/profile/infra/datasource/firebase_message_datasourece.dart
@@ -1,16 +1,29 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demopico/core/common/collections/collections.dart';
+import 'package:demopico/features/external/datasources/firebase/crud_firebase.dart';
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
+import 'package:demopico/features/external/datasources/firebase/firestore.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_message_datasource.dart';
+import 'package:flutter/material.dart';
 
 
 class MessageFirestoreDatasource implements IMessageDatasource<FirebaseDTO> {
-  final FirebaseFirestore _firestore;
+  final CrudFirebase _boilerplate;
 
-  MessageFirestoreDatasource({required FirebaseFirestore firestore}) : _firestore = firestore;
+  static MessageFirestoreDatasource? _instance;
+  static MessageFirestoreDatasource get instance {
+    return _instance ??= MessageFirestoreDatasource(
+      datasource: CrudFirebase(
+        collection: Collections.chats, 
+        firestore: Firestore.getInstance,
+      ),
+    );
+  }
+
+  MessageFirestoreDatasource({required CrudFirebase datasource}) : _boilerplate = datasource;
 
   @override
   Stream<List<FirebaseDTO>> getMessagesForChat(String idChat) {
-    final snapshots = _firestore
+    final snapshots = _boilerplate.dataSource
         .collection('chats')
         .doc(idChat)
         .collection('messages')
@@ -26,38 +39,49 @@ class MessageFirestoreDatasource implements IMessageDatasource<FirebaseDTO> {
 
   @override
   Future<List<FirebaseDTO>> getChatForUser(String idUser) async {
-    final querySnapshot = await _firestore
+    final querySnapshot = await _boilerplate.dataSource
         .collection('chats')
-        .where('participants', arrayContains: idUser)
+        .where('participantsIds', arrayContains: idUser)
         .get();
-
+    debugPrint("Chats founded: ${querySnapshot.docs.length}");
     return querySnapshot.docs.map((doc) {
       return FirebaseDTO(id: doc.id, data: doc.data());
     }).toList();
   }
 
   @override
-  Future<void> sendMessage(String idChat, FirebaseDTO message) async {
-
-    await _firestore
+  Future<FirebaseDTO> sendMessage(String idChat, FirebaseDTO message) async {
+    
+    final newDoc = await _boilerplate.dataSource
         .collection('chats')
         .doc(idChat)
         .collection('messages')
         .add(message.data);
 
-      await _firestore
+      await _boilerplate.dataSource
         .collection('chats')
         .doc(idChat)
-        .update({'lastReadMessage': message.data});
+        .update({
+          'lastMessage': message.data["content"],
+          'lastUpdate': message.data["dateTime"]
+        });
+      
+      return message.copyWith(id: newDoc.id);
   }
 
   @override
   Future<void> readMessage(String idChat, FirebaseDTO message) async {
-    await _firestore
+    await _boilerplate.dataSource
         .collection('chats')
         .doc(idChat)
         .collection('messages')
         .doc(message.id)
         .update({'isRead': true});
+  }
+
+  @override
+  Future<FirebaseDTO> createChatForUser(FirebaseDTO chatDTO) async {
+    final createdChat = await _boilerplate.create(chatDTO);
+    return createdChat;
   }
 }
