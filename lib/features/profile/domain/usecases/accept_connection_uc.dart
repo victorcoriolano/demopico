5070f07fa8668beb299b9d0e7fe2b73450c2fb59
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 19:47:55 -0300

    criando chat ao aceitar uma conexão

--- lib/features/profile/domain/usecases/accept_connection_uc.dart
+++ lib/features/profile/domain/usecases/accept_connection_uc.dart
@@ -1,7 +1,9 @@
 
+import 'package:demopico/features/profile/domain/interfaces/i_message_repository.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_network_repository.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_profile_repository.dart';
 import 'package:demopico/features/profile/domain/models/relationship.dart';
+import 'package:demopico/features/profile/infra/repository/chat_repository.dart';
 import 'package:demopico/features/profile/infra/repository/network_repository.dart';
 import 'package:demopico/features/profile/infra/repository/profile_repository.dart';
 
@@ -11,17 +13,21 @@ class AcceptConnectionUc {
     _instance ??= AcceptConnectionUc(
       networkRepository: NetworkRepository.instance,
       profileRepository: ProfileRepositoryImpl.getInstance,
+      messageRepository: ChatRepository.instance,
     );
     return _instance!;
   }
 
   final INetworkRepository _networkRepository;
   final IProfileRepository _profileRepository;
+  final ImessageRepository _chatRepository;
 
   AcceptConnectionUc({
+    required ImessageRepository messageRepository,
     required INetworkRepository networkRepository,
     required IProfileRepository profileRepository})
       : _networkRepository = networkRepository,
+        _chatRepository = messageRepository,
         _profileRepository =profileRepository ;
 
   Future<Relationship> execute(Relationship connection, ) async {
@@ -33,12 +39,16 @@ class AcceptConnectionUc {
     profileAddressedUser.profile!.connections.add(connection.requesterUser.id);
     profileRequesterUser.profile!.connections.add(connection.addressed.id);
 
+
     await Future.wait(
       [
         _profileRepository.updateProfile(profileAddressedUser.profile!),
         _profileRepository.updateProfile(profileRequesterUser.profile!)
       ]
     );
+
+    //criando chat
+    await _chatRepository.createChat(relationship.addressed, relationship.requesterUser);
     return relationship;
 
   }
