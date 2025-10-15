860d045591716642733272fb2680436ae2fea253
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-15 19:55:42 -0300

    método pra criar chat entre dois users

--- lib/features/profile/presentation/view_model/chat_list_view_model.dart
+++ lib/features/profile/presentation/view_model/chat_list_view_model.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_message_repository.dart';
 import 'package:demopico/features/profile/domain/models/chat.dart';
 import 'package:flutter/material.dart';
@@ -15,14 +18,32 @@ class ChatListViewModel extends ChangeNotifier {
   String? statement;
   StateViewModel stateVM = StateViewModel.loading;
 
+  
+
   Future<void> fetchChats(String idUser) async {
     try {
       chats = await _repository.getChatForUser(idUser);
-      stateVM =StateViewModel.success;
+      debugPrint("chats: ${chats.length}");
+      stateVM = StateViewModel.success;
     } catch (e, st){
       debugPrint("VM - Erro na view model: ${e.toString()} $st");
       stateVM = StateViewModel.error;
       statement = "Ocorreu um erro ao carregar a tela";
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  Future<Chat?> createChat(UserIdentification currentUser, UserIdentification otherUser) async {
+    try {
+      final createChat = await _repository.createChat(currentUser, otherUser);
+      chats.add(createChat);
+      return createChat;
+    } catch (e, st){
+      debugPrint("VM - Erro na view model: ${e.toString()} $st");
+      stateVM = StateViewModel.error;
+      statement = "Ocorreu um erro ao criar chat";
+      return null;
     } finally {
       notifyListeners();
     }
