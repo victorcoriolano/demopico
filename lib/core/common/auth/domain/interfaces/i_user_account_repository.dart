9952debbb72b33f6a795346c59e2a7028a153df3
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-16 21:43:34 -0300

    passando o email pra redefinir senha

--- lib/core/common/auth/domain/interfaces/i_user_account_repository.dart
+++ lib/core/common/auth/domain/interfaces/i_user_account_repository.dart
@@ -1,6 +1,9 @@
+import 'package:demopico/core/common/auth/domain/value_objects/password_vo.dart';
+
 abstract class IUserAccountRepository {
   bool get isVerified;
   Future<void> sendEmailVerification();
-  Future<bool> changePassWord();    
+  Future<bool> resetPassword(String email); 
+  Future<void> updatePassword(PasswordVo password);   
   Future<void> deleteAccount();
 } 
