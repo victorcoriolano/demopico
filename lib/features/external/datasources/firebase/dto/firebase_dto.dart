03e3162c4ccbe96c5adb24100241ea82e5ac7ba0
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-18 18:18:00 -0300

    adicionando construtor a partir de um document snapshot e método pra resolver campos que representam referencia de outros documentos

--- lib/features/external/datasources/firebase/dto/firebase_dto.dart
+++ lib/features/external/datasources/firebase/dto/firebase_dto.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
 class FirebaseDTO {
   String _id;
   final Map<String, dynamic> data;
@@ -19,5 +22,28 @@ class FirebaseDTO {
   set setId(String newId) {
     _id = newId;
   }
+
+  factory FirebaseDTO.fromDocumentSnapshot(DocumentSnapshot document){
+    return FirebaseDTO(
+      id: document.id, 
+      data: document.data() as Map<String, dynamic>);
+  }
+
+  FirebaseDTO resolveReference(String nameField){
+    final currentData = data;
+
+    if (currentData.containsKey(nameField) && currentData[nameField] is DocumentReference){
+
+      final copyData = Map<String, dynamic>.from(currentData);
+
+      copyData[nameField] = (currentData[nameField] as DocumentReference).id;
+
+      debugPrint(copyData[nameField]);
+      return copyWith(data: copyData);
+    }
+    return this;
+  }
+
+
 
 }
