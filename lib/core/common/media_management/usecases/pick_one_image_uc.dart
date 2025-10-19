f679690c96be8a01b38518aef4a835a4f1a1c054
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-18 22:41:51 -0300

    use case de pegar uma imagem

--- lib/core/common/media_management/usecases/pick_one_image_uc.dart
+++ lib/core/common/media_management/usecases/pick_one_image_uc.dart
@@ -3,10 +3,14 @@ import 'package:demopico/core/common/errors/domain_failures.dart';
 import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/core/common/media_management/interfaces/repository/i_pick_image_repository.dart';
 import 'package:demopico/core/common/media_management/models/file_model.dart';
+import 'package:demopico/core/common/media_management/services/image_picker_service.dart';
 import 'package:flutter/cupertino.dart';
 
 class PickOneImageUc {
   final IPickFileRepository _repository;
+
+  static PickOneImageUc? _instance;
+  static PickOneImageUc get instance => _instance ?? PickOneImageUc(repository: ImagePickerService.getInstance);
 
   PickOneImageUc({
     required IPickFileRepository repository})
