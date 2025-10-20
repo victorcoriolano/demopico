def625f5b498a2064e34c0d120c3499222747e63
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-20 18:53:49 -0300

    removendo responsabilidades da auth view model account

--- lib/features/user/presentation/controllers/auth_view_model_account.dart
+++ lib/features/user/presentation/controllers/auth_view_model_account.dart
@@ -1,18 +1,9 @@
-import 'package:demopico/core/app/theme/theme.dart';
 import 'package:demopico/core/common/auth/domain/entities/user_entity.dart';
-import 'package:demopico/core/common/auth/domain/usecases/change_password_uc.dart';
 import 'package:demopico/core/common/auth/domain/usecases/reset_password_uc.dart';
 import 'package:demopico/core/common/auth/domain/usecases/delete_account_uc.dart';
-import 'package:demopico/core/common/auth/domain/usecases/get_auth_state_uc.dart';
 import 'package:demopico/core/common/auth/domain/usecases/logout_uc.dart';
-import 'package:demopico/core/common/auth/domain/value_objects/password_vo.dart';
 import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/core/common/media_management/models/file_model.dart';
-import 'package:demopico/core/common/media_management/models/upload_result_file_model.dart';
-import 'package:demopico/core/common/media_management/usecases/pick_one_image_uc.dart';
-import 'package:demopico/core/common/media_management/usecases/upload_file_uc.dart';
-import 'package:demopico/core/common/media_management/usecases/upload_files_uc.dart';
-import 'package:demopico/features/profile/domain/usecases/update_profile.dart';
 import 'package:demopico/features/user/domain/enums/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -27,36 +18,22 @@ class AuthViewModelAccount extends ChangeNotifier {
       _instance ??= AuthViewModelAccount(
         changePasswordUc: ResetPasswordUc.getInstance,
         deleteAccountUc: DeleteAccountUc.getInstance,
-        getAuthState: GetCurrentUserUc.instance,
         logoutUc: LogoutUc.getInstance,
-        changePass: ChangePasswordUc.getInstance,
-        pickAImage: PickOneImageUc.instance,
-        uploadFile: UploadFileUC.getInstance,
       );
 
   AuthViewModelAccount({
     required LogoutUc logoutUc,
     required DeleteAccountUc deleteAccountUc,
     required ResetPasswordUc changePasswordUc,
-    required GetCurrentUserUc getAuthState,
-    required ChangePasswordUc changePass,
-    required PickOneImageUc pickAImage,
-    required UploadFileUC uploadFile,
   }): _resetPasswordUc = changePasswordUc,
-      _deleteAccountUc = deleteAccountUc,
-      _getAuthState = getAuthState,
-      _changePasswordUc = changePass,
       _logoutUc = logoutUc,
-      _pickOneImageUc = pickAImage,
-      _uploadFile = uploadFile;
+      _deleteAccountUc = deleteAccountUc
+      ;
 
   final LogoutUc _logoutUc;
-  final DeleteAccountUc _deleteAccountUc;
   final ResetPasswordUc _resetPasswordUc;
-  final GetCurrentUserUc _getAuthState;
-  final ChangePasswordUc _changePasswordUc;
-  final PickOneImageUc _pickOneImageUc;
-  final UploadFileUC _uploadFile;
+  final DeleteAccountUc _deleteAccountUc;
+
 
   /// inicializando como null object pra não ter ficar fazendo verificações de null toda hora  
   FileModel avatar = NullFileModel();
@@ -95,85 +72,6 @@ class AuthViewModelAccount extends ChangeNotifier {
     }
   }
 
-  Future<void> changePasswordFlow(PasswordVo newPassword) async {
-    try {
-      await _changePasswordUc.execute(newPassword);
-      Get.snackbar(
-      'Atenção',
-      'Um link para redefinir sua senha foi enviado para seu e-mail. Por favor, verifique sua caixa de entrada.',
-      snackPosition: SnackPosition.BOTTOM,
-      backgroundColor: Colors.blueAccent,
-      colorText: Colors.white,
-      dismissDirection: DismissDirection.down
-      );  
-    } on Failure catch (e) {
-      FailureServer.showError(e);
-    }
-  }
-
-  /// Update Profile Flow =>
-  /// Select new profile photo
-  Future<FileModel> selectNewImage(bool isBackGround) async {
-    try {
-      final selectedFile = await _pickOneImageUc.execute();
-      isBackGround ? backgroundImage = selectedFile : avatar = selectedFile;
-      return selectedFile;
-    } on Failure catch (e){
-      FailureServer.showError(e);
-      return NullFileModel();
-    }
-  }
-  /// confirmar atualização 
-  /// subir para o firestore storage
-  Future<UserEntity> uploadFileProfile(FileModel file) async {
-    try {
-      final streamUpload = _uploadFile.execute(file, "users/avatar/${(_currentUser as UserEntity).id}");
-      streamUpload.listen(
-        (onData) {
-          if (onData.state == UploadState.success){
-            debugPrint("Sucesso no upload: ${onData.url}");
-            avatarUrl = onData.url;
-            final profileUpdated = (_currentUser as UserEntity).profileUser.copyWith(avatar: avatarUrl);
-            _currentUser = (_currentUser as UserEntity).copyWith(profileUser: profileUpdated);
-          }
-        }, 
-        onDone: () => debugPrint("Done"),
-        onError: (error) {
-          debugPrint("Ocorreu um erro ao subir arquivo: ${error.toString()}");
-        }
-      );
-      return _currentUser as UserEntity;
-    }on Failure catch (e) {
-      FailureServer.showError(e);
-      return _currentUser as UserEntity;
-    }
-  }
-
-  Future<UserEntity> uploadBackGroundImage(FileModel file) async {
-    try {
-      final streamUpload = _uploadFile.execute(file, "users/backGround/${(_currentUser as UserEntity).id}");
-      streamUpload.listen(
-        (onData) {
-          if (onData.state == UploadState.success){
-            debugPrint("Sucesso no upload: ${onData.url}");
-            imageBackGroundUrl = onData.url;
-            final profileUpdated = (_currentUser as UserEntity).profileUser.copyWith(backgroundPicture: imageBackGroundUrl);
-            _currentUser = (_currentUser as UserEntity).copyWith(profileUser: profileUpdated);
-            debugPrint((_currentUser as UserEntity).profileUser.backgroundPicture);
-          }
-        }, 
-        onDone: () => debugPrint("Done"),
-        onError: (error) {
-          debugPrint("Ocorreu um erro ao subir arquivo: ${error.toString()}");
-        }
-      );
-      return _currentUser as UserEntity;
-    }on Failure catch (e) {
-      FailureServer.showError(e);
-      return _currentUser as UserEntity;
-    }
-  }
-  /// atualizar user entity
 
   User _currentUser = AnonymousUserEntity();
 
