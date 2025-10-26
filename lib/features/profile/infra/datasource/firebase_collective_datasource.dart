ef3ca8e1180b318868d44e5e14bd16e517e53bf9
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-26 15:01:47 -0300

    implementação do datasource do coletivo

---
+++ lib/features/profile/infra/datasource/firebase_collective_datasource.dart
@@ -0,0 +1,49 @@
+import 'package:demopico/core/common/collections/collections.dart';
+import 'package:demopico/features/external/datasources/firebase/crud_firebase.dart';
+import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
+import 'package:demopico/features/external/datasources/firebase/firestore.dart';
+import 'package:demopico/features/external/interfaces/i_crud_datasource.dart';
+import 'package:demopico/features/profile/domain/interfaces/i_coletivo_datasource.dart';
+
+class FirebaseCollectiveDatasource implements IColetivoDatasource {
+  final ICrudDataSource _crudBoilerplate;
+    static FirebaseCollectiveDatasource? _instance;
+
+    FirebaseCollectiveDatasource({ required ICrudDataSource crudDatasoure}):_crudBoilerplate = crudDatasoure;
+    static FirebaseCollectiveDatasource get instance =>
+      _instance ??= FirebaseCollectiveDatasource(crudDatasoure: CrudFirebase(collection: Collections.collectives, firestore: Firestore.getInstance));
+  
+  @override
+  Future<void> addUserOnCollective(FirebaseDTO user) {
+    // TODO: implement addUserOnCollective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<FirebaseDTO> createColetivo(FirebaseDTO coletivo) async {
+    return await _crudBoilerplate.create(coletivo);
+  }
+
+  @override
+  Future<FirebaseDTO> getCollectivoDoc(String id) async {
+    return await _crudBoilerplate.read(id);
+  }
+
+  @override
+  Future<void> removeUser(FirebaseDTO user) {
+    // TODO: implement removeUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> requestEntry(FirebaseDTO user) {
+    // TODO: implement requestEntry
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateColetivo(FirebaseDTO coletivo) async {
+    return await _crudBoilerplate.update(coletivo);
+  }
+
+}
