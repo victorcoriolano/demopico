7f37cecc3fd5d2e65c01b585f695ec3443a56ce6
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-26 14:57:00 -0300

    organização dos caminhos

--- lib/core/app/routes/app_routes.dart
+++ lib/core/app/routes/app_routes.dart
@@ -3,18 +3,30 @@
 abstract class Paths {
   static const home = '/home';
   static const hub = '/hub';
+
+  
   static const login = '/login';
+  static const signUp = '/sign-up';
+
   static const profile = '/profile';
   static const chat = '/chat-room';
   static const searchProfile = '/search';
-  static const favoriteSpot = '/favorites-spot';
+  // children of profile
   static const editProfile = '/edit-profile';
+  static const createPostPage = '/create-post-page';
+  static const createCollectivePage = '/create-collective-page';
+  static const myNetwork = '/my-network';
+  static const connections = '/connections';
+  static const notifications = '/notifications';
+  static const aboutPage = '/about-page';
+  
+
   static const map = '/map-page';
+  //children map page
   static const mySpots = '/my-spots';
   static const historySpot = '/spot-history';
-  static const createPostPage = '/create-post-page';
   static const createSpotPage = '/create-spot-page';
-  static const signUp = '/sign-up';
-  static const myNetwork = '/my-network';
-  static const aboutPage = '/about-page';
+  static const favoriteSpot = '/favorites-spot';
+  
+  
 }
