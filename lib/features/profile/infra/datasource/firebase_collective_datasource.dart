9cfe2e13496213ea3d6017f0a503cf7ea64df01f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-27 17:08:34 -0300

    adicionando método de pegar os coletivos de um determinado perfil de usuário

--- lib/features/profile/infra/datasource/firebase_collective_datasource.dart
+++ lib/features/profile/infra/datasource/firebase_collective_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demopico/core/app/routes/app_routes.dart';
 import 'package:demopico/core/common/collections/collections.dart';
 import 'package:demopico/features/external/datasources/firebase/crud_firebase.dart';
 import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto.dart';
@@ -6,10 +8,10 @@ import 'package:demopico/features/external/interfaces/i_crud_datasource.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_coletivo_datasource.dart';
 
 class FirebaseCollectiveDatasource implements IColetivoDatasource {
-  final ICrudDataSource _crudBoilerplate;
+  final ICrudDataSource<FirebaseDTO, FirebaseFirestore> _crudBoilerplate;
     static FirebaseCollectiveDatasource? _instance;
 
-    FirebaseCollectiveDatasource({ required ICrudDataSource crudDatasoure}):_crudBoilerplate = crudDatasoure;
+    FirebaseCollectiveDatasource({ required ICrudDataSource<FirebaseDTO, FirebaseFirestore> crudDatasoure}):_crudBoilerplate = crudDatasoure;
     static FirebaseCollectiveDatasource get instance =>
       _instance ??= FirebaseCollectiveDatasource(crudDatasoure: CrudFirebase(collection: Collections.collectives, firestore: Firestore.getInstance));
   
@@ -43,7 +45,12 @@ class FirebaseCollectiveDatasource implements IColetivoDatasource {
 
   @override
   Future<void> updateColetivo(FirebaseDTO coletivo) async {
-    return await _crudBoilerplate.update(coletivo);
+    await _crudBoilerplate.update(coletivo);
+  }
+  
+  @override
+  Future<List<FirebaseDTO>> getCollectiveForProfile(String idProfile) async {
+     return await _crudBoilerplate.readArrayContains(field: "members", value: idProfile);
   }
 
 }
