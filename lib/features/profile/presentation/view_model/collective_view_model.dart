89f56957d805af7e3aac498f669623b7c7f1ab5b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-27 17:14:40 -0300

    view model dos dados referentes ao coletivo

---
+++ lib/features/profile/presentation/view_model/collective_view_model.dart
@@ -0,0 +1,46 @@
+import 'package:demopico/core/common/auth/domain/entities/coletivo_entity.dart';
+import 'package:demopico/core/common/errors/failure_server.dart';
+import 'package:demopico/features/profile/domain/usecases/get_collective_uc.dart';
+import 'package:demopico/features/profile/domain/usecases/get_collectives_for_profile_uc.dart';
+import 'package:flutter/material.dart';
+
+class CollectiveViewModel extends ChangeNotifier {
+  final GetCollectivesForProfileUc _getCollectivesForProfileUc;
+  final GetCollectiveUc _getTotalInformation;
+
+    static CollectiveViewModel? _instance;
+    static CollectiveViewModel get instance =>
+      _instance ??= CollectiveViewModel(getColUC: GetCollectivesForProfileUc.instance, getColl: GetCollectiveUc.instance);
+
+  CollectiveViewModel({
+    required GetCollectiveUc getColl,
+    required GetCollectivesForProfileUc getColUC 
+  }): _getCollectivesForProfileUc = getColUC, _getTotalInformation = getColl;
+
+  List<ColetivoEntity> collectives = [];
+  late ColetivoEntity coletivo;
+  bool isLoading = false;
+
+  Future<void> getCollectives(String id) async {
+    isLoading = true;
+    notifyListeners();
+    try {
+      collectives = await _getCollectivesForProfileUc.execute(id);
+
+    }on Failure catch (failure) {
+      FailureServer.showError(failure);
+    }
+    finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> getTotalInformationCollective(String idCollective) async {
+    try {
+      coletivo = await _getTotalInformation.execute(idCollective);
+    }on Failure catch (failure){
+      FailureServer.showError(failure);
+    } 
+  }
+}
