0b67bdd11d376647db36d019984bacfcca231f04
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-30 23:12:05 -0300

    métodos serializable e tipo de notificação

--- lib/features/profile/domain/models/notification.dart
+++ lib/features/profile/domain/models/notification.dart
@@ -4,12 +4,42 @@ class NotificationItem {
   final String message;
   final bool isRead;
   final DateTime timestamp;
+  final TypeNotification type;
 
   NotificationItem({
+    required this.type,
     required this.isRead,
     required this.id,
     required this.userId,
     required this.message,
     required this.timestamp,
   }); 
+
+  factory NotificationItem.fromMap(Map<String, dynamic> map) {
+    return NotificationItem(
+      id: map['id'],
+      userId: map['userId'],
+      message: map['message'],
+      isRead: map['isRead'],
+      timestamp: DateTime.parse(map['timestamp']),
+      type: TypeNotification.values.firstWhere((t) => t.name == map['type']),
+    );  
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'message': message,
+      'isRead': isRead,
+      'timestamp': timestamp.toIso8601String(),
+      'type': type.name,
+    };  
+  }
+}
+
+enum TypeNotification {
+  inviteCollective,
+  newPostCollective,
+  newLikeOnPub,
+  newCommentOnPub,  
 }
