e33a58df0ddaddf57bb7bccc75f5ba6398248bba
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-30 23:11:42 -0300

    interface do repository

---
+++ lib/features/profile/domain/interfaces/i_notification_repository.dart
@@ -0,0 +1,7 @@
+import 'package:demopico/features/profile/domain/models/notification.dart';
+
+abstract interface class INotificationRepository {
+  Future<void> createNotification(NotificationItem notification);
+  Stream<List<NotificationItem>> watchNotifications(String idUser);
+  Future<void> markNotificationAsRead(String notificationId);
+}
