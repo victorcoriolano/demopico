aaf74a187e7f9abcfb08d2bdcb7780d29b71197c
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-01 21:26:04 -0300

    corrigindo as interfaces

--- lib/features/profile/domain/interfaces/i_message_datasource.dart
+++ lib/features/profile/domain/interfaces/i_message_datasource.dart
@@ -1,7 +1,8 @@
 abstract class IMessageDatasource<DTO> {
-    Stream<List<DTO>> getMessagesForChat(String idChat);
+    Stream<List<DTO>> watchMessagesForChat(String idChat);
     Future<List<DTO>> getChatForUser(String idUser);
-    Future<DTO> createChatForUser(DTO initialChat);
+    Future<DTO> createChat(DTO initialChat);
+    Future<DTO> createGroupChat(DTO initialChat);
     Future<DTO> sendMessage(String idChat, DTO message);
     Future<void> readMessage(String idChat, DTO message);
 }
