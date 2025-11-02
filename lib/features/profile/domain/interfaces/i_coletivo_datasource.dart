ebd6e6350deb0f08a059b153d3e02219f45d75bf
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-02 18:08:09 -0300

    método pra pegar todos os coletivos

--- lib/features/profile/domain/interfaces/i_coletivo_datasource.dart
+++ lib/features/profile/domain/interfaces/i_coletivo_datasource.dart
@@ -3,6 +3,7 @@ import 'package:demopico/features/external/datasources/firebase/dto/firebase_dto
 abstract interface class IColetivoDatasource {
   Future<FirebaseDTO> getCollectivoDoc(String id);
   Future<List<FirebaseDTO>> getCollectiveForProfile(String idProfile);
+  Future<List<FirebaseDTO>> getAllCollectives();
     Future<FirebaseDTO> createColetivo(FirebaseDTO coletivo);
     Future<void> updateColetivo(FirebaseDTO coletivo);
     Future<void> addUserOnCollective(FirebaseDTO user);
