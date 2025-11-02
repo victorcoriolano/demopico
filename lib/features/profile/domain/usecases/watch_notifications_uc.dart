771608a249811f514db2d9a8d249273c0b4a007c
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-01 21:40:30 -0300

    todos

--- lib/features/profile/domain/usecases/watch_notifications_uc.dart
+++ lib/features/profile/domain/usecases/watch_notifications_uc.dart
@@ -6,9 +6,16 @@ import 'package:demopico/features/profile/infra/repository/notification_reposito
 class WatchNotificationsUc {
   final INotificationRepository _repository;
 
+  //TODO: corrigir erro de navegação pra tela de chats 
+  //TODO: corrigir erro logout/login - esta pegando os dados do user antigo
+  //TODO: CORRIGIR OS TESTES
+  //TODO: IMPLEMENTAR MÉTODOS DO REPOSITORY QUE FICARAM FALTANDO
+  
+
   WatchNotificationsUc() : _repository = NotificationRepositoryImpl();
 
   Stream<List<NotificationItem>> execute(String idUser) {
     return _repository.watchNotifications(idUser);
   }
 } 
+
