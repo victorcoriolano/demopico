0cf02f08d8ae97ec7b6760c4310340611d8b72df
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-05 17:44:05 -0300

    metodos pra adicionar e remover usuários do grupo do chat

--- lib/features/profile/domain/interfaces/i_message_repository.dart
+++ lib/features/profile/domain/interfaces/i_message_repository.dart
@@ -8,4 +8,7 @@ abstract class ImessageRepository {
     Future<Chat> createChat(Chat chat, UserIdentification currentUser );
     Future<Message> sendMessage(String idChat, Message message);
     Future<void> readMessage(String idChat, Message message);
+    Future<void> addUserOnGroup(String idChat, List<String> idUsers);
+    Future<void> removeUserOnGroup(String idChat, List<String> idUsers);
+    
 }
