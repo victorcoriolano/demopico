00fa56da7ac4213c1652c5ed6ab19f757f777d91
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-04 23:35:15 -0300

    método pra criar chat do coletivo

--- lib/features/profile/presentation/view_model/chat_list_view_model.dart
+++ lib/features/profile/presentation/view_model/chat_list_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
+import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_message_repository.dart';
 import 'package:demopico/features/profile/domain/models/chat.dart';
 import 'package:flutter/material.dart';
@@ -18,9 +19,9 @@ class ChatListViewModel extends ChangeNotifier {
   String? statement;
   StateViewModel stateVM = StateViewModel.loading;
 
-  
-
   Future<void> fetchChats(UserIdentification userIdenti) async {
+    stateVM = StateViewModel.loading;
+    notifyListeners();
     try {
       chats = await _repository.getChatForUser(userIdenti);
       debugPrint("chats: ${chats.length}");
@@ -34,13 +35,16 @@ class ChatListViewModel extends ChangeNotifier {
     }
   }
 
-  Future<Chat?> createChat(UserIdentification currentUser, UserIdentification otherUser) async {
+  Future<Conversation?> createChat(UserIdentification currentUser, UserIdentification otherUser) async {
+    stateVM = StateViewModel.loading;
+    notifyListeners();
     try {
       if (chats.isEmpty) await fetchChats(currentUser);
-      if (chats.any((chat) => chat.participantsIds.contains(otherUser.id))) return chats.firstWhere((chat) => chat.participantsIds.contains(otherUser.id));
-      final createChat = await _repository.createChat(currentUser, otherUser);
+      if (chats.any((chat) => chat.participantsIds.contains(otherUser.id))) return chats.firstWhere((chat) => chat.participantsIds.contains(otherUser.id)) as Conversation;
+      final chat = Conversation.initFromUsers(currentUser, otherUser);
+      final createChat = await _repository.createChat(chat ,currentUser);
       chats.add(createChat);
-      return createChat;
+      return createChat as Conversation;
     } catch (e, st){
       debugPrint("VM - Erro na view model: ${e.toString()} $st");
       stateVM = StateViewModel.error;
@@ -49,5 +53,25 @@ class ChatListViewModel extends ChangeNotifier {
     } finally {
       notifyListeners();
     }
+  }
+
+  Future<void> createOrGetCollectiveChat({required 
+    UserIdentification currentUser, required 
+    List<UserIdentification> members,required String nameChat,required String photo}) async {
+      debugPrint("chamou o método");
+      try{
+        if (chats.isEmpty) await fetchChats(currentUser);
+      if (chats.any((chat) => chat.participantsIds.toSet().containsAll(members.map((m) => m.id).toList()))) return;
+      debugPrint('grupo não encontrado - criando um novo');
+      final chat = GroupChat.initial(members, nameChat, photo);
+      final createdChat = await _repository.createChat(chat, currentUser);
+      debugPrint(createdChat.toString());
+
+      chats.add(createdChat);
+      notifyListeners();
+
+      } on Failure catch (e){
+        FailureServer.showError(e);
+      }
   }
 }
