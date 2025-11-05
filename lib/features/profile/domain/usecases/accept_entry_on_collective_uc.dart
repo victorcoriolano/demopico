587270cf6e2815e3abe81ad42bc606f399e7ed6a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-05 17:44:53 -0300

    adicionando user no grup de mensagens ao adicionar ele no coletivo

--- lib/features/profile/domain/usecases/accept_entry_on_collective_uc.dart
+++ lib/features/profile/domain/usecases/accept_entry_on_collective_uc.dart
@@ -1,11 +1,12 @@
-
 import 'package:demopico/core/common/auth/domain/entities/coletivo_entity.dart';
 import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/core/common/errors/domain_failures.dart';
 import 'package:demopico/core/common/errors/failure_server.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_coletivo_repository.dart';
+import 'package:demopico/features/profile/domain/interfaces/i_message_repository.dart';
 import 'package:demopico/features/profile/domain/interfaces/i_notification_repository.dart';
 import 'package:demopico/features/profile/domain/models/notification.dart';
+import 'package:demopico/features/profile/infra/repository/chat_repository.dart';
 import 'package:demopico/features/profile/infra/repository/coletivo_repository_impl.dart';
 import 'package:demopico/features/profile/infra/repository/notification_repository_impl.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +14,15 @@ import 'package:flutter/material.dart';
 class AcceptEntryOnCollectiveUc {
   final IColetivoRepository _coletivoRepository;
   final INotificationRepository _notificationRepository;
+  final ImessageRepository _imessageRepository;
 
-  AcceptEntryOnCollectiveUc() 
-    : _coletivoRepository = ColetivoRepositoryImpl.instance,
-      _notificationRepository = NotificationRepositoryImpl();
+  AcceptEntryOnCollectiveUc()
+      : _coletivoRepository = ColetivoRepositoryImpl.instance,
+        _notificationRepository = NotificationRepositoryImpl(),
+        _imessageRepository = ChatRepository.instance;
 
-  Future<ColetivoEntity> execute(UserIdentification user, ColetivoEntity coletivo) async {
+  Future<ColetivoEntity> execute(
+      UserIdentification user, ColetivoEntity coletivo) async {
     final listRequestsEntry = coletivo.entryRequests;
     final listMembers = coletivo.members;
     if (!listRequestsEntry.contains(user.id)) throw AnotherFailure(message: "Usuário não requisitou a entrada ainda");
@@ -26,21 +30,25 @@ class AcceptEntryOnCollectiveUc {
     if (listMembers.contains(user)) throw AnotherFailure(message: "Usuário já é um membro");
     listMembers.add(user);
 
-    coletivo = coletivo.copyWith(entryRequests: listRequestsEntry, members: listMembers);
-
-    //notificando o usuário 
-    final notify = NotificationItem(
-      type: TypeNotification.newUpdateOnCollective, 
-      isRead: false, 
-      id: "", 
-      userId: user.id, 
-      message: "Você foi aceito no coletivo ${coletivo.nameColetivo}", 
-      timestamp: DateTime.now());
+    coletivo = coletivo.copyWith(
+        entryRequests: listRequestsEntry, members: listMembers);
     
-    _notificationRepository.createNotification(notify);
 
     try {
       await _coletivoRepository.updateColetivo(coletivo);
+      // adicionado user ao chat
+      await _imessageRepository.addUserOnGroup(
+          coletivo.id, listMembers.map((u) => u.id).toList());
+      //notificando o usuário
+      final notify = NotificationItem(
+          type: TypeNotification.newUpdateOnCollective,
+          isRead: false,
+          id: "",
+          userId: user.id,
+          message: "Você foi aceito no coletivo ${coletivo.nameColetivo}",
+          timestamp: DateTime.now());
+
+      _notificationRepository.createNotification(notify);
       return coletivo;
     } on Failure catch (failure) {
       debugPrint("UC erro ao aceitar usuário no coletivo: $failure");
