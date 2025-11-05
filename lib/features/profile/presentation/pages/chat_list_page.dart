1ab9facf80d650ce87d95a0c6ab82cd7545b987f
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-04 23:33:14 -0300

    adicionando carregamento no post frame callback

--- lib/features/profile/presentation/pages/chat_list_page.dart
+++ lib/features/profile/presentation/pages/chat_list_page.dart
@@ -7,7 +7,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ChatListPage extends StatefulWidget {
-  const ChatListPage({ super.key, });
+  const ChatListPage({
+    super.key,
+  });
 
   @override
   State<ChatListPage> createState() => _ChatListPageState();
@@ -18,45 +20,48 @@ class _ChatListPageState extends State<ChatListPage> {
 
   @override
   void initState() {
-   super.initState();
-   final authState = context.read<AuthViewModelAccount>().authState;
-   switch (authState){
-    
-     case AuthAuthenticated():
-       final user = UserIdentification(
-        id: authState.user.id, 
-        name: authState.user.displayName.value, 
-        profilePictureUrl: authState.user.avatar);
-       context.read<ChatListViewModel>().fetchChats(user);
-       idUser = authState.user.id;
-     case AuthUnauthenticated():
-       context.read<ChatListViewModel>().statement = "Usuário não autenticado";
-       context.read<ChatListViewModel>().stateVM = StateViewModel.error;
-   }
-   
-  }  
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final authState = context.read<AuthViewModelAccount>().authState;
+      switch (authState) {
+        case AuthAuthenticated():
+          final user = UserIdentification(
+              id: authState.user.id,
+              name: authState.user.displayName.value,
+              profilePictureUrl: authState.user.avatar);
+          context.read<ChatListViewModel>().fetchChats(user);
+          idUser = authState.user.id;
+        case AuthUnauthenticated():
+          context.read<ChatListViewModel>().statement =
+              "Usuário não autenticado";
+          context.read<ChatListViewModel>().stateVM = StateViewModel.error;
+      }
+    });
+  }
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('MINHAS CONVERSAS'), centerTitle: true,),
-           body: Consumer<ChatListViewModel>(
-            builder: (context, viewModel, child) {
-              // Usa um switch para construir a UI com base no estado da ViewModel
-            switch (viewModel.stateVM) {
-              case StateViewModel.loading:
-                return const Center(child: CircularProgressIndicator());
-              case StateViewModel.error:
-                return Center(
-                  child: Text(viewModel.statement ?? 'Erro desconhecido'),
-                );
-              case StateViewModel.success:
-                if (viewModel.chats.isEmpty) {
-                  return const Center(child: Text('Nenhuma conversa encontrada.'));
-                }
-                return ChatList(chats: viewModel.chats,currentID: idUser!);
-            } 
-            }),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MINHAS CONVERSAS'),
+        centerTitle: true,
+      ),
+      body: Consumer<ChatListViewModel>(builder: (context, viewModel, child) {
+        // Usa um switch para construir a UI com base no estado da ViewModel
+        switch (viewModel.stateVM) {
+          case StateViewModel.loading:
+            return const Center(child: CircularProgressIndicator());
+          case StateViewModel.error:
+            return Center(
+              child: Text(viewModel.statement ?? 'Erro desconhecido'),
+            );
+          case StateViewModel.success:
+            if (viewModel.chats.isEmpty) {
+              return const Center(child: Text('Nenhuma conversa encontrada.'));
+            }
+            return ChatList(chats: viewModel.chats, currentID: idUser!);
+        }
+      }),
+    );
   }
 }
