9ce6e842ba4cffbca2a79cc6ebdc6eaba5db549d
Gabriel Pires <dasaybarreto724@gmail.com>
2025-11-04 23:29:36 -0300

    metodo pra pregar mensagem pelo id

--- lib/features/profile/domain/interfaces/i_message_datasource.dart
+++ lib/features/profile/domain/interfaces/i_message_datasource.dart
@@ -1,6 +1,7 @@
 abstract class IMessageDatasource<DTO> {
     Stream<List<DTO>> watchMessagesForChat(String idChat);
     Future<List<DTO>> getChatForUser(String idUser);
+    Future<DTO> getMessageById(String idChat, String idMessage);
     Future<DTO> createChat(DTO initialChat);
     Future<DTO> createGroupChat(DTO initialChat);
     Future<DTO> sendMessage(String idChat, DTO message);
