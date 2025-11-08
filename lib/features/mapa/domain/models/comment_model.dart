240703823621804247c06222231cd770f1e1baf1
selingindev <88356338+selingindev@users.noreply.github.com>
2025-11-07 23:57:34 -0300

    USER IDENTIFICATION

--- lib/features/mapa/domain/models/comment_model.dart
+++ lib/features/mapa/domain/models/comment_model.dart
@@ -5,8 +5,7 @@ class CommentModel extends Comment{
 
   CommentModel({
     required this.id,
-    required super.peakId,
-    required super.userId,
+    required super.userIdentification,
     required super.content,
     required super.timestamp,
   });
@@ -14,8 +13,7 @@ class CommentModel extends Comment{
   factory CommentModel.fromEntity(Comment comment) {
     return CommentModel(
       id: '',
-      peakId: comment.peakId,
-      userId: comment.userId,
+      userIdentification: comment.userIdentification,
       content: comment.content,
       timestamp: comment.timestamp,
     );
@@ -25,17 +23,15 @@ class CommentModel extends Comment{
   factory CommentModel.fromJson(Map<String, dynamic> json, String id) {
     return CommentModel(
       id: id,
-      peakId: json['peakId'],
-      userId: json['userId'],
+      userIdentification: json['userIdentification'],
       content: json['content'],
-      timestamp: DateTime.parse(json['timestamp']),
+      timestamp: DateTime.parse (json['timestamp']),
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'peakId': peakId,
-      'userId': userId,
+      'userIdentification': userIdentification,
       'content': content,
       'timestamp': timestamp.toIso8601String(),
     };
@@ -43,17 +39,14 @@ class CommentModel extends Comment{
 
   CommentModel copyWith({
     String? id,
-    String? peakId,
-    String? userId,
     String? content,
     DateTime? timestamp,
   }){
     return CommentModel(
       id: id ?? this.id,
-      peakId: peakId ?? this.peakId,
-      userId: userId ?? this.userId,
       content: content ?? this.content,
-      timestamp: timestamp ?? this.timestamp,
+      timestamp: timestamp ?? this.timestamp, 
+      userIdentification: userIdentification,
     );
   }
 }
