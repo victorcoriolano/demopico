9be736e09b6e20f8f6c9186e3fb4abca0fcc7780
selingindev <88356338+selingindev@users.noreply.github.com>
2025-11-08 16:27:13 -0300

    pico id pq tinha removido antes ahjsahsahs

--- lib/features/mapa/domain/entities/comment.dart
+++ lib/features/mapa/domain/entities/comment.dart
@@ -2,11 +2,13 @@ import 'package:demopico/core/common/auth/domain/entities/user_identification.da
 
 class Comment {
   final UserIdentification userIdentification;
+  final String picoId;
   final String content;
   final DateTime timestamp;
-
+  
   Comment({
     required this.userIdentification,
+    required this.picoId,
     required this.content,
     required this.timestamp,
   });
--- lib/features/mapa/domain/models/comment_model.dart
+++ lib/features/mapa/domain/models/comment_model.dart
@@ -1,11 +1,13 @@
+import 'package:demopico/core/common/auth/domain/entities/user_identification.dart';
 import 'package:demopico/features/mapa/domain/entities/comment.dart';
 
-class CommentModel extends Comment{
+class  CommentModel extends Comment{
     final String id;
 
   CommentModel({
     required this.id,
     required super.userIdentification,
+    required super.picoId,
     required super.content,
     required super.timestamp,
   });
@@ -14,6 +16,7 @@ class CommentModel extends Comment{
     return CommentModel(
       id: '',
       userIdentification: comment.userIdentification,
+      picoId: comment.picoId,
       content: comment.content,
       timestamp: comment.timestamp,
     );
@@ -23,7 +26,8 @@ class CommentModel extends Comment{
   factory CommentModel.fromJson(Map<String, dynamic> json, String id) {
     return CommentModel(
       id: id,
-      userIdentification: json['userIdentification'],
+      userIdentification: UserIdentification.fromJson(json['userIdentification']),
+      picoId: json['picoId'],
       content: json['content'],
       timestamp: DateTime.parse (json['timestamp']),
     );
@@ -31,7 +35,8 @@ class CommentModel extends Comment{
 
   Map<String, dynamic> toMap() {
     return {
-      'userIdentification': userIdentification,
+      'userIdentification': userIdentification.toJson(),
+      'picoId': picoId,
       'content': content,
       'timestamp': timestamp.toIso8601String(),
     };
@@ -39,6 +44,8 @@ class CommentModel extends Comment{
 
   CommentModel copyWith({
     String? id,
+    UserIdentification? userIdentification,
+    String? picoId,
     String? content,
     DateTime? timestamp,
   }){
@@ -46,7 +53,8 @@ class CommentModel extends Comment{
       id: id ?? this.id,
       content: content ?? this.content,
       timestamp: timestamp ?? this.timestamp, 
-      userIdentification: userIdentification,
+      userIdentification: userIdentification ?? this.userIdentification,
+      picoId: picoId ?? this.picoId,
     );
   }
 }
