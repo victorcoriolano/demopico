63cf9bbe35d4a3c1df7bef8e44ff301ebc7d8404
selingindev <88356338+selingindev@users.noreply.github.com>
2025-11-11 17:07:18 -0300

    photo and name vcom vlaidator

--- lib/features/mapa/presentation/widgets/spot_info_widgets/photo_and_name_widget.dart
+++ lib/features/mapa/presentation/widgets/spot_info_widgets/photo_and_name_widget.dart
@@ -1,11 +1,22 @@
+import 'package:demopico/core/app/routes/app_routes.dart';
 import 'package:demopico/core/app/theme/theme.dart';
+import 'package:demopico/core/common/mixins/route_profile_validator.dart';
+import 'package:demopico/features/profile/presentation/pages/profile_page_user.dart';
+import 'package:demopico/features/user/domain/enums/auth_state.dart';
+import 'package:demopico/features/user/presentation/controllers/auth_view_model_account.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PhotoAndNameWidget extends StatelessWidget {
   final String nameUserCreator;
   final String? urlImageUser;
+  final String idUserCreator;
+
   const PhotoAndNameWidget(
-      {super.key, required this.nameUserCreator, required this.urlImageUser});
+      {super.key,
+      required this.nameUserCreator,
+      required this.urlImageUser,
+      required this.idUserCreator});
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +34,24 @@ class PhotoAndNameWidget extends StatelessWidget {
                 width: 0.5,
               ),
             ),
-            child: CircleAvatar(
-                foregroundColor: kWhite,
-                backgroundColor: kLightRed,
-                backgroundImage:
-                    urlImageUser != null ? NetworkImage(urlImageUser!) : null,
-                radius: 25,
-                child: urlImageUser == null
-                    ? Icon(
-                        Icons.person,
-                        size: 38,
-                      )
-                    : null),
+            child: InkWell(
+              onTap: () {
+                AuthState user = AuthViewModelAccount.instance.authState;
+                RouteProfileValidator.validateRoute(user, idUserCreator);
+              },
+              child: CircleAvatar(
+                  foregroundColor: kWhite,
+                  backgroundColor: kLightRed,
+                  backgroundImage:
+                      urlImageUser != null ? NetworkImage(urlImageUser!) : null,
+                  radius: 25,
+                  child: urlImageUser == null
+                      ? Icon(
+                          Icons.person,
+                          size: 38,
+                        )
+                      : null),
+            ),
           ),
           const SizedBox(height: 5),
           Text(
