5959567d32e8c58b902c97e82801882b70f7948c
selingindev <88356338+selingindev@users.noreply.github.com>
2025-11-11 17:05:51 -0300

    tirnado os imports que nao vi

--- lib/features/mapa/presentation/pages/comment_page.dart
+++ lib/features/mapa/presentation/pages/comment_page.dart
@@ -1,15 +1,11 @@
-import 'package:demopico/core/app/routes/app_routes.dart';
 import 'package:demopico/core/app/theme/theme.dart';
 import 'package:demopico/core/common/auth/domain/entities/user_entity.dart';
 import 'package:demopico/core/common/mixins/route_profile_validator.dart';
 import 'package:demopico/core/common/widgets/snackbar_utils.dart';
 import 'package:demopico/features/mapa/presentation/controllers/comment_controller.dart';
-import 'package:demopico/features/profile/presentation/pages/profile_page_user.dart';
 import 'package:demopico/features/user/domain/enums/auth_state.dart';
 import 'package:demopico/features/user/presentation/controllers/auth_view_model_account.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/utils.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
