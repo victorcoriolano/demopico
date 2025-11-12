207e81d082d80af699e0978ffba822c0126dfcec
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-11-12 19:40:45 -0300

    mudanças grduais

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -4,6 +4,7 @@ import 'package:demopico/core/app/providers/providers.dart';
 import 'package:demopico/core/app/routes/app_pages.dart';
 import 'package:demopico/core/app/theme/theme.dart';
 import 'package:demopico/features/home/presentation/widgets/central_page_background.dart';
+import 'package:demopico/features/home/presentation/widgets/events_bottom_sheet.dart';
 import 'package:demopico/features/home/presentation/widgets/skatepico_logo.dart';
 import 'package:demopico/features/mapa/presentation/controllers/map_controller.dart' show MapControllerProvider;
 import 'package:demopico/features/mapa/presentation/view_services/modal_helper.dart' show ModalHelper;
@@ -23,7 +24,7 @@ class MyAppWidget extends StatelessWidget {
     return MultiProvider(
       providers: myProviders,
       child: GetMaterialApp(
-        home: HomePage(),
+        home: HomePageTest(),
         debugShowCheckedModeBanner: false,
         title: 'SKATEPICO',
         theme: appTheme,
@@ -61,8 +62,67 @@ class _HomePageTestState extends State<HomePageTest> {
                     context, pico);
                     }),
           ),
-        Expanded(child: SizedBox(height: 20)),
+          
+        SizedBox(height: 20),
+        Stack(
+          fit: StackFit.loose,
+          children: [
+            EventsBottomSheet(),          
+          ],
+        )
+
+        //DraggableBackground(),
         ],
+      ),
+    );
+  }
+}
+
+class DraggableBackground extends StatelessWidget {
+  const DraggableBackground({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[20],
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16), bottom: Radius.circular(0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: BorderSide.strokeAlignInside,
+            blurStyle: BlurStyle.solid,
+            blurRadius: 1,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      height: 110,
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Text(
+          "ACESSAR O MENU",
+          style: TextStyle(
+            fontSize: 18,
+            height: 10,
+            decorationThickness: 2,
+            decoration: TextDecoration.underline,
+            decorationStyle: TextDecorationStyle.wavy,
+            decorationColor: Color(0xFFB0BEC5),
+            shadows: [
+              Shadow(
+                color: Colors.black26,
+                offset: Offset(1, 3),
+                blurRadius: 2,
+              ),
+            ],
+            fontFamily: 'CupertinoSystemDisplay',
+            fontWeight: FontWeight.bold,
+            color: Colors.blueGrey[800],
+          ),
+        ),
       ),
     );
   }
